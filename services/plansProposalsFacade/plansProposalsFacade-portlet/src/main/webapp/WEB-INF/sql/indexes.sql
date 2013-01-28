fce87ec28f6467a57f85b616eab072566115af4a
janusz <janusz@janusz.(none)>
2013-01-28 23:17:00 +0100

    Sql files for migration and facade tables

---
+++ services/plansProposalsFacade/plansProposalsFacade-portlet/src/main/webapp/WEB-INF/sql/indexes.sql
@@ -0,0 +1,132 @@
+create index IX_61FA63BB on xcolab_ActivitySubscription (classNameId, classPK, receiverId);
+create index IX_20164EB2 on xcolab_ActivitySubscription (classNameId, classPK, type_, extraData);
+create index IX_C2ED8710 on xcolab_ActivitySubscription (classNameId, classPK, type_, extraData, receiverId);
+create index IX_1413A2B6 on xcolab_ActivitySubscription (classNameId, classPK, type_, receiverId);
+create index IX_33049EE6 on xcolab_ActivitySubscription (receiverId);
+
+create index IX_5192E6B6 on xcolab_Contest (PlanTypeId);
+create index IX_CEF1EFC6 on xcolab_Contest (contestActive);
+create index IX_D29429DB on xcolab_Contest (contestActive, featured_);
+create index IX_491DA3A6 on xcolab_Contest (contestActive, flag);
+create index IX_C09FE3B3 on xcolab_Contest (contestActive, flagText);
+
+create index IX_379DF74A on xcolab_ContestDebate (ContestPK);
+
+create index IX_ED61C03C on xcolab_ContestPhase (ContestPK);
+create index IX_2BA2B787 on xcolab_ContestPhase (ContestPK, PhaseStartDate, PhaseEndDate);
+create index IX_9F1D3B81 on xcolab_ContestPhase (ContestPK, phaseActiveOverride);
+
+create index IX_D97B920F on xcolab_ContestPhaseColumn (ContestPhasePK);
+
+create index IX_E1468F04 on xcolab_ContestTeamMember (contestId);
+
+create index IX_8526458A on xcolab_DiscussionCategory (categoryGroupId);
+create index IX_306B69EF on xcolab_DiscussionCategory (categoryId);
+
+create index IX_23E0BE87 on xcolab_DiscussionMessage (body, categoryGroupId);
+create index IX_39430975 on xcolab_DiscussionMessage (categoryId, threadId);
+create index IX_189EA1C3 on xcolab_DiscussionMessage (messageId);
+create index IX_1A115E3F on xcolab_DiscussionMessage (subject, categoryGroupId);
+create index IX_FDE36548 on xcolab_DiscussionMessage (threadId);
+
+create index IX_63652E37 on xcolab_DiscussionMessageFlag (messageId);
+
+create index IX_B61888D4 on xcolab_FocusArea (name);
+
+create index IX_CE67B1A0 on xcolab_FocusAreaOntologyTerm (focusAreaId);
+
+create index IX_9DF5C6F0 on xcolab_Message (fromId);
+
+create index IX_E4B60412 on xcolab_MessageRecipientStatus (messageId);
+create index IX_76FF2A4C on xcolab_MessageRecipientStatus (messageId, userId);
+create index IX_74DCC2DA on xcolab_MessageRecipientStatus (userId);
+create index IX_88CD5CB0 on xcolab_MessageRecipientStatus (userId, archived);
+
+create index IX_F504493F on xcolab_MessagingUserPreferences (userId);
+
+create index IX_C4F6226E on xcolab_ModelDiscussion (categoryId);
+create index IX_72D6F073 on xcolab_ModelDiscussion (modelId);
+
+create index IX_16CBB25B on xcolab_ModelGlobalPreference (modelCategoryId);
+create index IX_E9B5E03D on xcolab_ModelGlobalPreference (modelId);
+
+create index IX_23506DA6 on xcolab_ModelInputGroup (modelId);
+create index IX_CC01932 on xcolab_ModelInputGroup (parentGroupPK);
+
+create index IX_39326F55 on xcolab_ModelInputItem (modelGroupId);
+create index IX_13790D44 on xcolab_ModelInputItem (modelId);
+create index IX_EF979667 on xcolab_ModelInputItem (modelId, modelInputItemID);
+create index IX_CCEFE733 on xcolab_ModelInputItem (modelInputItemID);
+
+create index IX_6D3808C8 on xcolab_ModelOutputChartOrder (modelId, modelOutputLabel);
+
+create index IX_A17AABB on xcolab_ModelOutputItem (modelOutputItemId);
+
+create index IX_E7C0C412 on xcolab_ModelPosition (modelId);
+
+create index IX_18FC4546 on xcolab_OntologySpace (name);
+
+create index IX_50DE58D6 on xcolab_OntologyTerm (name);
+create index IX_3E2347AB on xcolab_OntologyTerm (ontologySpaceId);
+create index IX_FEB70AB0 on xcolab_OntologyTerm (parentId);
+create index IX_A6AC072 on xcolab_OntologyTerm (parentId, ontologySpaceId);
+
+create index IX_4AE95AB4 on xcolab_OntologyTermEntity (classNameId);
+create index IX_7AF843AF on xcolab_OntologyTermEntity (classNameId, classPK);
+create index IX_EF334F35 on xcolab_OntologyTermEntity (termId);
+create index IX_BA88C839 on xcolab_OntologyTermEntity (termId, classNameId);
+
+create index IX_FAAC13FD on xcolab_PlanAttribute (attributeName, attributeValue);
+create index IX_EF85A9DB on xcolab_PlanAttribute (planId);
+create index IX_9A8772FC on xcolab_PlanAttribute (planId, attributeName);
+
+create index IX_F9CFF936 on xcolab_PlanAttributeFilter (planUserSettingsId, attributeName);
+
+create index IX_E94D3159 on xcolab_PlanColumnSettings (planUserSettingsId, columnName);
+
+create index IX_92C3D6FB on xcolab_PlanDescription (planId);
+create index IX_7658F2C4 on xcolab_PlanDescription (planId, planVersion);
+
+create index IX_9FCD5E52 on xcolab_PlanFan (planId);
+create index IX_4B38448C on xcolab_PlanFan (planId, userId);
+create index IX_208A9DF4 on xcolab_PlanFan (userId);
+
+create index IX_B269B130 on xcolab_PlanItem (planId);
+
+create index IX_B8E3C5A2 on xcolab_PlanMeta (planId);
+
+create index IX_B093827F on xcolab_PlanModelRun (planId);
+create index IX_1CF31DC0 on xcolab_PlanModelRun (planId, planVersion);
+
+create index IX_D1556A46 on xcolab_PlanPosition (positionId);
+
+create index IX_B1AEB06B on xcolab_PlanPositionItem (planPositionsId);
+
+create index IX_4FA12229 on xcolab_PlanPositions (planId);
+
+create index IX_EC8EC92C on xcolab_PlanPropertyFilter (planUserSettingsId, propertyName);
+
+create index IX_EBB4128D on xcolab_PlanRelated (relatedPlanId);
+
+create index IX_8D33E176 on xcolab_PlanSection (planId, planSectionDefinitionId);
+create index IX_21590829 on xcolab_PlanSection (planId, planSectionDefinitionId, planVersion);
+
+create index IX_9411AFEC on xcolab_PlanSectionPlanMap (relatedPlanId);
+create index IX_C8499DDD on xcolab_PlanSectionPlanMap (sectionId);
+
+create index IX_79B9BCB6 on xcolab_PlanTeamHistory (planId);
+create index IX_84A1FEF0 on xcolab_PlanTeamHistory (planId, userId);
+
+create index IX_32F8E764 on xcolab_PlanTemplateSection (planTemplateId);
+
+create index IX_EC7AD748 on xcolab_PlanType (isDefault);
+
+create index IX_F5C3FCBC on xcolab_PlanTypeAttribute (planTypeId, attributeName);
+
+create index IX_2A263730 on xcolab_PlanVote (contestId);
+create index IX_4E915F6A on xcolab_PlanVote (contestId, userId);
+create index IX_4E60E847 on xcolab_PlanVote (planId);
+
+create index IX_C1242E47 on xcolab_PlansFilterPosition (userId, planTypeId);
+
+create index IX_95AABD4 on xcolab_PlansUserSettings (userId, planTypeId);
---
+++ services/plansProposalsFacade/plansProposalsFacade-portlet/src/main/webapp/WEB-INF/sql/tables.sql
@@ -0,0 +1,506 @@
+create table xcolab_ActivitySubscription (
+	pk LONG not null primary key,
+	classNameId LONG,
+	classPK LONG,
+	type_ INTEGER,
+	extraData TEXT null,
+	receiverId LONG,
+	createDate DATE null,
+	modifiedDate DATE null
+);
+
+create table xcolab_Contest (
+	ContestPK LONG not null primary key,
+	ContestName VARCHAR(2048) null,
+	ContestShortName VARCHAR(1024) null,
+	ContestDescription VARCHAR(2048) null,
+	ContestModelDescription VARCHAR(2048) null,
+	ContestPositionsDescription VARCHAR(2048) null,
+	defaultPlanDescription TEXT null,
+	PlanTypeId LONG,
+	created DATE null,
+	updated DATE null,
+	authorId LONG,
+	contestActive BOOLEAN,
+	planTemplateId LONG,
+	focusAreaId LONG,
+	contestLogoId LONG,
+	featured_ BOOLEAN,
+	plansOpenByDefault BOOLEAN,
+	flag INTEGER,
+	flagText VARCHAR(1024) null,
+	flagTooltip VARCHAR(1024) null,
+	groupId LONG,
+	discussionGroupId LONG,
+	weight INTEGER,
+	resourcesUrl VARCHAR(1024) null
+);
+
+create table xcolab_ContestDebate (
+	id_ LONG not null primary key,
+	debateId LONG,
+	ContestPK LONG
+);
+
+create table xcolab_ContestPhase (
+	ContestPhasePK LONG not null primary key,
+	ContestPK LONG,
+	ContestPhaseType LONG,
+	ContestPhaseDescriptionOverride TEXT null,
+	phaseActiveOverride BOOLEAN,
+	PhaseStartDate DATE null,
+	PhaseEndDate DATE null,
+	nextStatus VARCHAR(75) null,
+	created DATE null,
+	updated DATE null,
+	authorId LONG
+);
+
+create table xcolab_ContestPhaseColumn (
+	id_ LONG not null primary key,
+	ContestPhasePK LONG,
+	columnName VARCHAR(75) null,
+	columnWeight INTEGER,
+	defaultSort BOOLEAN
+);
+
+create table xcolab_ContestPhaseType (
+	id_ LONG not null primary key,
+	name VARCHAR(1024) null,
+	description TEXT null,
+	status VARCHAR(75) null
+);
+
+create table xcolab_ContestTeamMember (
+	id_ LONG not null primary key,
+	contestId LONG,
+	userId LONG,
+	role VARCHAR(75) null
+);
+
+create table xcolab_DiscussionCategory (
+	pk LONG not null primary key,
+	categoryId LONG,
+	categoryGroupId LONG,
+	authorId LONG,
+	name VARCHAR(1024) null,
+	description TEXT null,
+	createDate DATE null,
+	deleted DATE null,
+	threadsCount INTEGER,
+	lastActivityDate DATE null,
+	lastActivityAuthorId LONG
+);
+
+create table xcolab_DiscussionCategoryGroup (
+	id_ LONG not null primary key,
+	description TEXT null,
+	url VARCHAR(1024) null,
+	commentsThread LONG
+);
+
+create table xcolab_DiscussionMessage (
+	pk LONG not null primary key,
+	messageId LONG,
+	subject VARCHAR(1024) null,
+	body TEXT null,
+	threadId LONG,
+	categoryId LONG,
+	categoryGroupId LONG,
+	authorId LONG,
+	createDate DATE null,
+	version LONG,
+	deleted DATE null,
+	responsesCount INTEGER,
+	lastActivityDate DATE null,
+	lastActivityAuthorId LONG
+);
+
+create table xcolab_DiscussionMessageFlag (
+	pk LONG not null primary key,
+	messageId LONG,
+	flagType VARCHAR(75) null,
+	data_ VARCHAR(2048) null,
+	created DATE null,
+	userId LONG
+);
+
+create table xcolab_FocusArea (
+	id_ LONG not null primary key,
+	name VARCHAR(256) null
+);
+
+create table xcolab_FocusAreaOntologyTerm (
+	focusAreaId LONG not null,
+	ontologyTermId LONG not null,
+	primary key (focusAreaId, ontologyTermId)
+);
+
+create table xcolab_Message (
+	messageId LONG not null primary key,
+	fromId LONG,
+	repliesTo LONG,
+	createDate DATE null,
+	subject VARCHAR(2048) null,
+	content TEXT null
+);
+
+create table xcolab_MessageRecipientStatus (
+	messageRecipientId LONG not null primary key,
+	messageId LONG,
+	userId LONG,
+	opened BOOLEAN,
+	archived BOOLEAN
+);
+
+create table xcolab_MessagingUserPreferences (
+	messagingPreferencesId LONG not null primary key,
+	userId LONG,
+	emailOnSend BOOLEAN,
+	emailOnReceipt BOOLEAN,
+	emailOnActivity BOOLEAN
+);
+
+create table xcolab_ModelCategory (
+	modelCategoryPK LONG not null primary key,
+	modelCategoryName VARCHAR(256) null,
+	modelCategoryDescription VARCHAR(2048) null,
+	modelCategoryDisplayWeight INTEGER
+);
+
+create table xcolab_ModelDiscussion (
+	modelDiscussionId LONG not null primary key,
+	modelId LONG,
+	categoryId LONG
+);
+
+create table xcolab_ModelGlobalPreference (
+	modelGlobalPreferencePK LONG not null primary key,
+	modelId LONG,
+	visible BOOLEAN,
+	weight INTEGER,
+	expertEvaluationPageId LONG,
+	modelCategoryId LONG
+);
+
+create table xcolab_ModelInputGroup (
+	modelInputGroupPK LONG not null primary key,
+	modelId LONG,
+	nameAndDescriptionMetaDataId LONG,
+	name VARCHAR(1024) null,
+	description TEXT null,
+	displayItemOrder INTEGER,
+	groupType VARCHAR(256) null,
+	parentGroupPK LONG
+);
+
+create table xcolab_ModelInputItem (
+	modelInputItemPK LONG not null primary key,
+	modelId LONG,
+	modelInputItemID LONG,
+	modelGroupId LONG,
+	displayItemOrder INTEGER,
+	type_ VARCHAR(256) null,
+	properties VARCHAR(2048) null
+);
+
+create table xcolab_ModelOutputChartOrder (
+	modelOutputChartOrderPK LONG not null primary key,
+	modelId LONG,
+	modelOutputLabel VARCHAR(1024) null,
+	modelOutputChartOrder INTEGER,
+	modelIndexRangePolicy VARCHAR(2048) null,
+	modelIndexRangeMessage VARCHAR(2048) null,
+	modelIndexErrorPolicy VARCHAR(2048) null,
+	modelIndexErrorMessage VARCHAR(2048) null,
+	modelChartIsVisible BOOLEAN
+);
+
+create table xcolab_ModelOutputItem (
+	modelOutputItemModifierPK LONG not null primary key,
+	modelId LONG,
+	modelOutputItemId LONG,
+	modelOutputItemOrder INTEGER,
+	modelItemRangePolicy VARCHAR(2048) null,
+	modelItemRangeMessage VARCHAR(2048) null,
+	modelItemErrorPolicy VARCHAR(2048) null,
+	modelItemErrorMessage VARCHAR(2048) null,
+	modelItemLabelFormat VARCHAR(2048) null,
+	modelItemIsVisible BOOLEAN,
+	itemType VARCHAR(256) null,
+	relatedOutputItem LONG
+);
+
+create table xcolab_ModelPosition (
+	id_ LONG not null primary key,
+	positionId LONG,
+	modelId LONG
+);
+
+create table xcolab_OntologySpace (
+	id_ LONG not null primary key,
+	name VARCHAR(256) null,
+	description TEXT null
+);
+
+create table xcolab_OntologyTerm (
+	id_ LONG not null primary key,
+	parentId LONG,
+	ontologySpaceId LONG,
+	name VARCHAR(256) null,
+	descriptionUrl VARCHAR(1024) null
+);
+
+create table xcolab_OntologyTermEntity (
+	id_ LONG not null primary key,
+	termId LONG,
+	classNameId LONG,
+	classPK LONG
+);
+
+create table xcolab_PlanAttribute (
+	attributeId LONG not null primary key,
+	planId LONG,
+	attributeName VARCHAR(256) null,
+	attributeValue VARCHAR(2048) null
+);
+
+create table xcolab_PlanAttributeFilter (
+	planAttributeFilterId LONG not null primary key,
+	attributeName VARCHAR(75) null,
+	planUserSettingsId LONG,
+	max DOUBLE,
+	min DOUBLE,
+	stringVal VARCHAR(2048) null
+);
+
+create table xcolab_PlanColumnSettings (
+	planColumnSettingsId LONG not null primary key,
+	columnName VARCHAR(75) null,
+	planUserSettingsId LONG,
+	visible BOOLEAN
+);
+
+create table xcolab_PlanDescription (
+	id_ LONG not null primary key,
+	planId LONG,
+	name VARCHAR(1024) null,
+	description TEXT null,
+	version LONG,
+	planVersion LONG,
+	created DATE null,
+	updateAuthorId LONG,
+	image LONG,
+	pitch VARCHAR(2048) null
+);
+
+create table xcolab_PlanFan (
+	id_ LONG not null primary key,
+	userId LONG,
+	planId LONG,
+	created DATE null,
+	deleted DATE null
+);
+
+create table xcolab_PlanItem (
+	id_ LONG not null primary key,
+	planId LONG,
+	state VARCHAR(75) null,
+	updated DATE null,
+	updateAuthorId LONG,
+	updateType VARCHAR(75) null,
+	version LONG
+);
+
+create table xcolab_PlanMeta (
+	id_ LONG not null primary key,
+	planId LONG,
+	planTypeId LONG,
+	planCreated LONG,
+	authorId LONG,
+	votes INTEGER,
+	planGroupId LONG,
+	open BOOLEAN,
+	status VARCHAR(75) null,
+	mbCategoryId LONG,
+	categoryGroupId LONG,
+	version LONG,
+	planVersion LONG,
+	created DATE null,
+	updateAuthorId LONG,
+	modelId LONG,
+	promoted BOOLEAN,
+	previousContestPhase LONG,
+	contestPhase LONG
+);
+
+create table xcolab_PlanModelRun (
+	id_ LONG not null primary key,
+	planId LONG,
+	scenarioId LONG,
+	planVersion LONG,
+	version LONG,
+	created DATE null,
+	updateAuthorId LONG
+);
+
+create table xcolab_PlanPosition (
+	planId LONG not null,
+	positionId LONG not null,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	primary key (planId, positionId)
+);
+
+create table xcolab_PlanPositionItem (
+	planPositionsId LONG not null,
+	positionId LONG not null,
+	primary key (planPositionsId, positionId)
+);
+
+create table xcolab_PlanPositions (
+	id_ LONG not null primary key,
+	planId LONG,
+	planVersion LONG,
+	version LONG,
+	created DATE null,
+	updateAuthorId LONG
+);
+
+create table xcolab_PlanPropertyFilter (
+	planPropertyFilterId LONG not null primary key,
+	propertyName VARCHAR(75) null,
+	planUserSettingsId LONG,
+	value VARCHAR(75) null
+);
+
+create table xcolab_PlanRelated (
+	sectionId LONG not null,
+	relatedPlanId LONG not null,
+	primary key (sectionId, relatedPlanId)
+);
+
+create table xcolab_PlanSection (
+	id_ LONG not null primary key,
+	planSectionDefinitionId LONG,
+	planId LONG,
+	content TEXT null,
+	created DATE null,
+	version LONG,
+	planVersion LONG,
+	updateAuthorId LONG
+);
+
+create table xcolab_PlanSectionDefinition (
+	id_ LONG not null primary key,
+	adminTitle VARCHAR(1024) null,
+	title VARCHAR(1024) null,
+	defaultText TEXT null,
+	helpText TEXT null,
+	characterLimit INTEGER,
+	focusAreaId LONG,
+	locked BOOLEAN
+);
+
+create table xcolab_PlanSectionPlanMap (
+	sectionId LONG not null,
+	relatedPlanId LONG not null,
+	primary key (sectionId, relatedPlanId)
+);
+
+create table xcolab_PlanTeamHistory (
+	id_ LONG not null primary key,
+	planId LONG,
+	userId LONG,
+	action VARCHAR(75) null,
+	payload VARCHAR(75) null,
+	created DATE null,
+	updateAuthorId LONG
+);
+
+create table xcolab_PlanTemplate (
+	id_ LONG not null primary key,
+	name VARCHAR(1024) null
+);
+
+create table xcolab_PlanTemplateSection (
+	planTemplateId LONG not null,
+	planSectionId LONG not null,
+	weight INTEGER,
+	primary key (planTemplateId, planSectionId)
+);
+
+create table xcolab_PlanType (
+	planTypeId LONG not null primary key,
+	name VARCHAR(75) null,
+	description VARCHAR(2048) null,
+	modelId LONG,
+	modelTypeName VARCHAR(75) null,
+	published BOOLEAN,
+	publishedCounterpartId LONG,
+	isDefault BOOLEAN,
+	defaultModelId LONG,
+	defaultScenarioId LONG
+);
+
+create table xcolab_PlanTypeAttribute (
+	planTypeAttributeId LONG not null primary key,
+	planTypeId LONG,
+	attributeName VARCHAR(75) null
+);
+
+create table xcolab_PlanTypeColumn (
+	planTypeColumnId LONG not null primary key,
+	planTypeId LONG,
+	weight INTEGER,
+	columnName VARCHAR(75) null,
+	visibleByDefault BOOLEAN
+);
+
+create table xcolab_PlanVote (
+	userId LONG not null,
+	contestId LONG not null,
+	planId LONG,
+	createDate DATE null,
+	primary key (userId, contestId)
+);
+
+create table xcolab_PlansFilter (
+	userId LONG not null,
+	planTypeId LONG not null,
+	name VARCHAR(75) null,
+	creator VARCHAR(75) null,
+	description VARCHAR(75) null,
+	CO2From DOUBLE,
+	CO2To DOUBLE,
+	votesFrom DOUBLE,
+	votesTo DOUBLE,
+	damageFrom DOUBLE,
+	damageTo DOUBLE,
+	mitigationFrom DOUBLE,
+	mitigationTo DOUBLE,
+	dateFrom DATE null,
+	dateTo DATE null,
+	filterPositionsAll BOOLEAN,
+	enabled BOOLEAN,
+	primary key (userId, planTypeId)
+);
+
+create table xcolab_PlansFilterPosition (
+	userId LONG not null,
+	planTypeId LONG not null,
+	positionId LONG not null,
+	primary key (userId, planTypeId, positionId)
+);
+
+create table xcolab_PlansUserSettings (
+	planUserSettingsId LONG not null primary key,
+	userId LONG,
+	planTypeId LONG,
+	sortColumn VARCHAR(75) null,
+	sortDirection VARCHAR(75) null,
+	filterEnabled BOOLEAN,
+	filterPositionsAll BOOLEAN
+);
