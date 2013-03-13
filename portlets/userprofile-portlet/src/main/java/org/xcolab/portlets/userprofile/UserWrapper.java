3272463aa5a44a62100ebddce2c2f43caf8b7408
JanuszPa <janusz.parfieniuk@gmail.com>
2013-03-13 17:54:43 +0100

    [XC-2, XC-24], Fies for user profile bio edit form

--- portlets/userprofile-portlet/src/main/java/org/xcolab/portlets/userprofile/UserWrapper.java
+++ portlets/userprofile-portlet/src/main/java/org/xcolab/portlets/userprofile/UserWrapper.java
@@ -68,7 +68,8 @@ public class UserWrapper {
         this.user = user;
         about = ExpandoValueLocalServiceUtil.getData(User.class.getName(), CommunityConstants.EXPANDO, 
                 CommunityConstants.BIO, user.getUserId(), StringPool.BLANK);
-        filteredAbout = Helper.filterLinkifyUrls(Helper.filterLineBreaks(about));
+        //filteredAbout = Helper.filterLinkifyUrls(Helper.filterLineBreaks(about));
+        filteredAbout = about;
         
         realName = getName(user.getFullName(), user.getScreenName());
         firstName = user.getFirstName();
