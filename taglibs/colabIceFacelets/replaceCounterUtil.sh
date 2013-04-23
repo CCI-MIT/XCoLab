f6547aec571fac39967d4710d5cc0fa4dc0a3569
JanuszPa <janusz.parfieniuk@gmail.com>
2013-04-23 09:06:53 +0200

    Discussions portlet goes IceFaces 3.2

--- taglibs/colabIceFacelets/replaceCounterUtil.sh
+++ taglibs/colabIceFacelets/replaceCounterUtil.sh
@@ -3,7 +3,7 @@ for plik in `grep "\&amp;nbsp;" . -r  --exclude="*svn*" -l --include="*xhtml"`
 do
 	echo fixing file $plik
     cat $plik | sed s/amp\;nbsp\;/#160\;/g > /tmp/tmp_src
-#    cp ../../../tmp_src $plik
+    cp /tmp/tmp_src $plik
 done
 
 
