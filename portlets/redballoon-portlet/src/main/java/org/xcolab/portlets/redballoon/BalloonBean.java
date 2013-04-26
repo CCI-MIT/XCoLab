18cba47731a58f5d0e0cfae4b812f42a75b5cb03
pdeboer <pdeboer5@gmail.com>
2013-04-25 20:29:34 -0400

    cookie progress

--- portlets/redballoon-portlet/src/main/java/org/xcolab/portlets/redballoon/BalloonBean.java
+++ portlets/redballoon-portlet/src/main/java/org/xcolab/portlets/redballoon/BalloonBean.java
@@ -11,26 +11,74 @@ import com.liferay.util.mail.MailEngineException;
 
 public class BalloonBean {
 	private String email = "";
-	private String page = "2aboutReferral";
 	private boolean redirectUserToGetURL = false;
-	private String display = "nothing";
+	private View forceNextPage = null;
+	private BalloonCookie cookie;
+	private HttpServletRequest httpReq;
+	private boolean setCookie = false;
 
 	/**
-	 * @return the display
+	 * views
+	 * 
+	 * @author pdeboer
+	 * 
 	 */
-	public String getDisplay() {
-		return display;
+	private enum View {
+		ABOUT_REFERRAL, ABOUT_COLAB, SHARE
 	}
 
 	/**
-	 * 
+	 * @return the page
 	 */
+	public String getPage() {
+		if (forceNextPage != null) {
+			return forceNextPage.toString();
+		} else if (cookie != null && cookie.getUuid() != null) {
+			return View.SHARE.toString();
+		} else {
+			return View.ABOUT_COLAB.toString();
+		}
+	}
+
 	public BalloonBean() {
+		this(false);
+	}
+
+	/**
+	 * if testing, set skipinit to true
+	 * 
+	 * @param skipInit
+	 */
+	public BalloonBean(boolean skipInit) {
+		setCookie = false;
+		
+		if (!skipInit) {
+			init();
+		}
+	}
+	
+	/**
+	 * @return the cookie
+	 */
+	public BalloonCookie getCookie() {
+		return cookie;
+	}
+
+	private void init() {
 		email = getEmailOfCurrentUser();
-		HttpServletRequest httpReq = PortalUtil
-				.getOriginalServletRequest(PortalUtil
-						.getHttpServletRequest(Helper.getPortletRequest()));
-		display = httpReq.getParameter("param");
+		httpReq = PortalUtil.getOriginalServletRequest(PortalUtil
+				.getHttpServletRequest(Helper.getPortletRequest()));
+
+		cookie = BalloonCookie.fromCookieArray(httpReq.getCookies());
+
+		String GETParamURL = httpReq.getParameter("url");
+
+		if (GETParamURL != null) {
+			cookie = new BalloonCookie();
+			cookie.setUuid(GETParamURL);
+
+			setCookie = true;
+		}
 	}
 
 	/**
@@ -53,27 +101,19 @@ public class BalloonBean {
 		}
 	}
 
-	/**
-	 * @return the page
-	 */
-	public String getPage() {
-		return page;
-	}
-
-	/**
-	 * @param page
-	 *            the page to set
-	 */
-	public void setPage(String page) {
-		this.page = page;
-	}
-
 	public String getEmail() {
 		return email;
 	}
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	/**
+	 * @return the setCookie
+	 */
+	public boolean isSetCookie() {
+		return setCookie;
 	}
 
 	public void requestURL(ActionEvent e) throws AddressException,
