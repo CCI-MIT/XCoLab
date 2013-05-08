6a41ac4c9ab1b6ed9a552dc26e365541f3eb69e1
pdeboer <pdeboer5@gmail.com>
2013-05-07 23:30:01 -0400

    facelets v3

---
+++ taglibs/colabIceFacelets3/src/main/java/org/climatecollaboratorium/validators/RequiredFieldValidator.java
@@ -0,0 +1,15 @@
+package org.climatecollaboratorium.validators;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+public class RequiredFieldValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext ctx, UIComponent comp, Object val) throws ValidatorException {
+        
+    }
+
+}
