81a8e26d321c6f5cde8918118a631c8fdedf95e0
pdeboer <pdeboer5@gmail.com>
2013-05-08 16:06:32 -0400

    mail function

--- portlets/redballoon-portlet/src/main/java/org/xcolab/portlets/redballoon/BalloonBean.java
+++ portlets/redballoon-portlet/src/main/java/org/xcolab/portlets/redballoon/BalloonBean.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.faces.event.ActionEvent;
 import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.portlet.PortletSession;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
@@ -23,6 +25,7 @@ import com.liferay.portlet.expando.model.ExpandoTable;
 import com.liferay.portlet.expando.service.ExpandoColumnLocalServiceUtil;
 import com.liferay.portlet.expando.service.ExpandoTableLocalServiceUtil;
 import com.liferay.portlet.expando.service.ExpandoValueLocalServiceUtil;
+import com.liferay.util.mail.MailEngine;
 import com.liferay.util.mail.MailEngineException;
 
 public class BalloonBean implements Serializable {
@@ -84,6 +87,7 @@ public class BalloonBean implements Serializable {
 		String GETParamURL = httpReq.getParameter("url");
 		if (GETParamURL != null) {
 			balloonCookie.setUrl(GETParamURL);
+			sendNotificationEmail();
 		}
 
 		String GETParamID = httpReq.getParameter("user");
@@ -108,17 +112,51 @@ public class BalloonBean implements Serializable {
 		ensureSignedInUserHasExpandoValue();
 	}
 
+	private void sendNotificationEmail() throws AddressException,
+			MailEngineException {
+
+		PortletSession session = Helper.getPortletRequest().getPortletSession();
+		if (session.getAttributeMap().containsKey(EMAIL_SENT))
+			return;
+		String messageSubject = EMAIL_SUBJECT;
+		String messageBody = EMAIL_BODY.replaceAll("URL_PLACEHOLDER",
+				balloonCookie.getUrl());
+
+		InternetAddress addressFrom = new InternetAddress(FROM_ADDRESS);
+
+		String[] receipients = new String[] { balloonCookie.getEmail() };
+		InternetAddress[] addressTo = new InternetAddress[receipients.length];
+		for (int i = 0; i < receipients.length; i++) {
+			addressTo[i] = new InternetAddress(receipients[i]);
+		}
+
+		InternetAddress replyTo[] = { new InternetAddress(FROM_ADDRESS) };
+
+		MailEngine.send(addressFrom, addressTo, null, null, null,
+				messageSubject, messageBody, false, replyTo, null, null);
+
+		session.setAttribute(EMAIL_SENT, true);
+
+	}
+
+	private final static String EMAIL_SUBJECT = "Help find the winner of MIT's Climate CoLab Grand Prize!";
+	private final static String EMAIL_BODY = "<p>Here is your unique link, which you can use to tell your friends and family about the MIT Climate CoLab contest:</p>\n\n<p><a href=\"URL_PLACEHOLDER\">URL_PLACEHOLDER</a></p>\n\n<p>Remember: If your sharing eventually leads to the winner of the $10,000 Grand Prize, you\'ll win some of the $2,000 referral prize, even if that person is up to 10 degrees of separation from you!</p>\n\n<p>For your convenience, we\'ve included a blurb about the contest below for you to send to your friends, but you can spread the word any way you\'d like (just remember to include your unique link, so you can win the referral prize!)</p>\n\n<p><strong>MIT\'s Climate CoLab: Your ideas can help combat climate change, and you might win $10,000</strong>\nAt MIT\u2019s Climate CoLab you can work with people from all over the world to develop ideas for what we can actually do about climate change.</p>\n \n<p>If you submit one of the winning ideas, you\u2019ll be able to present it to the media, government officials, business executives, and scientists at an MIT conference on November 6-7, where a grand prize of $10,000 will be awarded.</p>\n\n \n<p>Even if you don\u2019t have new ideas yourself, you can help improve other people\u2019s ideas and support the ones you find most promising.  And that\'s not all: If you refer one of your friends or colleagues to the contest via Facebook, Twitter or e-mail, and one of them\u2014or a friend of a friend, or friend of a friend of a friend, etc.\u2014wins the Grand Prize, you\u2019ll receive a share of a $2,000 referral prize!</p>\n \n<p>Current contests address low-carbon energy, building efficiency, adaptation, geoengineering, and many other topics. Entries are due May 31.</p>\n \n<p>Can crowdsourcing save the planet? Join the crowd and find out at http://climatecolab.org</p>";
+
+	private final static String FROM_ADDRESS = "no-reply@climatecolab.org";
+	private final static String EMAIL_SENT = BalloonBean.class.getName()
+			+ "EMAIL_SENT";
+
 	public void ensureSignedInUserHasExpandoValue() throws Exception {
-		//check if logged in
+		// check if logged in
 		try {
-			if(Long.parseLong(Helper.getLiferayUserId()) < 1) {
+			if (Long.parseLong(Helper.getLiferayUserId()) < 1) {
 				return;
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			return;
 		}
-		
-		//set user id in expando
+
+		// set user id in expando
 		if (balloonCookie != null
 				&& StringUtils.isNotBlank(balloonCookie.getUuid())) {
 			// add user id to expando table to track his registration
