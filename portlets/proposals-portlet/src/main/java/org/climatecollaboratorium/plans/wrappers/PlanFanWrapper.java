ac7d04e1e8600bf60e8dbc76d8eb40f605e73113
pdeboer <pdeboer5@gmail.com>
2013-06-06 13:35:45 -0400

    planfan serializable

--- portlets/proposals-portlet/src/main/java/org/climatecollaboratorium/plans/wrappers/PlanFanWrapper.java
+++ portlets/proposals-portlet/src/main/java/org/climatecollaboratorium/plans/wrappers/PlanFanWrapper.java
@@ -1,12 +1,18 @@
 package org.climatecollaboratorium.plans.wrappers;
 
+import java.io.Serializable;
+
 import com.ext.portlet.model.PlanFan;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.service.UserLocalServiceUtil;
 
-public class PlanFanWrapper {
-    private PlanFan wrapped;
+public class PlanFanWrapper implements Serializable {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private PlanFan wrapped;
     
     public PlanFanWrapper(PlanFan wrapped) {
         this.wrapped = wrapped;
