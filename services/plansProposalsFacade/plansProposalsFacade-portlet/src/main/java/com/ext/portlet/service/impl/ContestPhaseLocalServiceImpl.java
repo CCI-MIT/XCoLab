e51895467bc0a3890605968e984edff9db318d13
JanuszPa <janusz.parfieniuk@gmail.com>
2013-06-12 19:51:27 +0200

    Fixes to autopromote functionality

--- services/plansProposalsFacade/plansProposalsFacade-portlet/src/main/java/com/ext/portlet/service/impl/ContestPhaseLocalServiceImpl.java
+++ services/plansProposalsFacade/plansProposalsFacade-portlet/src/main/java/com/ext/portlet/service/impl/ContestPhaseLocalServiceImpl.java
@@ -19,49 +19,60 @@ import com.ext.portlet.service.PlanItemLocalServiceUtil;
 import com.ext.portlet.service.base.ContestPhaseLocalServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 
 /**
  * The implementation of the contest phase local service.
- *
+ * 
  * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.ext.portlet.service.ContestPhaseLocalService} interface.
- *
+ * All custom service methods should be put in this class. Whenever methods are
+ * added, rerun ServiceBuilder to copy their definitions into the
+ * {@link com.ext.portlet.service.ContestPhaseLocalService} interface.
+ * 
  * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can only
+ * be accessed from within the same VM.
  * </p>
- *
+ * 
  * @author Brian Wing Shun Chan
  * @see com.ext.portlet.service.base.ContestPhaseLocalServiceBaseImpl
  * @see com.ext.portlet.service.ContestPhaseLocalServiceUtil
  */
-public class ContestPhaseLocalServiceImpl
-    extends ContestPhaseLocalServiceBaseImpl {
+public class ContestPhaseLocalServiceImpl extends ContestPhaseLocalServiceBaseImpl {
     /*
      * NOTE FOR DEVELOPERS:
-     *
-     * Never reference this interface directly. Always use {@link com.ext.portlet.service.ContestPhaseLocalServiceUtil} to access the contest phase local service.
+     * 
+     * Never reference this interface directly. Always use {@link
+     * com.ext.portlet.service.ContestPhaseLocalServiceUtil} to access the
+     * contest phase local service.
      */
+    private final static Log _log = LogFactoryUtil.getLog(ContestPhaseLocalServiceImpl.class);
 
     public List<PlanItem> getPlans(ContestPhase contestPhase) throws SystemException, PortalException {
-        return PlanItemLocalServiceUtil.getPlans(Collections.emptyMap(),Collections.emptyMap(),0L,contestPhase.getContestPhasePK(),
-                0,Integer.MAX_VALUE,"","",false);
-        //return PlanItemLocalServiceUtil.getPlansInContestPhase(this);
+        return PlanItemLocalServiceUtil.getPlans(Collections.emptyMap(), Collections.emptyMap(), 0L,
+                contestPhase.getContestPhasePK(), 0, Integer.MAX_VALUE, "", "", false);
+        // return PlanItemLocalServiceUtil.getPlansInContestPhase(this);
     }
 
     public ContestStatus getContestStatus(ContestPhase contestPhase) throws SystemException, PortalException {
-        String status = ContestPhaseTypeLocalServiceUtil.getContestPhaseType(contestPhase.getContestPhaseType()).getStatus();
-        return status == null?null:ContestStatus.valueOf(status);
+        String status = ContestPhaseTypeLocalServiceUtil.getContestPhaseType(contestPhase.getContestPhaseType())
+                .getStatus();
+        return status == null ? null : ContestStatus.valueOf(status);
     }
-    
+
     public String getContestStatusStr(ContestPhase contestPhase) throws SystemException, PortalException {
-        String status = ContestPhaseTypeLocalServiceUtil.getContestPhaseType(contestPhase.getContestPhaseType()).getStatus();
+        String status = ContestPhaseTypeLocalServiceUtil.getContestPhaseType(contestPhase.getContestPhaseType())
+                .getStatus();
         return status;
     }
 
     public List<String> getPhaseColumns(ContestPhase contestPhase) throws SystemException {
         List<String> ret = new ArrayList<String>();
-        for (ContestPhaseColumn phaseColumn: ContestPhaseColumnLocalServiceUtil.getPhaseColumns(contestPhase.getContestPhasePK())) {
+        for (ContestPhaseColumn phaseColumn : ContestPhaseColumnLocalServiceUtil.getPhaseColumns(contestPhase
+                .getContestPhasePK())) {
             ret.add(phaseColumn.getColumnName());
         }
         return ret;
@@ -71,11 +82,10 @@ public class ContestPhaseLocalServiceImpl
         return ContestPhaseColumnLocalServiceUtil.getPhaseColumns(contestPhase.getContestPhasePK());
     }
 
-
     public List<ContestPhase> getPreviousPhases(ContestPhase contestPhase) throws SystemException, PortalException {
         List<ContestPhase> phases = ContestPhaseLocalServiceUtil.getPhasesForContest(getContest(contestPhase));
         List<ContestPhase> ret = new ArrayList<ContestPhase>();
-        for (ContestPhase phase: phases) {
+        for (ContestPhase phase : phases) {
             if (phase.getPhaseStartDate().before(contestPhase.getPhaseStartDate())) {
                 ret.add(phase);
             }
@@ -85,7 +95,7 @@ public class ContestPhaseLocalServiceImpl
 
     public ContestPhase getNextContestPhase(ContestPhase contestPhase) throws SystemException, PortalException {
         boolean currentFound = false;
-        for (ContestPhase phase: ContestPhaseLocalServiceUtil.getPhasesForContest(getContest(contestPhase))) {
+        for (ContestPhase phase : ContestPhaseLocalServiceUtil.getPhasesForContest(getContest(contestPhase))) {
             if (currentFound) {
                 return phase;
             }
@@ -97,7 +107,7 @@ public class ContestPhaseLocalServiceImpl
     }
 
     public boolean getPhaseActive(ContestPhase contestPhase) {
-        if (contestPhase.getPhaseActiveOverride()) { 
+        if (contestPhase.getPhaseActiveOverride()) {
             return contestPhase.getPhaseActiveOverride();
         }
         if (contestPhase.getPhaseInactiveOverride()) {
@@ -110,75 +120,74 @@ public class ContestPhaseLocalServiceImpl
         return false;
     }
 
-    
+    public List<ContestPhase> getPhasesForContest(Contest contest) throws SystemException {
+        return contestPhasePersistence.findByContestId(contest.getContestPK());
+    }
 
-   public List<ContestPhase> getPhasesForContest(Contest contest) throws SystemException {
-       return contestPhasePersistence.findByContestId(contest.getContestPK());
-   }
+    public ContestPhase getActivePhaseForContest(Contest contest) throws NoSuchContestPhaseException, SystemException {
+        Date now = new Date();
+        try {
+            contestPhasePersistence.findByPhaseActiveOverride_Last(contest.getContestPK(), true,
+                    new OrderByComparator() {
 
-   public ContestPhase getActivePhaseForContest(Contest contest) throws NoSuchContestPhaseException, SystemException {
-       Date now = new Date();
-       try {
-           contestPhasePersistence.findByPhaseActiveOverride_Last(contest.getContestPK(), true, new OrderByComparator() {
-               
-               private final String[] ORDERY_BY_FIELDS = new String[] {"PhaseStartDate"};
-               @Override
-            public String[] getOrderByFields() {
-                   return ORDERY_BY_FIELDS;
+                        private final String[] ORDERY_BY_FIELDS = new String[] { "PhaseStartDate" };
+
+                        @Override
+                        public String[] getOrderByFields() {
+                            return ORDERY_BY_FIELDS;
+                        }
+
+                        @Override
+                        public int compare(Object arg0, Object arg1) {
+                            if (((ContestPhase) arg0).getPhaseStartDate().before(
+                                    ((ContestPhase) arg1).getPhaseStartDate())) {
+                                return -1;
+                            }
+                            return 1;
+                        }
+                    });
+        } catch (NoSuchContestPhaseException e) {
+            // ignore
+        }
+        return contestPhasePersistence.findByContestIdStartEnd(contest.getContestPK(), now, now);
+    }
+
+    /** from ContestPhaseImpl **/
+
+    public Contest getContest(ContestPhase contestPhase) throws SystemException, PortalException {
+        return ContestLocalServiceUtil.getContest(contestPhase.getContestPK());
+    }
+
+    public String getName(ContestPhase contestPhase) throws PortalException, SystemException {
+        return ContestPhaseTypeLocalServiceUtil.getContestPhaseType(contestPhase.getContestPhaseType()).getName();
+    }
+
+    /**
+     * Method responsible for autopromotion of proposals between phases.
+     * 
+     * @throws SystemException
+     * @throws PortalException
+     */
+    public void autoPromoteProposals() throws SystemException, PortalException {
+        Date now = new Date();
+        for (ContestPhase phase : contestPhasePersistence.findByPhaseAutopromote("PROMOTE")) {
+            if (phase.getPhaseEndDate() != null && phase.getPhaseEndDate().before(now) && !getPhaseActive(phase)) {
+                // we have a candidate for promotion, find next phase
+                try {
+                    ContestPhase nextPhase = getNextContestPhase(phase);
+                    PlanItemLocalServiceUtil.promotePlans(phase.getContestPhasePK(), nextPhase.getContestPhasePK());
+
+                    // update phase for which promotion was done (mark it as
+                    // "promotion done")
+                    phase.setContestPhaseAutopromote("PROMOTE_DONE");
+                    updateContestPhase(phase);
+                } catch (SystemException | PortalException e ) {
+                    _log.error("Exception thrown when doing autopromotion for phase " + phase.getContestPhasePK(), e);
+                    continue;
+                }
             }
+        }
 
-            @Override
-               public int compare(Object arg0, Object arg1) {
-                   if (((ContestPhase) arg0).getPhaseStartDate().before(((ContestPhase) arg1).getPhaseStartDate())) {
-                       return -1;
-                   }
-                   return 1;
-               }
-           });
-       }
-       catch (NoSuchContestPhaseException e) {
-           // ignore
-       }
-       return contestPhasePersistence.findByContestIdStartEnd(contest.getContestPK(), now, now);
-   }
-
-
-   /** from ContestPhaseImpl **/
-
-
-   public Contest getContest(ContestPhase contestPhase) throws SystemException, PortalException {
-       return ContestLocalServiceUtil.getContest(contestPhase.getContestPK());
-   }
-   
-   public String getName(ContestPhase contestPhase) throws PortalException, SystemException {
-       return ContestPhaseTypeLocalServiceUtil.getContestPhaseType(contestPhase.getContestPhaseType()).getName();
-   }
-   
-   /**
-    * Method responsible for autopromotion of proposals between phases. 
-    * 
-    * @throws SystemException
-    * @throws PortalException 
-    */
-   public void autoPromoteProposals() throws SystemException, PortalException {
-       Date now = new Date();
-       for (ContestPhase phase: contestPhasePersistence.findByPhaseAutopromote("PROMOTE")) {
-           if (phase.getPhaseEndDate() != null && phase.getPhaseEndDate().before(now) && ! getPhaseActive(phase)) {
-               // we have a candidate for promotion, find next phase
-               
-               ContestPhase nextPhase = getNextContestPhase(phase);
-               
-               PlanItemLocalServiceUtil.promotePlans(phase.getContestPhasePK(), nextPhase.getContestPhasePK());
-               
-               // update phase for which promotion was done (mark it as "promotion done") 
-               phase.setContestPhaseAutopromote("PROMOTE_DONE");
-               updateContestPhase(phase);   
-           }
-       }
-       
-       
-   }
-   
-   
+    }
 
 }
