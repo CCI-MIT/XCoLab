90cea28bc50ccee288443a5fd5374827c24ac609
JanuszPa <janusz.parfieniuk@gmail.com>
2013-08-12 12:19:14 +0200

    Link to user profile on account creation activity entry

--- portlets/loginregister-portlet/src/main/java/org/xcolab/portlets/loginregister/activity/LoginRegisterActivityKeys.java
+++ portlets/loginregister-portlet/src/main/java/org/xcolab/portlets/loginregister/activity/LoginRegisterActivityKeys.java
@@ -1,24 +1,30 @@
 package org.xcolab.portlets.loginregister.activity;
 
+import com.ext.portlet.community.CommunityUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.User;
+import com.liferay.portlet.social.model.SocialActivity;
 
 public enum LoginRegisterActivityKeys {
     USER_REGISTERED(1, "New account created", "%s joined ClimateColab community");
-    
+    private static Log _log = LogFactoryUtil.getLog(LoginRegisterActivityKeys.class);
+
     private final int type;
     private final String title;
     private final String body;
-    
-    
+
     private LoginRegisterActivityKeys(int type, String title, String body) {
         this.type = type;
         this.title = title;
         this.body = body;
     }
-    
+
     public static LoginRegisterActivityKeys getForType(int type) {
-        for (LoginRegisterActivityKeys key: LoginRegisterActivityKeys.values()) {
-            if (key.type == type) 
+        for (LoginRegisterActivityKeys key : LoginRegisterActivityKeys.values()) {
+            if (key.type == type)
                 return key;
         }
         return null;
@@ -29,11 +35,22 @@ public enum LoginRegisterActivityKeys {
     }
 
     public String getTitle(User user) {
-        return String.format(title, user.getScreenName());
+        return String.format(title, getUserLink(user));
     }
 
     public String getBody(User user) {
-        return String.format(body, user.getScreenName());
+        return String.format(body, getUserLink(user));
     }
-    
+
+    private String getUserLink(User user) {
+        String result = "<user removed>";
+        try {
+            return CommunityUtil.generateUserURL(user.getUserId());
+        } catch (PortalException e) {
+            _log.info(e.getMessage());
+        } catch (SystemException e) {
+            _log.info(e.getMessage());
+        }
+        return result;
+    }
 }
