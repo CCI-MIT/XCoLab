74392db7f10619cd38f6646f6d2164df1e337722
pdeboer <pdeboer5@gmail.com>
2013-10-08 06:26:25 -0400

    judging functions

--- portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/requests/UpdateProposalDetailsBean.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/requests/UpdateProposalDetailsBean.java
@@ -11,18 +11,18 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 
 public class UpdateProposalDetailsBean {
-    private Map<Long, String> sectionsContent = new HashMap<Long, String>();  
+    private Map<Long, String> sectionsContent = new HashMap<Long, String>();
     private String pitch;
-    
+
     @NotBlank
     private String name;
-    
-    
-    private String team; 
+
+
+    private String team;
     private long imageId;
-    
+
     public UpdateProposalDetailsBean(ProposalWrapper proposal) throws PortalException, SystemException {
-        for (ProposalSectionWrapper section: proposal.getSections()) {
+        for (ProposalSectionWrapper section : proposal.getSections()) {
             sectionsContent.put(section.getSectionDefinitionId(), section.getContent());
         }
         pitch = proposal.getPitch();
@@ -33,15 +33,19 @@ public class UpdateProposalDetailsBean {
 
     public UpdateProposalDetailsBean() {
     }
+
     public Map<Long, String> getSectionsContent() {
         return sectionsContent;
     }
+
     public void setSectionsContent(Map<Long, String> sectionsContent) {
         this.sectionsContent = sectionsContent;
     }
+
     public String getPitch() {
         return pitch;
     }
+
     public void setPitch(String pitch) {
         this.pitch = pitch;
     }
@@ -69,7 +73,6 @@ public class UpdateProposalDetailsBean {
     public void setImageId(long imageId) {
         this.imageId = imageId;
     }
-    
-    
-    
+
+
 }
