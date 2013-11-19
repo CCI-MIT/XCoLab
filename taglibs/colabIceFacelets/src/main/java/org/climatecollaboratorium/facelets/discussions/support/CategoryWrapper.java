396b9cda522d4499e605467c401a1da60a762433
Patrick de Boer <pdeboer@mit.edu>
2013-11-19 16:28:10 -0500

    dependency

--- taglibs/colabIceFacelets/src/main/java/org/climatecollaboratorium/facelets/discussions/support/CategoryWrapper.java
+++ taglibs/colabIceFacelets/src/main/java/org/climatecollaboratorium/facelets/discussions/support/CategoryWrapper.java
@@ -10,9 +10,9 @@ import java.util.List;
 import javax.faces.component.UIInput;
 import javax.faces.event.ActionEvent;
 
+import com.ext.portlet.DiscussionActivityKeys;
 import org.climatecollaboratorium.facelets.discussions.DiscussionBean;
 import org.climatecollaboratorium.facelets.discussions.ThreadSortClumns;
-import org.climatecollaboratorium.facelets.discussions.activity.DiscussionActivityKeys;
 import org.climatecollaboratorium.utils.ContentFilterHelper;
 import org.climatecollaboratorium.utils.Helper;
 import org.climatecollaboratorium.utils.HumanTime;
@@ -136,7 +136,7 @@ public class CategoryWrapper implements Serializable {
             ThemeDisplay td = Helper.getThemeDisplay();
             SocialActivityLocalServiceUtil.addActivity(td.getUserId(), td.getScopeGroupId(),
                     DiscussionCategoryGroup.class.getName(), discussionBean.getDiscussionId(), 
-                    DiscussionActivityKeys.ADD_CATEGORY.id(), 
+                    DiscussionActivityKeys.ADD_CATEGORY.id(),
                     ActivityUtil.getExtraDataForIds(wrapped.getCategoryId()), 0);
         }
     }
