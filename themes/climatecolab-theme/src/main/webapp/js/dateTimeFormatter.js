041731657bc80435f1280a695d27643a56833d2e
Patrick Hiesel <patrick@hiesel.net>
2013-11-19 16:04:05 -0500

    added tags to time zone customization

--- themes/climatecolab-theme/src/main/webapp/js/dateTimeFormatter.js
+++ themes/climatecolab-theme/src/main/webapp/js/dateTimeFormatter.js
@@ -8,13 +8,15 @@ var dateTimeFormatter = {
         return moment.unix(unixEpoch / 1000).format("MM/DD/YYYY");
     },
 
+    dateText: function (unixEpoch){
+        return moment.unix(unixEpoch / 1000).format("MMM. DD, YYYY");
+    },
+
         time: function (unixEpoch){
         return moment.unix(unixEpoch / 1000).format("hh:mm A") + ' ' + dateTimeFormatter.getZoneAsCode();
     },
-
         getZoneAsCode: function (){
         var paren = new Date().toString().match(/\(.+\)/);
         return paren ? paren[0].match(/([A-Z])/g).join("") : "";
     }
 };
-
