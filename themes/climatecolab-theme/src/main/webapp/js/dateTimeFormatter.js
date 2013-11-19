b1ad24769114f997c03ded53d030bea2dfff6616
Patrick Hiesel <patrick@hiesel.net>
2013-11-19 15:21:26 -0500

    added global datetime formatter according to user's system's locale

---
+++ themes/climatecolab-theme/src/main/webapp/js/dateTimeFormatter.js
@@ -0,0 +1,20 @@
+// namespace = dateTimeFormatter
+var dateTimeFormatter = {
+        dateTime: function (unixEpoch){
+        return moment.unix(unixEpoch / 1000).format("MM/DD/YYYY hh:mm A") + ' ' + dateTimeFormatter.getZoneAsCode();
+    },
+
+        date: function (unixEpoch){
+        return moment.unix(unixEpoch / 1000).format("MM/DD/YYYY");
+    },
+
+        time: function (unixEpoch){
+        return moment.unix(unixEpoch / 1000).format("hh:mm A") + ' ' + dateTimeFormatter.getZoneAsCode();
+    },
+
+        getZoneAsCode: function (){
+        var paren = new Date().toString().match(/\(.+\)/);
+        return paren ? paren[0].match(/([A-Z])/g).join("") : "";
+    }
+};
+
