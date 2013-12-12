28e9b9526913cd66e8f662632e34eca7b71fed40
Patrick Hiesel <patrick@hiesel.net>
2013-12-12 17:00:21 -0500

    improved counting

--- portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/utils/ProposalPickerFilterUtil.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/utils/ProposalPickerFilterUtil.java
@@ -2,10 +2,9 @@ package org.xcolab.portlets.proposals.utils;
 
 import com.ext.portlet.NoSuchProposalContestPhaseAttributeException;
 import com.ext.portlet.ProposalAttributeKeys;
-import com.ext.portlet.service.PlanSectionDefinitionLocalServiceUtil;
-import com.ext.portlet.service.ProposalLocalServiceUtil;
-import com.ext.portlet.service.Proposal2PhaseLocalServiceUtil;
-import com.ext.portlet.service.ProposalContestPhaseAttributeLocalServiceUtil;
+import com.ext.portlet.model.FocusArea;
+import com.ext.portlet.model.OntologyTerm;
+import com.ext.portlet.service.*;
 
 
 import java.util.ArrayList;
@@ -82,8 +81,26 @@ public enum ProposalPickerFilterUtil {
     ONTOLOGY(new ProposalPickerFilter() {
         @Override
         public void filter(List<Pair<Proposal, Date>> proposals, Object additionalFilterCriterion) {
-            long sectionId = Long.parseLong((String) additionalFilterCriterion);
-            /* ToDo */
+            long sectionId = (Long) additionalFilterCriterion;
+            /*
+            try{
+                // section definition Id
+                long definitionId = PlanSectionLocalServiceUtil.getPlanSection(sectionId).getPlanSectionDefinitionId();
+                // FocusArea
+                FocusArea fa = PlanSectionDefinitionLocalServiceUtil.getFocusArea(PlanSectionDefinitionLocalServiceUtil.getPlanSectionDefinition(definitionId));
+                // List of terms in this area
+                List<OntologyTerm> terms = FocusAreaLocalServiceUtil.getTerms(fa);
+                // get all children
+                for (OntologyTerm ot : terms){
+                    List<OntologyTerm> childTerms = OntologyTermLocalServiceUtil.getChildTerms(ot);
+                    for (OntologyTerm child : terms){
+                        // now what?
+                    }
+                }
+            } catch (Exception e){
+
+            }
+        */
         }
     });
 
