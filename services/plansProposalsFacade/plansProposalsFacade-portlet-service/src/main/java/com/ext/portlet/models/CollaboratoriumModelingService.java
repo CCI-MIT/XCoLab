b2a43542167a44c5a19ed0f9cd9701b92cd3ba77
janusz <janusz.parfieniuk@gmail.com>
2014-01-12 12:42:26 +0100

    Async modeling service initialization

--- services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/com/ext/portlet/models/CollaboratoriumModelingService.java
+++ services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/com/ext/portlet/models/CollaboratoriumModelingService.java
@@ -6,47 +6,62 @@
 
 package com.ext.portlet.models;
 
-import com.ext.PropertiesUtils;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.PropsUtil;
-import com.liferay.util.portlet.PortletProps;
 
 import edu.mit.cci.roma.client.Simulation;
 import edu.mit.cci.roma.client.comm.ClientRepository;
- 
+
 public class CollaboratoriumModelingService {
 
-    static ClientRepository instance;
+	static ClientRepository instance;
+	static Object mutex = new Object();
 
-    private static Log _log = LogFactoryUtil.getLog(CollaboratoriumModelingService.class);
+	private static Log _log = LogFactoryUtil
+			.getLog(CollaboratoriumModelingService.class);
 
-    public static ClientRepository repository() throws SystemException {
-        if (instance == null) {
+	public static ClientRepository repository() throws SystemException {
+		if (instance == null) {
 
-            // try to read configuration from default location (portal-ext.properties)
-            String host = PropsUtil.get("edu.mit.roma.address");
-            if (host == null) {
-                _log.error("Can't find edu.mit.roma.address property, it has to be set in portal-ext.properties");
-                throw new SystemException("Error initializing modeling service client: Can't find " +
-                		"edu.mit.roma.address property, it has to be set in portal-ext.properties");
-            }
-           
-             _log.info("Starting up modeling client ("+host+")");
-            try {
-                instance = ClientRepository.instance(host);
+			synchronized (mutex) {
 
-                for (Simulation s : ClientRepository.instance().getAllSimulations()) {
-                    _log.info("Loaded... " + s.getName());
-                }
-                _log.info("Modeling client initialized");
-            } catch (Exception e) {
-                _log.error(e);
-                throw new SystemException("Error initializing modeling service client", e);
-            }
+				if (instance == null) {
+					// start in separate thread as container can block waiting
+					// for this and if there is no roma-server available it will
+					// block forever
+					new Thread(new Runnable() {
 
-        }
-        return instance;
-    }
+						@Override
+						public void run() {
+							// TODO Auto-generated method stub
+							// try to read configuration from default location
+							// (portal-ext.properties)
+							String host = PropsUtil.get("edu.mit.roma.address");
+							if (host == null) {
+								_log.error("Can't find edu.mit.roma.address property, it has to be set in portal-ext.properties");
+							}
+
+							_log.info("Starting up modeling client (" + host
+									+ ")");
+							try {
+								instance = ClientRepository.instance(host);
+
+								for (Simulation s : ClientRepository.instance()
+										.getAllSimulations()) {
+									_log.info("Loaded... " + s.getName());
+								}
+								_log.info("Modeling client initialized");
+							} catch (Exception e) {
+								_log.error(e);
+							}
+						}
+					}).start();
+				}
+			}
+
+		}
+		return instance;
+	}
 }
