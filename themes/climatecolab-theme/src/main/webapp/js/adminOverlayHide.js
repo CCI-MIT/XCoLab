549a8ee33c77d0d6ea2ee5dceeba232ccfbc20de
Klemens Mang <klemens.mang@me.com>
2014-02-26 14:28:33 -0500

    template update to LR62

---
+++ themes/climatecolab-theme/src/main/webapp/js/adminOverlayHide.js
@@ -0,0 +1,27 @@
+
+$( document ).ready(function() {
+    setIconHelpHandler();
+    hideAdminOverlay();
+});
+
+function hideAdminOverlay() {
+    var childs = $('.portlet-topper-toolbar').children();
+    if (childs.length == 0) {
+        $('.portlet-topper h1').hide();
+    }
+}
+
+function setIconHelpHandler() {
+    // Get all the help icons
+    var elements = $('.taglib-icon-help');
+    // hide the help text initially
+    elements.find('.hide-accessible').hide();
+
+    elements.mouseover(function() {
+        $(this).find('.hide-accessible').show();
+    });
+
+    elements.mouseleave(function() {
+        $(this).find('.hide-accessible').hide();
+    });
+}
