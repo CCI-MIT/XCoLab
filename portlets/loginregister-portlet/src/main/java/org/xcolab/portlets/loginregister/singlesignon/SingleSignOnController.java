8439ab11609c385c76e4afbc7e5adcb79142e6f1
Klemens Mang <klemens.mang@me.com>
2014-02-28 12:00:57 -0500

    SSO hashing fix

--- portlets/loginregister-portlet/src/main/java/org/xcolab/portlets/loginregister/singlesignon/SingleSignOnController.java
+++ portlets/loginregister-portlet/src/main/java/org/xcolab/portlets/loginregister/singlesignon/SingleSignOnController.java
@@ -1,5 +1,7 @@
 package org.xcolab.portlets.loginregister.singlesignon;
 
+import com.ext.utils.authentication.AuthenticationService;
+import com.ext.utils.authentication.service.AuthenticationServiceUtil;
 import com.liferay.portal.NoSuchUserException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -8,9 +10,10 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.model.User;
-import com.liferay.portal.security.pwd.BasePasswordEncryptor;
+import com.liferay.portal.security.pwd.PasswordEncryptorUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PropsUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,8 +28,6 @@ import java.io.IOException;
 public class SingleSignOnController {
 
     private final static Log _log = LogFactoryUtil.getLog(SingleSignOnController.class);
-
-    private static final String SHA_ALGORITHM_PREFIX = "{SHA-1}";
 
     @RequestMapping(params = "action=provideSSOCredentials")
     public void linkUser(ActionRequest request, Model model, ActionResponse response,
@@ -44,55 +45,41 @@ public class SingleSignOnController {
             return;
         }
 
-        String encrAlgorithm = PropsUtil.get(PropsKeys.PASSWORDS_ENCRYPTION_ALGORITHM);
-        _log.info("Encryption algorithm: " + encrAlgorithm);
+        PortletSession portletSession = request.getPortletSession();
 
-        // Statically instantiate the PBKDF2 pw encryptor. Please change this when Liferay decides to change the password encryption
-        // scheme again...
-        String encrPassword = u.getPassword();
+        try {
+            AuthenticationServiceUtil.logUserIn(request, response, u.getScreenName(), password);
+            u = (User)request.getAttribute(WebKeys.USER);
+            if (u != null) {
+                String fbIdString = (String) portletSession.getAttribute("FACEBOOK_USER_ID",PortletSession.APPLICATION_SCOPE);
+                String openId = (String) portletSession.getAttribute("SSO_OPENID_ID",PortletSession.APPLICATION_SCOPE);
 
-        PasswordEncryptor encryptor;
-        // SHA hash
-        if (encrPassword.contains(SHA_ALGORITHM_PREFIX)) {
-            final int startIdx = encrPassword.indexOf(SHA_ALGORITHM_PREFIX);
-            encrPassword = encrPassword.substring(startIdx + SHA_ALGORITHM_PREFIX.length());
+                if (Validator.isNotNull(fbIdString)){
+                    // update FB credentials
+                    long fbId = Long.parseLong(fbIdString);
+                    u.setFacebookId(fbId);
+                    UserLocalServiceUtil.updateUser(u);
+                    response.sendRedirect(themeDisplay.getURLHome());
+                } else if (Validator.isNotNull(openId)){
+                    u.setOpenId(openId);
+                    UserLocalServiceUtil.updateUser(u);
+                    portletSession.setAttribute("OPEN_ID_LOGIN",new Long(u.getUserId()), PortletSession.APPLICATION_SCOPE);
+                    response.sendRedirect(themeDisplay.getURLHome());
+                }
+                response.setRenderParameter("error", "true");
+                response.setRenderParameter("SSO", "general");
+                request.setAttribute("error","An unknown error occured.");
 
-            encryptor = new SSHAPasswordEncryptor();
-        } else {
-            encryptor = new PBKDF2PasswordEncryptor();
+                return;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        _log.error("encr. password: " + u.getPassword());
-        String encPassword = encryptor.encrypt(password, u.getPassword());
-
         // passwords don't match
-        if (!encPassword.equalsIgnoreCase(u.getPassword())) {
             response.setRenderParameter("status", "registerOrLogin");
             response.setRenderParameter("SSO", "general");
             request.setAttribute("credentialsError",true);
-            return;
-        }
-
-        PortletSession portletSession = request.getPortletSession();
-
-        String fbIdString = (String) portletSession.getAttribute("FACEBOOK_USER_ID",PortletSession.APPLICATION_SCOPE);
-        String openId = (String) portletSession.getAttribute("SSO_OPENID_ID",PortletSession.APPLICATION_SCOPE);
-
-        if (Validator.isNotNull(fbIdString)){
-            // update FB credentials
-                long fbId = Long.parseLong(fbIdString);
-                u.setFacebookId(fbId);
-                UserLocalServiceUtil.updateUser(u);
-                response.sendRedirect(themeDisplay.getURLHome());
-        } else if (Validator.isNotNull(openId)){
-            u.setOpenId(openId);
-            UserLocalServiceUtil.updateUser(u);
-            portletSession.setAttribute("OPEN_ID_LOGIN",new Long(u.getUserId()), PortletSession.APPLICATION_SCOPE);
-            response.sendRedirect(themeDisplay.getURLHome());
-        }
-        response.setRenderParameter("error", "true");
-        response.setRenderParameter("SSO", "general");
-        request.setAttribute("error","An unknown error occured.");
     }
 
     @RequestMapping(params = "error=true")
