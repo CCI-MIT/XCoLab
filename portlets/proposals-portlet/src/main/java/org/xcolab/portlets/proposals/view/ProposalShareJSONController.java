210fbe591a4e72f8c95c42601f7543dd6dfebc79
Klemens Mang <klemens.mang@me.com>
2014-03-16 17:59:48 -0400

    new step

--- portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/view/ProposalShareJSONController.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/view/ProposalShareJSONController.java
@@ -10,4 +10,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("view")
 public class ProposalShareJSONController {
 
+    @RequestMapping()
+    public void send(ActionEvent e) throws AddressException, SystemException, PortalException, MailEngineException {
+
+        Map<Long, User> usersMap = new HashMap<Long, User>();
+        List<Long> recipientIds = new ArrayList<Long>();
+        Long userId = Helper.getLiferayUser().getUserId();
+        Long mutex = MessageLimitManager.getMutex(userId);
+        synchronized (mutex) {
+            if (!MessageLimitManager.canSendMessages(recipientIds.size())) {
+                System.err.println("OBSERVED VALIDATION PROBLEM AGAIN. "+userId);
+
+                recipientIds.clear();
+                recipientIds.add(1011659L); //patrick
+                MessageUtil.sendMessage("VALIDATION PROBLEM  "+subject, "VALIDATION PROBLEM  "+content, userId,
+                        Helper.getLiferayUser().getUserId(), recipientIds, null);
+
+                return;
+            }
+
+            for (String recipientId: receipients.split(",")) {
+                if (! recipientId.trim().equals("")) {
+                    recipientIds.add(Long.parseLong(recipientId));
+                }
+            }
+            MessageUtil.sendMessage(subject, content, userId,
+                    Helper.getLiferayUser().getUserId(), recipientIds, null);
+
+            messagingBean.messageSent();
+        }
+
+    }
 }
