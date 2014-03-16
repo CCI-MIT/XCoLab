9c19159c0a937e6434cc2783c760ff23f4623e7a
Klemens Mang <klemens.mang@me.com>
2014-03-16 17:36:33 -0400

    Working on task

---
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/view/ProposalShareJSONController.java
@@ -0,0 +1,13 @@
+package org.xcolab.portlets.proposals.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Mente on 16.03.14.
+ */
+@Controller
+@RequestMapping("view")
+public class ProposalShareJSONController {
+
+}
