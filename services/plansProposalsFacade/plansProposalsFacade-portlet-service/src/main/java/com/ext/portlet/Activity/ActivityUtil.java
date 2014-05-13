14eab292fca72aba2c1cf342c199d96dbf32a4c6
Klemens Mang <klemens.mang@me.com>
2014-05-13 10:24:44 -0400

    Refactored ActivityUtil class

--- services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/com/ext/portlet/Activity/ActivityUtil.java
+++ services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/com/ext/portlet/Activity/ActivityUtil.java
@@ -6,21 +6,9 @@
 
 package com.ext.portlet.Activity;
 
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.*;
 
-import com.ext.portlet.model.ActivitySubscription;
-import com.ext.portlet.service.ActivitySubscriptionLocalServiceUtil;
-import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
-import com.liferay.portal.kernel.dao.orm.DynamicQuery;
-import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.OrderFactoryUtil;
-import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
@@ -39,72 +27,21 @@ import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.SortFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
-import com.liferay.portlet.social.NoSuchActivityException;
 import com.liferay.portlet.social.model.SocialActivity;
 import com.liferay.portlet.social.service.SocialActivityLocalServiceUtil;
-import com.liferay.portlet.social.service.persistence.SocialActivityUtil;
-
-import javax.management.Query;
 
 public class ActivityUtil {
 
     private static Log _log = LogFactoryUtil.getLog(ActivityUtil.class);
 
-    private final static String encoding = "UTF-8";
-
     public static final long AGGREGATION_TIME_WINDOW = 1000 * 60 * 60 * 1l; // 1h
 
     private static final long DEFAULT_COMPANY_ID = 10112L;
 
-    private static SubscriptionProvider provider = new SubscriptionProvider() {
-
-        public void createSubscription(String portlet, long userid, long entityid, int type) throws SystemException {
-            ActivityUtil.createSubscription(portlet, userid, entityid, type);
-
-        }
-
-        public void deleteSubscription(String portlet, long userid, long entityid, int type) throws SystemException {
-            ActivityUtil.deleteSubscription(portlet, userid, entityid, type);
-
-        }
-
-        @Override
-        public boolean isSubscribed(String portlet, long userid, long entityid, int type) throws SystemException {
-            return ActivityUtil.isSubscribed(portlet, userid, entityid, type);
-        }
-
-    };
-
-    public static SubscriberFactory getAssociatedSubscriptionFactory(ActivitySubscription a) {
-        /*
-        int activity = Integer.parseInt(a.getActivitytype());
-		if ("plans".equals(a.getPortletId())){
-			return PlanActivityKeys.fromId(activity);
-		} else if ("debates".equals(a.getPortletId())) {
-			return DebateActivityKeys.fromId(activity);
-		}
-		*/
-        return null;
-
-    }
-
-    public static Map<String, CompositeSubscriptionHolder> getCompositeSubscriptions(long userId) throws SystemException {
-        List<ActivitySubscription> subscriptions = ActivitySubscriptionLocalServiceUtil.findByUser(userId);
-        Map<String, CompositeSubscriptionHolder> subs = new HashMap<String, CompositeSubscriptionHolder>();
-        for (ActivitySubscription sub : subscriptions) {
-            String hash = CompositeSubscriptionHolder.createHash(sub);
-            if (!subs.containsKey(hash)) {
-                subs.put(hash, new CompositeSubscriptionHolder(sub));
-            } else {
-                subs.get(hash).addSubscription(sub);
-            }
-        }
-        return subs;
-    }
+    private static final String ADMINISTRATOR_ROLE_NAME = "Administrator";
 
     public static List<SocialActivity> retrieveAllActivities(int pagestart, int next) throws SystemException {
 
@@ -113,41 +50,31 @@ public class ActivityUtil {
         return activities;
     }
 
-    public static List<SocialActivity> retrieveWindowedActivities(int pagestart, int next) throws SystemException, PortalException {
-    	return retrieveWindowedActivities(pagestart, next, false);
+    public static List<SocialActivity> retrieveWindowedActivities(int start, int end) throws SystemException, PortalException {
+    	return retrieveWindowedActivities(start, end, false);
     }
     
-    public static List<SocialActivity> retrieveWindowedActivities(int pagestart, int next, boolean showAdmin) throws SystemException, PortalException {
+    public static List<SocialActivity> retrieveWindowedActivities(int start, int end, boolean showAdmin) throws SystemException, PortalException {
         Hits hits;
     	if (showAdmin) {
-            hits = getActivitySearchResults(pagestart, next);
+            hits = getAllAggregatedActivitySearchResults(start, end);
     	}
     	else {
-            Role r = RoleLocalServiceUtil.getRole(10112L,"Administrator");
-            
-            List<Long> administratorsIds = new ArrayList<Long>();
-            for (long userId: UserLocalServiceUtil.getRoleUserIds(r.getRoleId())) {
-            	administratorsIds.add(userId);
-            }
-    		hits = getActivitySearchResults(administratorsIds, pagestart, next);
+            List<Long> administratorsIds = getAdministratorIds();
+    		hits = getExcludedUserAggregatedActivitySearchResults(administratorsIds, start, end);
     	}
 
-        List<SocialActivity> aggregatedSocialActivities = new ArrayList<>(hits.getLength());
-        for (Document activityDoc : hits.getDocs()) {
-            try {
-                SocialActivity sa = SocialActivityLocalServiceUtil.getSocialActivity(GetterUtil.getLong(activityDoc.getField("activityId").getValue()));
-                aggregatedSocialActivities.add(sa);
-            } catch (Exception e) {
-                _log.error(e);
-            }
-        }
-
-		return aggregatedSocialActivities;
+        return retrieveAggregatedSocialActivities(hits);
     }
 
-    public static List<SocialActivity> retrieveWindowedActivities(long userId, int pagestart, int next) throws SystemException, PortalException {
-        Hits hits = getActivitySearchResults(userId, pagestart, next);
+    public static List<SocialActivity> retrieveWindowedActivities(long userId, int start, int end) throws SystemException, PortalException {
+        Hits hits = getAggregatedActivitySearchResults(userId, start, end);
+        return retrieveAggregatedSocialActivities(hits);
+    }
+
+    private static List<SocialActivity> retrieveAggregatedSocialActivities(Hits hits) {
         List<SocialActivity> aggregatedSocialActivities = new ArrayList<>(hits.getLength());
+
         for (Document activityDoc : hits.getDocs()) {
             try {
                 SocialActivity sa = SocialActivityLocalServiceUtil.getSocialActivity(GetterUtil.getLong(activityDoc.getField("activityId").getValue()));
@@ -178,42 +105,15 @@ public class ActivityUtil {
             activitiesMap.get(getSocialActivityKey(a)).add(a);
         }
 
-        //cluster
-        List<SocialActivity> aggregatedActivities = new LinkedList<>();
-        Comparator<SocialActivity> sorter = new Comparator<SocialActivity>() {
-            @Override
-            public int compare(SocialActivity o1, SocialActivity o2) {
-                return new Long(o1.getCreateDate()).compareTo(o2.getCreateDate());
-            }
-        };
-        for (Collection<SocialActivity> sal : activitiesMap.values()) {
-            List<SocialActivity> ascending = new ArrayList<>(sal); //convert to array for sorting
-            Collections.sort(ascending, sorter);
+        List<SocialActivity> aggregatedActivities = clusterActivities(activitiesMap);
 
-            SocialActivity curMin = null;
-            for (SocialActivity sa : ascending) {
-                if (curMin == null || sa.getCreateDate() - curMin.getCreateDate() < AGGREGATION_TIME_WINDOW) curMin = sa;
-                else {
-                    aggregatedActivities.add(curMin);
-                    curMin = sa;
-                }
-            }
-            aggregatedActivities.add(curMin);
-        }
-        //horrible code start
-        Collections.sort(aggregatedActivities, sorter);
-        Collections.reverse(aggregatedActivities);
-        //horrible code end
 
         return aggregatedActivities;
     }
 
-    private static String getSocialActivityKey(SocialActivity sa) {
-        return sa.getClassNameId() + "_" + sa.getClassPK() + "_" + sa.getType() + "_" + sa.getUserId();
-    }
 
     public static int getAllActivitiesCount() throws SystemException, PortalException {
-        int searchResultCount = getActivitySearchResults(QueryUtil.ALL_POS, QueryUtil.ALL_POS).getLength();
+        int searchResultCount = getAllAggregatedActivitySearchResults(QueryUtil.ALL_POS, QueryUtil.ALL_POS).getLength();
         if (searchResultCount == 0) {
             return groupAllActivities().size();
         }
@@ -221,144 +121,15 @@ public class ActivityUtil {
         return searchResultCount;
     }
 
-    public static void deleteSubscription(String portlet, long userid, long entityid, int type) throws SystemException {
-	    /*
-		ActivitySubscriptionPK subscriptionPK = new ActivitySubscriptionPK();
-		subscriptionPK.setEntityId(entityid);
-		subscriptionPK.setReceiverId(userid);
-		subscriptionPK.setActivitytype(type+"");
-		subscriptionPK.setPortletId(portlet);
-        ActivitySubscription subscription = null;
-        try {
-            subscription = ActivitySubscriptionLocalServiceUtil
-                    .getActivitySubscription(subscriptionPK);
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            _log.error(e);
-        }
-        if (subscription!=null) {
-            ActivitySubscriptionLocalServiceUtil.deleteActivitySubscription(subscription);
-        }
-		*/
-    }
-
-    public static boolean isSubscribed(String portlet, long userid, long entityid, int type) throws SystemException {
-        /*
-        ActivitySubscriptionPK subscriptionPK = new ActivitySubscriptionPK();
-		subscriptionPK.setPortletId(portlet);
-        subscriptionPK.setEntityId(entityid);
-		subscriptionPK.setReceiverId(userid);
-		subscriptionPK.setActivitytype(type+"");
-        try {
-            ActivitySubscriptionLocalServiceUtil.getActivitySubscription(subscriptionPK);
-        } catch (Exception e) {
-           return false;
-        }
-		return true;
-        */
-        return false;
-
-    }
-
     public static int getActivitiesCount(long userId) throws SystemException, PortalException {
 
-        int searchResultCount = getActivitySearchResults(userId, QueryUtil.ALL_POS, QueryUtil.ALL_POS).getLength();
+        int searchResultCount = getAggregatedActivitySearchResults(userId, QueryUtil.ALL_POS, QueryUtil.ALL_POS).getLength();
         if (searchResultCount == 0) {
             return groupUserActivities(userId).size();
         }
 
         return searchResultCount;
     }
-
-
-    public static void addSubscription(SubscriberFactory key, long userid, long entityid) throws SystemException {
-        key.subcribe(userid, entityid, provider);
-    }
-
-    public static boolean isSubscribed(SubscriberFactory key, long userid, long entityid) throws SystemException {
-        return key.isSubscribed(userid, entityid, provider);
-    }
-
-    public static void removeSubscription(SubscriberFactory key, long userid, long entityid) throws SystemException {
-        key.unsubcribe(userid, entityid, provider);
-    }
-
-    public static void createSubscription(String portlet, long userid, long entityid, int type) throws SystemException {
-		/*
-		
-		ActivitySubscriptionPK subscriptionPK= new ActivitySubscriptionPK();
-		subscriptionPK.setEntityId(entityid);
-		subscriptionPK.setReceiverId(userid);
-		subscriptionPK.setActivitytype(type+"");
-		subscriptionPK.setPortletId(portlet);
-		ActivitySubscription subscription = null;
-		try {
-			subscription = ActivitySubscriptionLocalServiceUtil.getActivitySubscription(subscriptionPK);
-		} catch (PortalException e) {
-			//not found, no problem
-		}
-		if (subscription != null) {
-			return;
-		}
-		subscription= ActivitySubscriptionLocalServiceUtil.createActivitySubscription(subscriptionPK);
-		subscription.setPortletId(portlet);
-		subscription.setCreateDate(new Date());
-		subscription.setModifiedDate(new Date());
-		ActivitySubscriptionLocalServiceUtil.addActivitySubscription(subscription);
-		*/
-    }
-
-
-    public static void addDefaultSubscriptions(long userid) throws SystemException {
-		/*for (SubscriberFactory factory:ActivityConstants.SUBS_DEFAULTS) {
-			factory.subcribe(userid,-1, provider);
-		}
-		*/
-    }
-
-    public static void clearSubscriptions(long userId) throws SystemException {
-        List<ActivitySubscription> subs = ActivitySubscriptionLocalServiceUtil.findByUser(userId);
-        for (ActivitySubscription sub : subs) {
-            ActivitySubscriptionLocalServiceUtil.deleteActivitySubscription(sub);
-        }
-    }
-
-    public static String encodeMap(Map<String, Serializable> map) {
-        StringBuilder encodedString = new StringBuilder();
-        for (String attribute : map.keySet()) {
-            try {
-                encodedString.append(URLEncoder.encode(attribute, encoding));
-                encodedString.append("=");
-                encodedString.append(URLEncoder.encode(map.get(attribute).toString(), encoding));
-                encodedString.append("&");
-            } catch (UnsupportedEncodingException e) {
-                _log.error(e);
-            }
-        }
-        return encodedString.toString();
-    }
-
-    public static Map<String, String> decodeMap(String encodedMap) {
-        Map<String, String> decodedMap = new HashMap<String, String>();
-        String[] parameters = encodedMap.split("&");
-        for (String parameter : parameters) {
-            if (parameter.trim().equals("")) {
-                continue;
-            }
-            String[] parts = parameter.split("=");
-            if (parts.length != 2) {
-                _log.error("Invalid parameter " + parts);
-            }
-            try {
-                decodedMap.put(URLDecoder.decode(parts[0], encoding), URLDecoder.decode(parts[1], encoding));
-            } catch (UnsupportedEncodingException e) {
-                _log.error(e);
-            }
-        }
-
-        return decodedMap;
-    }
-
 
     public static String getExtraDataForIds(Long... ids) {
         StringBuilder sb = new StringBuilder();
@@ -388,9 +159,44 @@ public class ActivityUtil {
         return ret;
     }
 
-    private static Hits getActivitySearchResults(long userId, int start, int end) throws SearchException {
+    private static List<SocialActivity> clusterActivities(Map<String, List<SocialActivity>> activitiesMap) {
+        //cluster
+        List<SocialActivity> aggregatedActivities = new LinkedList<>();
+        Comparator<SocialActivity> sorter = new Comparator<SocialActivity>() {
+            @Override
+            public int compare(SocialActivity o1, SocialActivity o2) {
+                return new Long(o1.getCreateDate()).compareTo(o2.getCreateDate());
+            }
+        };
+        for (Collection<SocialActivity> activitiesMapValue : activitiesMap.values()) {
+            List<SocialActivity> socialActivities = new ArrayList<>(activitiesMapValue); //convert to array for sorting
+            Collections.sort(socialActivities, sorter);
+
+            SocialActivity curMin = null;
+            for (SocialActivity socialActivity : socialActivities) {
+                if (curMin == null || socialActivity.getCreateDate() - curMin.getCreateDate() < AGGREGATION_TIME_WINDOW) {
+                    curMin = socialActivity;
+                }
+                else {
+                    aggregatedActivities.add(curMin);
+                    curMin = socialActivity;
+                }
+            }
+            aggregatedActivities.add(curMin);
+        }
+
+        Collections.sort(aggregatedActivities, Collections.reverseOrder(sorter));
+
+        return aggregatedActivities;
+    }
+
+    private static String getSocialActivityKey(SocialActivity sa) {
+        return sa.getClassNameId() + "_" + sa.getClassPK() + "_" + sa.getType() + "_" + sa.getUserId();
+    }
+
+    private static Hits getAggregatedActivitySearchResults(long userId, int start, int end) throws SearchException {
         SearchContext context = new SearchContext();
-        context.setCompanyId(10112L);
+        context.setCompanyId(DEFAULT_COMPANY_ID);
         BooleanQuery query = BooleanQueryFactoryUtil.create(context);
         query.addRequiredTerm(Field.ENTRY_CLASS_NAME, SocialActivity.class.getName());
 
@@ -407,13 +213,13 @@ public class ActivityUtil {
         return SearchEngineUtil.search(SearchEngineUtil.getDefaultSearchEngineId(), context.getCompanyId(), query, sort, start, end);
     }
 
-    private static Hits getActivitySearchResults(int start, int end) throws SearchException {
-        return getActivitySearchResults(new ArrayList<Long>(), start, end);
+    private static Hits getAllAggregatedActivitySearchResults(int start, int end) throws SearchException {
+        return getExcludedUserAggregatedActivitySearchResults(new ArrayList<Long>(), start, end);
     }
 
-    private static Hits getActivitySearchResults(List<Long> excludedUserIds, int start, int end) throws SearchException {
+    private static Hits getExcludedUserAggregatedActivitySearchResults(List<Long> excludedUserIds, int start, int end) throws SearchException {
         SearchContext context = new SearchContext();
-        context.setCompanyId(10112L);
+        context.setCompanyId(DEFAULT_COMPANY_ID);
         BooleanQuery query = BooleanQueryFactoryUtil.create(context);
         query.addRequiredTerm(Field.ENTRY_CLASS_NAME, SocialActivity.class.getName());
 
@@ -432,4 +238,14 @@ public class ActivityUtil {
         return SearchEngineUtil.search(SearchEngineUtil.getDefaultSearchEngineId(), context.getCompanyId(), query, sort, start, end);
     }
 
+    private static List<Long> getAdministratorIds() throws SystemException, PortalException {
+        Role r = RoleLocalServiceUtil.getRole(DEFAULT_COMPANY_ID, ADMINISTRATOR_ROLE_NAME);
+
+        List<Long> administratorsIds = new ArrayList<Long>();
+        for (long userId: UserLocalServiceUtil.getRoleUserIds(r.getRoleId())) {
+            administratorsIds.add(userId);
+        }
+
+        return administratorsIds;
+    }
 }
