8b7fb2f956a58a4ab7d6b1adbde804518bd0b5e0
JanuszP <janusz.parfieniuk@gmail.com>
2014-08-08 06:32:53 -0400

    clean after deployment to preserve disk space

--- portlets/deplyALL.sh
+++ portlets/deplyALL.sh
@@ -2,7 +2,7 @@ for D in *; do
     if [ -d "${D}" ]; then
         cd $D
                 pwd
-                mvn clean compile package liferay:deploy
+                mvn clean compile package liferay:deploy clean
                 cd ..
     fi
 done 
