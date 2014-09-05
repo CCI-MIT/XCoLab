19bf3bea8d5a4084c6f25557cf71faeff3373079
ManuelThurner <m.l.thurner@gmail.com>
2014-09-04 20:11:07 -0400

    Unit Testing Points: Setup contests and proposals and subproposal links

--- services/plansProposalsFacade/plansProposalsFacade-portlet/src/test/java/com/ext/portlet/service/impl/PointsLocalServiceImplTest.java
+++ services/plansProposalsFacade/plansProposalsFacade-portlet/src/test/java/com/ext/portlet/service/impl/PointsLocalServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.ext.portlet.service.impl;
 
 
+import com.ext.portlet.ProposalAttributeKeys;
 import com.ext.portlet.model.*;
 import com.ext.portlet.service.*;
 import com.liferay.portal.dao.jdbc.DataSourceFactoryImpl;
@@ -35,6 +36,7 @@ public class PointsLocalServiceImplTest {
 	private static Proposal2PhaseLocalService proposal2PhaseLocalService;
     private static ProposalContestPhaseAttributeLocalService proposalContestPhaseAttributeLocalService;
 	private static PointsLocalService pointsLocalService;
+	private static PlanSectionDefinitionLocalService planSectionDefinitionLocalService;
 
     private static SimpleDateFormat dateFormat;
 
@@ -66,6 +68,7 @@ public class PointsLocalServiceImplTest {
         proposal2PhaseLocalService = (Proposal2PhaseLocalService) PortalBeanLocatorUtil.locate(Proposal2PhaseLocalService.class.getName());
         proposalContestPhaseAttributeLocalService = (ProposalContestPhaseAttributeLocalService) PortalBeanLocatorUtil.locate(ProposalContestPhaseAttributeLocalService.class.getName());
         pointsLocalService = (PointsLocalService) PortalBeanLocatorUtil.locate(PointsLocalService.class.getName());
+        planSectionDefinitionLocalService = (PlanSectionDefinitionLocalService) PortalBeanLocatorUtil.locate(PlanSectionDefinitionLocalService.class.getName());
 	}
 
     @Test
@@ -73,25 +76,88 @@ public class PointsLocalServiceImplTest {
         long authorId = 10144L;
         PermissionCheckerUtil.setThreadValues(UserLocalServiceUtil.getUser(authorId));
 
-
-        //create contest
-        Contest c = contestLocalService.createNewContest(authorId, "Test-Global-Contest");
+        //create global contest
+        Contest globalContest = contestLocalService.createNewContest(authorId, "Test-Global-Contest");
+        globalContest.setPoints(10000);
+        globalContest.setDefaultParentPointType(1);
+        contestLocalService.updateContest(globalContest);
         //create phases
-        ContestPhase cp1 = createContestPhase(c, 1, false, "PROMOTE_DONE",  "2014-08-01 00:00:00", "2014-08-10 00:00:00");
-        ContestPhase cp2 = createContestPhase(c, 16, true, "PROMOTE_DONE",  "2014-08-10 00:00:01", "2014-08-14 00:00:00");
-        ContestPhase cp3 = createContestPhase(c, 18, false, "PROMOTE_DONE", "2014-08-14 00:00:01", "2014-08-16 00:00:00");
-        ContestPhase cp4 = createContestPhase(c, 19, true, "PROMOTE_DONE",  "2014-08-16 00:00:01", "2014-08-20 00:00:00");
-        ContestPhase cp5 = createContestPhase(c, 15, false, "PROMOTE_DONE", "2014-08-20 00:00:01", "2015-09-24 00:00:00");
+        ContestPhase gCp1 = createContestPhase(globalContest, 1, false, "PROMOTE_DONE",  "2014-08-01 00:00:00", "2014-08-10 00:00:00");
+        ContestPhase gCp2 = createContestPhase(globalContest, 16, true, "PROMOTE_DONE",  "2014-08-10 00:00:01", "2014-08-14 00:00:00");
+        ContestPhase gCp3 = createContestPhase(globalContest, 18, false, "PROMOTE_DONE", "2014-08-14 00:00:01", "2014-08-16 00:00:00");
+        ContestPhase gCp4 = createContestPhase(globalContest, 19, true, "PROMOTE_DONE",  "2014-08-16 00:00:01", "2014-08-20 00:00:00");
+        ContestPhase gCp5 = createContestPhase(globalContest, 15, false, "PROMOTE_DONE", "2014-08-20 00:00:01", "2015-09-24 00:00:00");
         //this contest did not expire yet.
-        ContestPhase cp6 = createContestPhase(c, 17, false, "", "2015-09-24 00:00:01", null);
+        ContestPhase gCp6 = createContestPhase(globalContest, 17, false, "", "2015-09-24 00:00:01", null);
 
         //create 10 proposals, advance half of them to the last phase.
-        List<Proposal> proposals = new ArrayList<Proposal>();
+        List<Proposal> globalProposals = new ArrayList<Proposal>();
         for (int i = 0; i < 10; i++) {
-            Proposal p = proposalLocalService.create(authorId, cp1.getContestPhasePK());
-            proposals.add(p);
+            Proposal proposal = proposalLocalService.create(authorId, gCp1.getContestPhasePK());
+            globalProposals.add(proposal);
+
+            //copy to first phases
+            copyProposalToPhase(proposal, gCp2);
+            //copy half of the proposals to other phases
+            if (i > 4) {
+                copyProposalToPhase(proposal, gCp3);
+                copyProposalToPhase(proposal, gCp4);
+                copyProposalToPhase(proposal, gCp5);
+            }
         }
 
+        //create side contests
+        List<Proposal> sideProposals = new ArrayList<Proposal>();
+        List<Contest> sideContests = new ArrayList<Contest>();
+        for (int i = 0; i < 2; i++) {
+            sideContests.add(contestLocalService.createNewContest(authorId, "Test-Side-Contest-"+(i+1)));
+            //create phases
+            ContestPhase sCp1 = createContestPhase(globalContest, 1, false, "PROMOTE_DONE", "2014-08-01 00:00:00", "2014-08-10 00:00:00");
+            ContestPhase sCp2 = createContestPhase(globalContest, 16, true, "PROMOTE_DONE", "2014-08-10 00:00:01", "2014-08-14 00:00:00");
+            ContestPhase sCp3 = createContestPhase(globalContest, 18, false, "PROMOTE_DONE", "2014-08-14 00:00:01", "2014-08-16 00:00:00");
+            ContestPhase sCp4 = createContestPhase(globalContest, 19, true, "PROMOTE_DONE", "2014-08-16 00:00:01", "2014-08-20 00:00:00");
+            ContestPhase sCp5 = createContestPhase(globalContest, 15, false, "PROMOTE_DONE", "2014-08-20 00:00:01", "2015-09-24 00:00:00");
+            //this contest did not expire yet.
+            ContestPhase sCp6 = createContestPhase(globalContest, 17, false, "", "2015-09-24 00:00:01", null);
+
+            for (int j = 0; j < 3; i++) {
+                Proposal proposal = proposalLocalService.create(authorId, gCp1.getContestPhasePK());
+                sideProposals.add(proposal);
+
+                //copy to first phases
+                copyProposalToPhase(proposal, gCp2);
+                //copy half of the proposals to other phases
+                if (i > 4) {
+                    copyProposalToPhase(proposal, gCp3);
+                    copyProposalToPhase(proposal, gCp4);
+                    copyProposalToPhase(proposal, gCp5);
+                }
+            }
+        }
+
+        //create some links from global proposals to side proposals
+        String sectionText = "These are the subproposals we link to:\n"+
+                "http://127.0.0.1:8080/web/guest/plans/-/plans/contestId/"+sideContests.get(0).getContestPK()+"/planId/"+sideProposals.get(0).getProposalId()+"\n\n"+
+                "http://127.0.0.1:8080/web/guest/plans/-/plans/contestId/"+sideContests.get(0).getContestPK()+"/planId/"+sideProposals.get(1).getProposalId()+"\n\n"+
+                "http://127.0.0.1:8080/web/guest/plans/-/plans/contestId/"+sideContests.get(1).getContestPK()+"/planId/"+sideProposals.get(4).getProposalId()+" and "+
+                "http://127.0.0.1:8080/web/guest/plans/-/plans/contestId/"+sideContests.get(1).getContestPK()+"/planId/"+sideProposals.get(5).getProposalId()+" and "+
+                "http://127.0.0.1:8080/web/guest/plans/-/plans/contestId/"+globalContest.getContestPK()+"/planId/"+globalProposals.get(3).getProposalId();
+        //1300907 is the sub proposal plan section definition
+        proposalLocalService.setAttribute(authorId, globalProposals.get(6).getProposalId(), ProposalAttributeKeys.SECTION, 1300907L, sectionText);
+
+        //set some point distributions
+
+        //test the hypothetical points distribution now.
+
+        //assert the points
+
+    }
+
+    private void copyProposalToPhase(Proposal p, ContestPhase cp) throws SystemException {
+        Proposal2Phase p2p = proposal2PhaseLocalService.create(p.getProposalId(), cp.getContestPhasePK());
+        p2p.setVersionFrom(1);
+        p2p.setVersionTo(1);
+        proposal2PhaseLocalService.updateProposal2Phase(p2p);
     }
 
     private ContestPhase createContestPhase(Contest c, long type, boolean fellowScreeningActive, String autoPromote, String startDate, String endDate) throws SystemException, ParseException {
