f24359e617abe99d678ae6ae76791784f5dfa2c3
ManuelThurner <m.l.thurner@gmail.com>
2014-12-02 14:14:29 -0500

    adapt orphaned proposal finder

--- other/tools/orphaned-proposal-sections-finder.rb
+++ other/tools/orphaned-proposal-sections-finder.rb
@@ -9,28 +9,33 @@ contests_affected = $client.query('
 SELECT ContestPK
 FROM xcolab_Contest c
 WHERE EXISTS(
-SELECT DISTINCT proposalId
-FROM xcolab_ProposalAttribute
-WHERE
-additionalId NOT IN (
-SELECT DISTINCT pts.planSectionId
-FROM xcolab_PlanTemplateSection pts, xcolab_Contest co
-WHERE pts.plantemplateId = co.planTemplateId AND
-co.ContestPK = c.ContestPK
-)
-AND
-proposalId IN (
-SELECT DISTINCT p2p.proposalId
-FROM
-xcolab_Proposal2Phase p2p,
-xcolab_ContestPhase cp
-WHERE
-p2p.contestphaseId = cp.contestphasepk AND
-cp.ContestPk = c.ContestPK
-)
-AND additionalId != 0
-AND name = "SECTION"
+    SELECT DISTINCT proposalId
+    FROM xcolab_ProposalAttribute
+    WHERE
+      additionalId NOT IN (
+        SELECT DISTINCT pts.planSectionId
+        FROM
+            xcolab_PlanTemplateSection pts,
+            xcolab_Contest co
+        WHERE
+            pts.plantemplateId = co.planTemplateId AND
+            co.ContestPK = c.ContestPK
+        )
+        AND
+        proposalId IN (
+          SELECT DISTINCT p2p.proposalId
+          FROM
+              xcolab_Proposal2Phase p2p,
+              xcolab_ContestPhase cp
+          WHERE
+              p2p.contestphaseId = cp.contestphasepk AND
+              cp.ContestPk = c.ContestPK
+      )
+      AND additionalId != 0
+      AND name = "SECTION"
 )')
+
+proposals_affected = {}
 
 contests_affected.each do |contest_id|
   c = contest_id["ContestPK"]
@@ -55,18 +60,36 @@ contests_affected.each do |contest_id|
 			p2p.contestphaseId = cp.contestphasepk AND
 			cp.ContestPk = #{c}
 		)
-        AND additionalId != 0
-        AND name = 'SECTION'
+    AND additionalId != 0
+    AND name = 'SECTION'
 GROUP BY
   proposalId, additionalId
 	")
 
   puts "Contest "+c.to_s+": "+proposals.count.to_s+" proposal/section pairs affected"
   puts ""
-  puts "Proposals and their planTemplateSectionIds which do not exist in the plan template:"
+  #puts "Proposals and their planTemplateSectionIds which do not exist in the plan template:"
+  puts "Proposals which have orphaned planTemplateIds and have never been moved:"
   proposals.each do |p|
-    puts "pId: "+p["proposalId"].to_s+", sectionId: "+p["additionalId"].to_s+", maxV: "+p["version"].to_s+", section-string-length: "+p["stringValue"].length.to_s
+    #find out if proposal was ever moved to another contest (= there are contest phases of at least two different contests)
+    contest_count = $client.query("
+      SELECT COUNT(DISTINCT ContestPK) AS count
+      FROM xcolab_Proposal2Phase p2p, xcolab_ContestPhase cp
+      WHERE proposalId = #{p["proposalId"]} AND p2p.contestPhaseId = cp.ContestPhasePK;"
+    )
+    if contest_count.first["count"] < 2
+      puts "pId: "+p["proposalId"].to_s+", sectionId: "+p["additionalId"].to_s+", maxV: "+p["version"].to_s+", section-string-length: "+p["stringValue"].length.to_s
+      proposals_affected[p["proposalId"]] = 1
+    end
+
   end
   puts "---"
   puts ""
+
+
+
 end
+
+puts "TOTAL proposals affected which have not been moved: "+proposals_affected.size.to_s
+
+puts proposals_affected.keys.inspect
