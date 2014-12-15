9d634a92553199d0ee8dcefd7ba6c847cf88e2ff
ManuelThurner <m.l.thurner@gmail.com>
2014-12-15 11:35:35 -0500

    add debug output to the email digest task

--- portlets/proposals-portlet/src/main/java/org/xcolab/utils/SendEmailNotifications.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/utils/SendEmailNotifications.java
@@ -27,31 +27,38 @@ public class SendEmailNotifications implements MessageListener {
 
     @Override
     public void receive(Message message) throws MessageListenerException {
+        System.out.println("digest-debug: SendEmailNotifications line 30 reached");
+
     	String requestUrl = null;
 		try {
 			Company company = CompanyLocalServiceUtil.getCompany(COMPANY_ID);
+            System.out.println("digest-debug: SendEmailNotifications line 35 reached");
 
             // Workaround to get the right port (80) on production
             int port = GetterUtil.getInteger(PortletProps.get(SERVER_PORT_PROPS_KEY));
             if (Validator.isNull(port) || port <= 0) {
                 port = PortalUtil.getPortalPort(false);
             }
+            System.out.println("digest-debug: SendEmailNotifications line 42 reached");
 
             String baseUrl = PortalUtil.getPortalURL(company.getVirtualHostname(), port, false);
-            //String baseUrl = "http://localhost:9082";
+            //String baseUrl = "http://localhost:8080";
 
             requestUrl = baseUrl + EXECUTE_SCHEDULER_PATH;
 			URL url = new URL(baseUrl + EXECUTE_SCHEDULER_PATH);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            System.out.println("digest-debug: SendEmailNotifications line 50 reached");
 
 			con.setRequestMethod("GET");
 			int statusCode = con.getResponseCode();
 
+            System.out.println("digest-debug: SendEmailNotifications line 55 reached, started request to scheduler path. Return status code: "+statusCode+" URL called is: "+baseUrl + EXECUTE_SCHEDULER_PATH);
+
 			if (statusCode != 200) {
 				throw new MessageListenerException(String.format("Could not process request: Bad request: (%s)", requestUrl));
 			}
-
 		} catch (IOException | SystemException | PortalException e) {
+            System.out.println("digest-debug: line 61 reached, exception thrown: "+e.getMessage()+e.getStackTrace());
 			throw new MessageListenerException(String.format("Could not process request (%s)", requestUrl), e);
 		}
 	}
