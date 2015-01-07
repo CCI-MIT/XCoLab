a997f3d7f94c72d209cf02afb25f9a28455cd780
tokn136m <tokn136@gmail.com>
2015-01-07 00:10:26 -0500

    Added team/author to export

--- services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/org/xcolab/utils/judging/ProposalReview.java
+++ services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/org/xcolab/utils/judging/ProposalReview.java
@@ -1,10 +1,17 @@
 package org.xcolab.utils.judging;
 
+import com.ext.portlet.NoSuchProposalAttributeException;
+import com.ext.portlet.ProposalAttributeKeys;
 import com.ext.portlet.model.ContestPhase;
 import com.ext.portlet.model.Proposal;
+import com.ext.portlet.model.ProposalAttribute;
 import com.ext.portlet.model.ProposalRatingType;
+import com.ext.portlet.service.ProposalLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.User;
-
+import com.liferay.portal.service.UserLocalServiceUtil;
+import org.apache.commons.lang.StringUtils;
 import java.util.*;
 
 
@@ -15,8 +22,6 @@ public class ProposalReview {
     private Proposal proposal;
     ContestPhase contestPhase;
     private String proposalUrl;
-
-
 
     private Map<ProposalRatingType, Double> ratingAverages;
     private Map<User, String> reviews;
@@ -159,4 +164,37 @@ public class ProposalReview {
     public void setProposalUrl(String proposalUrl) {
         this.proposalUrl = proposalUrl;
     }
+
+    public String getProposalTeamAuthor(){
+        try {
+            String authorName = getTeamOrNull();
+            if (StringUtils.isBlank(authorName)) {
+                authorName = UserLocalServiceUtil.getUser(proposal.getAuthorId()).getScreenName();
+            }
+            return authorName;
+        }
+        catch(SystemException se){
+        }
+        catch(PortalException pe){
+        }
+        return "";
+
+    }
+
+    private String getTeamOrNull(){
+        try {
+            for (ProposalAttribute attr: ProposalLocalServiceUtil.getAttributes(proposal.getProposalId(), proposal.getCurrentVersion())) {
+                if (attr.getName().equals(ProposalAttributeKeys.TEAM) && attr.getAdditionalId() == 0) {
+                    return attr.getStringValue();
+                }
+            }
+        }
+        catch(SystemException se){
+        }
+        catch(PortalException pe){
+        }
+
+        return null;
+    }
+
 }
