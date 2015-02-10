989fa45ff8a9cdd8109eef24cc8aa27e6a493d57
Patrick de Boer <pdeboer@mit.edu>
2015-02-10 19:22:35 +0100

    find by featured

--- portlets/contests-portlet/src/main/java/org/xcolab/portlets/contests/ContestsController.java
+++ portlets/contests-portlet/src/main/java/org/xcolab/portlets/contests/ContestsController.java
@@ -1,20 +1,18 @@
 package org.xcolab.portlets.contests;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.ext.portlet.model.Contest;
 import com.ext.portlet.service.ContestLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 @Controller
@@ -29,7 +27,7 @@ public class ContestsController {
     public String showContests(PortletRequest request, PortletResponse response, Model model) throws SystemException, PortalException {
     	
         List<ContestWrapper> ret = new ArrayList<>();
-        List<Contest> contests = ContestLocalServiceUtil.findByActiveFlagText(true, "Open");
+        List<Contest> contests = ContestLocalServiceUtil.findByActiveFeatured(true,true);
         Collections.shuffle(contests);
         for (Contest contest: contests) {
             if(ret.size() >= NUM_CONTESTS) {
