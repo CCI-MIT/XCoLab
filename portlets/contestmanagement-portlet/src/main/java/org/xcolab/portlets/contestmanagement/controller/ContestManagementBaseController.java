663f71213373de55da4493c805450bccd49d85e6
Klemens Mang <klemens.mang@me.com>
2015-02-27 16:33:10 +0100

    Refactoring; added focusArea/ontology to create contests functionality

--- portlets/contestmanagement-portlet/src/main/java/org/xcolab/portlets/contestmanagement/controller/ContestManagementBaseController.java
+++ portlets/contestmanagement-portlet/src/main/java/org/xcolab/portlets/contestmanagement/controller/ContestManagementBaseController.java
@@ -1,37 +1,17 @@
 package org.xcolab.portlets.contestmanagement.controller;
 
 import com.ext.portlet.model.Contest;
-import com.ext.portlet.model.ContestSchedule;
-import com.ext.portlet.service.ContestLocalServiceUtil;
-import com.ext.portlet.service.ContestScheduleLocalService;
-import com.ext.portlet.service.ContestScheduleLocalServiceUtil;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.model.Portlet;
 import com.liferay.portal.model.User;
 import com.liferay.portal.security.permission.PermissionChecker;
-import com.liferay.portal.service.permission.PortletPermission;
-import com.liferay.portal.service.permission.PortletPermissionUtil;
 import com.liferay.portal.theme.ThemeDisplay;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
-import org.xcolab.enums.ContestTier;
-import org.xcolab.portlets.contestmanagement.beans.ContestDescriptionBean;
-import org.xcolab.portlets.contestmanagement.wrappers.ContestScheduleWrapper;
+import org.xcolab.portlets.contestmanagement.utils.ContestCreatorUtil;
 
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
-import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -41,12 +21,10 @@ import java.util.Map;
 @RequestMapping("view")
 public class ContestManagementBaseController {
 
-    private static final String[] BASIC_CONTESTS_2015_SHORT_NAMES = new String[]{"Adaptation","Buildings","Energy Supply",
-            "Geoengineering","Industry","Land Use: Agriculture, Forestry, Livestock","Shifting Attitudes & Behavior",
-            "Transportation","US Carbon Price","Waste Management","OIP: Latin America Energy","Somerville: Atypical","Rural resilience"};
+
 
     // TODO: to be done when Laur is ready
-    private static final String[] REGION_CONTESTS_2015_SHORT_NAMES = new String[]{};
+    private static final String REGION_CONTESTS_2015_JSON = "";
 
     @RequestMapping(params = "createContest=true")
     public String createContestController(PortletRequest request, Model model, PortletResponse response) {
@@ -57,7 +35,7 @@ public class ContestManagementBaseController {
             User currentUser = themeDisplay.getUser();
 
             if(!currentUser.isDefaultUser() && portletPermissionChecker.isOmniadmin()) {
-                Contest contest = createNewContest("created contest");
+                Contest contest = ContestCreatorUtil.createNewContest("created contest");
                 String newContestLink = "/web/guest/cms/-/contestmanagement/contestId/" + contest.getContestPK() + "/tab/DESCRIPTION";
                 model.addAttribute("newContestLink", newContestLink);
                 view = "newContestCreated";
@@ -80,51 +58,12 @@ public class ContestManagementBaseController {
 
         if(!currentUser.isDefaultUser() && portletPermissionChecker.isOmniadmin()) {
 
-            Map<String, String> contestEditMap = create2015BasicContests(themeDisplay);
+            Map<String, String> contestEditMap = ContestCreatorUtil.create2015BasicContests(themeDisplay.getPortalURL());
             model.addAttribute("contestEditLinks", contestEditMap);
+            model.addAttribute("success", (contestEditMap != null));
             view = "creationDone";
         }
 
         return view;
-    }
-
-    private Map<String, String> create2015BasicContests(ThemeDisplay themeDisplay) {
-        Map<String, String> contestEditMap = new HashMap<>(BASIC_CONTESTS_2015_SHORT_NAMES.length);
-        for (String contestShortName : BASIC_CONTESTS_2015_SHORT_NAMES) {
-            try {
-                Contest newContest = createNewContest(contestShortName);
-                newContest.setContestTier(ContestTier.BASIC.getTierType());
-
-                // Look for the right contestSchedule
-                for (ContestSchedule schedule : ContestScheduleLocalServiceUtil.getContestSchedules(QueryUtil.ALL_POS, QueryUtil.ALL_POS)) {
-                    if (schedule.getName().equals(ContestScheduleWrapper.CONTEST_SCHEDULE_2015_SECTOR_LABEL)) {
-                        newContest.setContestScheduleId(schedule.getBaseScheduleId());
-                        break;
-                    }
-                }
-
-                newContest.persist();
-
-                String absoluteURL = themeDisplay.getPortalURL() + getContestEditLink(newContest);
-                contestEditMap.put(newContest.getContestShortName(), absoluteURL);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        return contestEditMap;
-    }
-
-    private Contest createNewContest(String contestShortName) throws SystemException, PortalException {
-        Contest contest = ContestLocalServiceUtil.createNewContest(10144L, contestShortName);
-        contest.setContestPrivate(true);
-        contest.persist();
-        // TODO for now there is always a template preselected
-        contest.setPlanTemplateId(102L);
-        return contest;
-    }
-
-    private String getContestEditLink(Contest contest) {
-        return "/web/guest/cms/-/contestmanagement/contestId/" + contest.getContestPK() + "/tab/DESCRIPTION";
     }
 }
--- portlets/contestmanagement-portlet/src/main/webapp/WEB-INF/views/creationDone.jsp
+++ portlets/contestmanagement-portlet/src/main/webapp/WEB-INF/views/creationDone.jsp
@@ -7,12 +7,21 @@
 	xmlns:collab="http://climatecolab.org/tags/collab_1.0"
 	xmlns:portlet="http://java.sun.com/portlet_2_0" version="2.0">
 	<jsp:directive.include file="init.jspx" />
-	Contests were created successfully.
 
-	<table>
-		<tr><th>Contest name</th><th>Contest edit link</th></tr>
-		<c:forEach items="${contestEditLinks}" var="contestEditLink">
-			<tr><td>${contestEditLink.key}</td><td><a href="${contestEditLink.value}">${contestEditLink.value}</a></td></tr>
-		</c:forEach>
-	</table>
+	<c:choose>
+		<c:when test="${success}">
+			<h3>Contests were created successfully.</h3>
+
+			<table>
+				<tr><th>Contest name</th><th>Contest edit link</th></tr>
+				<c:forEach items="${contestEditLinks}" var="contestEditLink">
+					<tr><td>${contestEditLink.key}</td><td><a href="${contestEditLink.value}">${contestEditLink.value}</a></td></tr>
+				</c:forEach>
+			</table>
+		</c:when>
+		<c:otherwise>
+			<h3>Contests could not be created.</h3>
+		</c:otherwise>
+	</c:choose>
+
 </jsp:root>
