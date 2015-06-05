11f69246d4597957ed19c984a5e8fb6581a003f9
tokn136m <tokn136@gmail.com>
2015-06-05 12:36:26 +0200

    hide public ratings in advancing tab

--- portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/view/ProposalJudgesTabController.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/view/ProposalJudgesTabController.java
@@ -49,10 +49,17 @@ public class ProposalJudgesTabController extends BaseProposalTabController {
         model.addAttribute("advanceOptions", JudgingSystemActions.AdvanceDecision.values());
 
 
-        List<ProposalRating> judgeRatingsUnWrapped = ProposalRatingLocalServiceUtil.getFellowRatingsForProposal(proposal.getProposalId(), contestPhase.getContestPhasePK());
-        List<ProposalRatingsWrapper> fellowRatings = wrapProposalRatings(judgeRatingsUnWrapped);
+        List<ProposalRating> fellowRatingsUnWrapped = ProposalRatingLocalServiceUtil.getFellowRatingsForProposal(proposal.getProposalId(), contestPhase.getContestPhasePK());
+        List<ProposalRatingsWrapper> fellowRatings = wrapProposalRatings(fellowRatingsUnWrapped);
 
-        List<ProposalRatingsWrapper> judgeRatings = wrapProposalRatings(ProposalRatingLocalServiceUtil.getJudgeRatingsForProposal(proposal.getProposalId(), contestPhase.getContestPhasePK()));
+        List<ProposalRating> judgesRatingsUnWrapped = ProposalRatingLocalServiceUtil.getJudgeRatingsForProposal(proposal.getProposalId(), contestPhase.getContestPhasePK());
+
+        for (Iterator i = judgesRatingsUnWrapped.iterator(); i.hasNext(); ){
+            ProposalRating judgesRatingUnWrapped = (ProposalRating) i.next();
+            if(judgesRatingUnWrapped.getOnlyForInternalUsage()) i.remove();
+        }
+
+        List<ProposalRatingsWrapper> judgeRatings = wrapProposalRatings(judgesRatingsUnWrapped);
         boolean isFrozen = ProposalContestPhaseAttributeLocalServiceUtil.isAttributeSetAndTrue(
                 proposal.getProposalId(),
                 contestPhase.getContestPhasePK(),
