08a43933319a3cc2c450ca1f09aadcc3c0cbf58c
Beatrice <lamche@mit.edu>
2015-06-18 16:42:49 -0400

    Impact Assessment Fellows are now sometimes abbreviated

--- portlets/members-portlet/src/main/java/org/xcolab/portlets/users/utils/MemberCategory.java
+++ portlets/members-portlet/src/main/java/org/xcolab/portlets/users/utils/MemberCategory.java
@@ -23,7 +23,24 @@ public enum MemberCategory {
     } 
 
     public String getPrintName() {
-        return WordUtils.capitalizeFully((name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase()).replaceAll("_", " "));
+        String printName = WordUtils.capitalizeFully((name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase()).replaceAll("_", " "));
+                if ((printName.length() > 12) && (printName.contains(" "))){
+                    String printNameAbbr ="";
+                    String[] printNameArray = printName.split("\\s+");
+                    int arrayCount = printNameArray.length-1;
+                    for (int i = 0; i < arrayCount; i++) {
+                        printNameAbbr = printNameAbbr + printNameArray[i].substring(0,1);
+                        // You may want to check for a non-word character before blindly
+                        // performing a replacement
+                        // It may also be necessary to adjust the character class
+                        //printNameArray[i] = printNameArray[i].replaceAll("[^\\w]", "");
+                    }
+                    printNameAbbr = printNameAbbr + " " + printNameArray[arrayCount];
+                    return printNameAbbr;
+                }
+        else{
+                    return printName;
+                }
     }
     
     public String getLowerCase() {
