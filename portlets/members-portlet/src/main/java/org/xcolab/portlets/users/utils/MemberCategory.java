7c432e6bad69b0e02dfe7bf4a98d05aa194d9863
Beatrice <lamche@mit.edu>
2015-06-18 19:09:52 -0400

    ENUM tests now for role filtering

--- portlets/members-portlet/src/main/java/org/xcolab/portlets/users/utils/MemberCategory.java
+++ portlets/members-portlet/src/main/java/org/xcolab/portlets/users/utils/MemberCategory.java
@@ -20,27 +20,36 @@ public enum MemberCategory {
     
     MemberCategory(String... roleNames) {
         this.roleNames = roleNames;
-    } 
+    }
 
     public String getPrintName() {
         String printName = WordUtils.capitalizeFully((name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase()).replaceAll("_", " "));
-                if ((printName.length() > 12) && (printName.contains(" "))){
-                    String printNameAbbr ="";
-                    String[] printNameArray = printName.split("\\s+");
-                    int arrayCount = printNameArray.length-1;
-                    for (int i = 0; i < arrayCount; i++) {
-                        printNameAbbr = printNameAbbr + printNameArray[i].substring(0,1);
-                        // You may want to check for a non-word character before blindly
-                        // performing a replacement
-                        // It may also be necessary to adjust the character class
-                        //printNameArray[i] = printNameArray[i].replaceAll("[^\\w]", "");
-                    }
-                    printNameAbbr = printNameAbbr + " " + printNameArray[arrayCount];
-                    return printNameAbbr;
-                }
-        else{
-                    return printName;
-                }
+        if ((printName.length() > 12) && (printName.contains(" "))) {
+            printName = abbreviateName(printName);
+        }
+        return printName;
+    }
+
+    public String abbreviateName(String printName){
+        String printNameAbbr = "";
+        String[] printNameArray = printName.split("\\s+");
+        int arrayCount = printNameArray.length - 1;
+        for (int i = 0; i < arrayCount; i++) {
+            printNameAbbr = printNameAbbr + printNameArray[i].substring(0, 1);
+        }
+        printNameAbbr = printNameAbbr + " " + printNameArray[arrayCount];
+        return printNameAbbr;
+    }
+
+    public String getImageUrl() {
+        if (name() == MemberCategory.IMPACT_ASSESSMENT_FELLOW.name()){
+            return MemberCategory.FELLOW.name().toLowerCase();
+        }
+        else if (name() == MemberCategory.CONTESTMANAGER.name()){
+            return MemberCategory.EXPERT.name().toLowerCase();
+        }
+        else
+            return name().toLowerCase();
     }
     
     public String getLowerCase() {
