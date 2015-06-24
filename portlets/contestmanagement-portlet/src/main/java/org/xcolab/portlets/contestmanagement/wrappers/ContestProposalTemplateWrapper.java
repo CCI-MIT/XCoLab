a832701e1895d900ad1d1e88b17381138a609a37
tokn136m <tokn136@gmail.com>
2015-06-24 21:45:50 +0200

    hibernate issue

--- portlets/contestmanagement-portlet/src/main/java/org/xcolab/portlets/contestmanagement/wrappers/ContestProposalTemplateWrapper.java
+++ portlets/contestmanagement-portlet/src/main/java/org/xcolab/portlets/contestmanagement/wrappers/ContestProposalTemplateWrapper.java
@@ -273,8 +273,6 @@ public class ContestProposalTemplateWrapper {
 
         if(createNew) {
             duplicateExistingPlanTemplate();
-        } else{
-            //deletePlanSectionDefinitionsOfProposalTemplateIfNotUsedInAnotherTemplate();
         }
 
         addSectionsToProposalTemplate();
