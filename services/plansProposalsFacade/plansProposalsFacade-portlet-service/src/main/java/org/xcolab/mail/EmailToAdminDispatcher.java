4b8b11cf8e1e05e9a20e3fbedc90df9a2858d1f6
Klemens Mang <klemens.mang@me.com>
2015-07-30 11:28:20 +0200

    Refactor: Error reporter

---
+++ services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/org/xcolab/mail/EmailToAdminDispatcher.java
@@ -0,0 +1,48 @@
+package org.xcolab.mail;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.util.mail.MailEngine;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+/**
+ * Created by Mente on 29/07/15.
+ */
+public class EmailToAdminDispatcher {
+
+    private static final Log _log = LogFactoryUtil.getLog(EmailToAdminDispatcher.class);
+
+    private static final String[] adminEmailRecipients = new String[] {"pdeboer@mit.edu", "knauert@mit.edu", "mail@klemensmang.com"};
+    private static final String fromEmailAddress = "no-reply@climatecolab.org";
+    private static final String fromEmailName = "MIT Climate CoLab";
+
+    private String subject;
+    private String body;
+
+    public EmailToAdminDispatcher(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public void sendMessage() {
+        try {
+            InternetAddress fromAddress = new InternetAddress(fromEmailAddress, fromEmailName);
+            MailEngine.send(fromAddress, getAllAdminRecipients(), subject, body, true);
+        } catch (Exception e) {
+            _log.error("Could not send error message", e);
+        }
+    }
+
+    private InternetAddress[] getAllAdminRecipients() throws AddressException {
+        InternetAddress[] addressTo = new InternetAddress[adminEmailRecipients.length];
+        for (int i = 0; i < adminEmailRecipients.length; i++) {
+            addressTo[i] = new InternetAddress(adminEmailRecipients[i]);
+        }
+
+        return addressTo;
+    }
+
+
+}
