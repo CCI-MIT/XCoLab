a66b136da137bbefe8e342e3507df1b64197ceff
Klemens Mang <klemens.mang@me.com>
2015-07-30 13:11:01 +0200

    some cleanup for [COLAB-532]

--- portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/exceptions/ProposalIdOrContestIdInvalidException.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/exceptions/ProposalIdOrContestIdInvalidException.java
@@ -1,28 +1,16 @@
 package org.xcolab.portlets.proposals.exceptions;
 
 import com.liferay.portal.kernel.exception.SystemException;
-import org.xcolab.portlets.proposals.wrappers.InvalidUrlErrorWrapper;
 
 /**
  * Created by Mente on 28/07/15.
  */
 public class ProposalIdOrContestIdInvalidException extends SystemException {
-    private InvalidUrlErrorWrapper invalidUrlErrorWrapper;
-
     public ProposalIdOrContestIdInvalidException(Exception exception) {
         super (exception);
-        invalidUrlErrorWrapper = new InvalidUrlErrorWrapper();
     }
 
     public ProposalIdOrContestIdInvalidException() {
-        invalidUrlErrorWrapper = new InvalidUrlErrorWrapper();
-    }
 
-    public void setInvalidUrlErrorWrapper(InvalidUrlErrorWrapper invalidUrlErrorWrapper) {
-        this.invalidUrlErrorWrapper = invalidUrlErrorWrapper;
-    }
-
-    public InvalidUrlErrorWrapper getInvalidUrlErrorWrapper() {
-        return this.invalidUrlErrorWrapper;
     }
 }
