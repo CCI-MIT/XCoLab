8b058b23e1f0702093d1a2a550d921571ea6e98c
Johannes Bachhuber <jobachhu@mit.edu>
2015-08-25 16:45:27 -0400

    Adjusted wording for message notifications

--- services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/com/ext/portlet/messaging/MessageConstants.java
+++ services/plansProposalsFacade/plansProposalsFacade-portlet-service/src/main/java/com/ext/portlet/messaging/MessageConstants.java
@@ -44,14 +44,14 @@ public class MessageConstants {
     public static final String MESSAGE = "Message";
 
 
-    public static final String EMAIL_MESSAGE_SUBJECT = "[Climate Colab] Message from $USER";
-    public static final String EMAIL_MESSAGE_TEMPLATE = "Climate Colab user <b>$USER</b> has sent you " +
+    public static final String EMAIL_MESSAGE_SUBJECT = "[Climate CoLab] Message from $USER";
+    public static final String EMAIL_MESSAGE_TEMPLATE = "The Climate CoLab user <b>$USER</b> has sent you " +
             "the following message:\n<br /><br />" +
             "<b>Subject: $SUBJECT</b>\n" +
             "<br /><br />$MESSAGE<br /><br /> \n" +
             "--------------<br />\n"+
             "<br /><br />Please do not reply to this email. You can view and respond to this message <a href='$URL'>here</a>.<br /><br />" +
-            "(If the above link does not work, please paste the following link directly into your browser.: $URL )";
+            "(If the above link does not work, please paste the following link directly into your browser: $URL )";
 
     public static final String EMAIL_MESSAGE_VAR_USER = "$USER";
     public static final String EMAIL_MESSAGE_VAR_MESSAGE = "$MESSAGE";
