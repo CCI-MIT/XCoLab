72d9a8e70d45c53039b6280e17c213cb6cfd4219
Johannes Bachhuber <jobachhu@mit.edu>
2015-10-29 15:37:41 -0400

    Added debug emails

--- portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/view/action/RemoveUserFromProposalTeamActionController.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/view/action/RemoveUserFromProposalTeamActionController.java
@@ -7,13 +7,18 @@ import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.User;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.xcolab.mail.EmailToAdminDispatcher;
 import org.xcolab.portlets.proposals.exceptions.ProposalsAuthorizationException;
+import org.xcolab.portlets.proposals.permissions.ProposalsPermissions;
 import org.xcolab.portlets.proposals.utils.ProposalsContext;
+import org.xcolab.portlets.proposals.wrappers.ProposalWrapper;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -39,22 +44,45 @@ public class RemoveUserFromProposalTeamActionController {
             throws PortalException, SystemException, ProposalsAuthorizationException {
 
         final Proposal proposal = proposalsContext.getProposal(request);
-        final User user = proposalsContext.getUser(request);
+        final User actingUser = proposalsContext.getUser(request);
         final long proposalId = proposal.getProposalId();
-        final long userId = user.getUserId();
+        final long actingUserId = actingUser.getUserId();
 
-        if (!proposalsContext.getPermissions(request).getCanManageUsers()) {
+        final ProposalsPermissions permissions = proposalsContext.getPermissions(request);
+        if (!permissions.getCanManageUsers()) {
             final String errorMessage = String.format(
                     "User %d does not have the necessary permissions to remove a user from the team of proposal %d",
-                    userId, proposalId);
+                    actingUserId, proposalId);
             _log.error(errorMessage);
             throw new ProposalsAuthorizationException(errorMessage);
         }
         if (memberUserId == proposal.getAuthorId()) {
-            final String errorMessage = String.format("User %d is trying to remove the owner of proposal %d",
-                    userId, proposalId);
+            final String errorMessage = String.format("User %d is trying to remove the owner %d of proposal %d",
+                    actingUserId, memberUserId, proposalId);
             _log.error(errorMessage);
-            throw new ProposalsAuthorizationException(errorMessage);
+            try {
+                throw new ProposalsAuthorizationException(errorMessage);
+            } catch (ProposalsAuthorizationException e) {
+                //TODO: remove debug email
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(String.format("Acting user: %s, %d <br/>", actingUser.getScreenName(), actingUserId));
+                User deletedUser = UserLocalServiceUtil.fetchUser(memberUserId);
+                stringBuilder.append(String.format("Deleted member: %s, %d <br/>", deletedUser.getScreenName(), deletedUser.getUserId()));
+                stringBuilder.append(String.format("Deleted from group: %d <br/>", proposal.getGroupId()));
+                stringBuilder.append(String.format("Proposal: %s, %d <br/>", new ProposalWrapper(proposal).getName(), proposalId));
+                stringBuilder.append("<br/>Acting user permissions: <br/>");
+                stringBuilder.append(String.format("CanManageUsers: %b <br/>", permissions.getCanManageUsers()));
+                stringBuilder.append(String.format("CanAdminProposal: %b <br/>", permissions.getCanAdminProposal()));
+                stringBuilder.append(String.format("CanAdminAll: %b <br/>", permissions.getCanAdminAll()));
+                stringBuilder.append(String.format("CanEdit: %b <br/>", permissions.getCanEdit()));
+                stringBuilder.append(String.format("CanDelete: %b <br/>", permissions.getCanDelete()));
+                stringBuilder.append(String.format("CanFellowActions: %b <br/>", permissions.getCanFellowActions()));
+                stringBuilder.append(String.format("CanJudgeActions: %b <br/>", permissions.getCanJudgeActions()));
+                stringBuilder.append("<br/>Stack trace: <br/>");
+                stringBuilder.append(ExceptionUtils.getStackTrace(e));
+                new EmailToAdminDispatcher(errorMessage, stringBuilder.toString()).sendMessage();
+                throw e;
+            }
         }
 
         ProposalLocalServiceUtil.removeUserFromTeam(proposalId,memberUserId);
