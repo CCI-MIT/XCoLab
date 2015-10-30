d0b6db17fa37188370774ae5270d3f34006d3d33
tokn136m <tokn136@gmail.com>
2015-10-30 09:47:31 +0100

    catch user url generation exception

--- portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/activity/generators/BaseProposalsFeedEntryGenerator.java
+++ portlets/proposals-portlet/src/main/java/org/xcolab/portlets/proposals/activity/generators/BaseProposalsFeedEntryGenerator.java
@@ -1,5 +1,8 @@
 package org.xcolab.portlets.proposals.activity.generators;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
 import org.xcolab.portlets.proposals.activity.ProposalActivityFeedEntryGenerator;
 import org.xcolab.portlets.proposals.utils.ProposalsURLGenerator;
 import org.xcolab.portlets.proposals.wrappers.ProposalWrapper;
@@ -10,28 +13,31 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.User;
-import com.liferay.portal.service.UserLocalServiceUtil;
 import com.liferay.portlet.social.model.SocialActivity;
 
 public abstract class BaseProposalsFeedEntryGenerator implements ProposalActivityFeedEntryGenerator {
-
+    private Log _log = LogFactoryUtil.getLog(BaseProposalsFeedEntryGenerator.class);
     private final static String hyperlink = "<a href=\"%s\">%s</a>";
-    
-    
+
+
     public Proposal getProposal(SocialActivity activity) throws PortalException, SystemException {
         return ProposalLocalServiceUtil.getProposal(activity.getClassPK());
     }
-    
+
     public String getProposalLink(Proposal proposal) throws PortalException, SystemException {
         ProposalWrapper wrapper = new ProposalWrapper(proposal);
-        return String.format(hyperlink,ProposalsURLGenerator.getProposalURL(proposal), wrapper.getName()); 
+        return String.format(hyperlink, ProposalsURLGenerator.getProposalURL(proposal), wrapper.getName());
     }
-    
+
     public String getUserLink(long userId) throws PortalException, SystemException {
         if (userId <= 0) return StringPool.BLANK;
-        User u = UserLocalServiceUtil.getUserById(userId);
-        String url = "<a href='" + ProposalsURLGenerator.getUserURL(userId)+ "'>" + u.getScreenName()+ "</a>";
-        return url;
+        try {
+            User u = UserLocalServiceUtil.getUserById(userId);
+            return "<a href='" + ProposalsURLGenerator.getUserURL(userId) + "'>" + u.getScreenName() + "</a>";
+        } catch (Exception e) {
+            _log.warn("Couldn't fetch user url", e);
+        }
+        return StringPool.BLANK;
     }
 
 }
