469366a2fbe4d14242dfbcb7c959e1147bf394b5
Stephan Rabanser <steverab93@gmail.com>
2016-03-25 12:42:25 -0400

    [COLAB-996][COLAB-1000] Transition to new button implementation

--- portlets/contestmanagement-portlet/src/main/webapp/js/discussionComments.js
+++ portlets/contestmanagement-portlet/src/main/webapp/js/discussionComments.js
@@ -20,7 +20,7 @@ function editComment(messageId, url){
     var formContent = '<form method="post" action="' + url + '">';
     formContent += '<textarea id="text_' + messageId + '" name="comment" style="width: 100%; height: 150px;"></textarea>';
     formContent += '<input name="messageId" type="hidden" value="' + messageId + '"/>';
-    formContent += '<div class="blue-button" style="margin-left: 320px; margin-top: 10px;"><a onclick=" $(this).parents(\'form:first\').submit()" type="submit" href="javascript:;">Save</a></div>';
+    formContent += '<div class="c-Button__primary" style="margin-left: 320px; margin-top: 10px;"><a onclick=" $(this).parents(\'form:first\').submit()" type="submit" href="javascript:;">Save</a></div>';
     formContent += '</form>';
     $('#message_' + messageId).append(formContent);
     $('#message_' + messageId).next().remove();
--- portlets/proposals-portlet/src/main/webapp/js/copyProposalWidget.js
+++ portlets/proposals-portlet/src/main/webapp/js/copyProposalWidget.js
@@ -9,7 +9,7 @@ function proposalCopy_loadContests(move, hideOnMove) {
     		html.push(obj.contestName);
     		html.push("</td>");
     		html.push("<td>");
-    		html.push('<div class="blue-button"><a href="/contests/'+ obj.contestYear +'/' + obj.contestUrlName + '/');
+    		html.push('<div class="c-Button__primary"><a href="/contests/'+ obj.contestYear +'/' + obj.contestUrlName + '/');
 
     		if (move && hideOnMove) {
     			html.push('/c/proposal/');
@@ -67,7 +67,7 @@ function loadProposalSections() {
     		}
     		html.push("</td>");
     		html.push("<td>");
-    		html.push('<div class="blue-button"><a href="javascript:;" class="copySectionBtn" data-section-id="');
+    		html.push('<div class="c-Button__primary"><a href="javascript:;" class="copySectionBtn" data-section-id="');
     	    html.push(obj.sectionId);
     	    html.push('">Copy</a></div>');
     		html.push("</td></tr>");
