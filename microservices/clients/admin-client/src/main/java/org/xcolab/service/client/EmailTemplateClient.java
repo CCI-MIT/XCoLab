cd11dee080331b0726f2c0d5b9aaa40175eb7433
Carlos Botelho <carlosbpf@gmail.com>
2016-04-08 22:16:00 -0300

    script for service build, Email client

--- microservices/clients/admin-client/src/main/java/org/xcolab/service/client/EmailTemplateClient.java
+++ microservices/clients/admin-client/src/main/java/org/xcolab/service/client/EmailTemplateClient.java
@@ -28,9 +28,16 @@ public class EmailTemplateClient {
         return response.getBody();
     }
 
-        public static ContestEmailTemplate getContestEmailTemplateByType(String emailTemplateType) {
+    public static ContestEmailTemplate getContestEmailTemplateByType(String emailTemplateType) {
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl("http://" +
                 EUREKA_APPLICATION_ID + "/emailTemplates/" + emailTemplateType + "");
         return restTemplate.getForObject(uriBuilder.build().toString(), ContestEmailTemplate.class);
     }
+
+    public static void updateContestEmailTemplate(ContestEmailTemplate contestEmailTemplate) {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl("http://" +
+                EUREKA_APPLICATION_ID + "/emailTemplates/" + contestEmailTemplate.getType_() + "");
+        restTemplate.getForObject(uriBuilder.build().toString(), ContestEmailTemplate.class);
+    }
+
 }
---
+++ microservices/services/buildAllServices.sh
@@ -0,0 +1,6 @@
+cd admin-service/
+mvn compile package spring-boot:repackage 
+cd ../emails-service/
+mvn compile package spring-boot:repackage 
+cd ../members-service/
+mvn compile package spring-boot:repackage 
