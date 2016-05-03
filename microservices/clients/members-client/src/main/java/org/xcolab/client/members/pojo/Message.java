edd99685eb22b8b40543768f43ba7f28e0ee639b
Johannes Bachhuber <jobachhu@mit.edu>
2016-05-03 11:32:25 +0200

    Fixed json parsing exception in members-client

--- microservices/clients/members-client/src/main/java/org/xcolab/client/members/pojo/Message.java
+++ microservices/clients/members-client/src/main/java/org/xcolab/client/members/pojo/Message.java
@@ -8,131 +8,132 @@ import java.sql.Timestamp;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Message implements Serializable {
 
-	private static final long serialVersionUID = 1208926668;
+    private static final long serialVersionUID = 1208926668;
 
-	private Long      messageid;
-	private Long      fromid;
-	private Long      repliesto;
-	private Timestamp createdate;
-	private String    subject;
-	private String    content;
-	private Boolean opened;
-	private Boolean archived;
+    private Long messageid;
+    private Long fromid;
+    private Long repliesto;
+    private Timestamp createdate;
+    private String subject;
+    private String content;
+    private Boolean opened;
+    private Boolean archived;
 
-	public Message() {}
+    public Message() {
+    }
 
-	public Message(Message value) {
-		this.messageid = value.messageid;
-		this.fromid = value.fromid;
-		this.repliesto = value.repliesto;
-		this.createdate = value.createdate;
-		this.subject = value.subject;
-		this.content = value.content;
-	}
+    public Message(Message value) {
+        this.messageid = value.messageid;
+        this.fromid = value.fromid;
+        this.repliesto = value.repliesto;
+        this.createdate = value.createdate;
+        this.subject = value.subject;
+        this.content = value.content;
+    }
 
-	public Message(Message value, Boolean opened, Boolean archived) {
-		this(value);
-		this.opened = opened;
-		this.archived = archived;
-	}
+    public Message(Message value, Boolean opened, Boolean archived) {
+        this(value);
+        this.opened = opened;
+        this.archived = archived;
+    }
 
-	public Message(
-			Long messageid,
-			Long fromid,
-			Long repliesto,
-			Timestamp createdate,
-			String subject,
-			String content,
-			Boolean opened,
-			Boolean archived) {
-		this.messageid = messageid;
-		this.fromid = fromid;
-		this.repliesto = repliesto;
-		this.createdate = createdate;
-		this.subject = subject;
-		this.content = content;
-		this.opened = opened;
-		this.archived = archived;
-	}
+    public Message(
+            Long messageid,
+            Long fromid,
+            Long repliesto,
+            Timestamp createdate,
+            String subject,
+            String content,
+            Boolean opened,
+            Boolean archived) {
+        this.messageid = messageid;
+        this.fromid = fromid;
+        this.repliesto = repliesto;
+        this.createdate = createdate;
+        this.subject = subject;
+        this.content = content;
+        this.opened = opened;
+        this.archived = archived;
+    }
 
-	public Long getMessageId() {
-		return this.messageid;
-	}
+    public Long getMessageId() {
+        return this.messageid;
+    }
 
-	public void setMessageId(Long messageid) {
-		this.messageid = messageid;
-	}
+    public void setMessageId(Long messageid) {
+        this.messageid = messageid;
+    }
 
-	public Long getFromId() {
-		return this.fromid;
-	}
+    public Long getFromId() {
+        return this.fromid;
+    }
 
-	public void setFromId(Long fromid) {
-		this.fromid = fromid;
-	}
+    public void setFromId(Long fromid) {
+        this.fromid = fromid;
+    }
 
-	public Long getRepliesTo() {
-		return this.repliesto;
-	}
+    public Long getRepliesTo() {
+        return this.repliesto;
+    }
 
-	public void setRepliesTo(Long repliesto) {
-		this.repliesto = repliesto;
-	}
+    public void setRepliesTo(Long repliesto) {
+        this.repliesto = repliesto;
+    }
 
-	public Timestamp getCreateDate() {
-		return this.createdate;
-	}
+    public Timestamp getCreateDate() {
+        return this.createdate;
+    }
 
-	public void setCreateDate(Timestamp createdate) {
-		this.createdate = createdate;
-	}
+    public void setCreateDate(Timestamp createdate) {
+        this.createdate = createdate;
+    }
 
-	public String getSubject() {
-		return this.subject;
-	}
+    public String getSubject() {
+        return this.subject;
+    }
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-	public String getContent() {
-		return this.content;
-	}
+    public String getContent() {
+        return this.content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder("Message (");
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Message (");
 
-		sb.append(messageid);
-		sb.append(", ").append(fromid);
-		sb.append(", ").append(repliesto);
-		sb.append(", ").append(createdate);
-		sb.append(", ").append(subject);
-		sb.append(", ").append(content);
-		sb.append(", ").append(opened);
-		sb.append(", ").append(archived);
+        sb.append(messageid);
+        sb.append(", ").append(fromid);
+        sb.append(", ").append(repliesto);
+        sb.append(", ").append(createdate);
+        sb.append(", ").append(subject);
+        sb.append(", ").append(content);
+        sb.append(", ").append(opened);
+        sb.append(", ").append(archived);
 
-		sb.append(")");
-		return sb.toString();
-	}
+        sb.append(")");
+        return sb.toString();
+    }
 
-	public Boolean getOpened() {
-		return opened;
-	}
+    public Boolean getOpened() {
+        return opened;
+    }
 
-	public void setOpened(Boolean opened) {
-		this.opened = opened;
-	}
+    public void setOpened(Boolean opened) {
+        this.opened = opened;
+    }
 
-	public Boolean getArchived() {
-		return archived;
-	}
+    public Boolean getArchived() {
+        return archived;
+    }
 
-	public void setArchived(Boolean archived) {
-		this.archived = archived;
-	}
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
 }
