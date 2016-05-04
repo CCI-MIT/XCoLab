ed8f2d4cb6dd349c8d288c9be1525af7e5ed42fb
Carlos Botelho <carlosbpf@gmail.com>
2016-05-04 01:50:10 -0300

    Changes to sendEmail php to use email service

--- other/maintenance/scripts/sendEmails.php
+++ other/maintenance/scripts/sendEmails.php
@@ -1,4 +1,6 @@
 <?php
+require 'jsonwrapper.php';
+
 $c = mysqli_connect("127.0.0.1", "ColabMaintenance", 'xcolab', "ColabMaintenance") or die(mysqli_error($c));
         $r = mysqli_query($c, "select * from email;") or die(mysqli_error($c));
 
@@ -12,9 +14,35 @@ $message = "Dear Climate CoLab member,\n\n" .
 $from="no-reply@climatecolab.org";
 
 mail($row[1],$subject,$message,"From: $from\n");
-
+sendEmail($row[1], $from, $subject, $message)
 }
 echo "Removing users\n\n";
 $r = mysqli_query($c,"DELETE FROM email") or die(mysqli_error($c));
         mysqli_close($c);
+
+function sendEmail($to, $from, $subject, $emailBody){
+        $postData = array(
+            'to' => array($to),
+            'from' => $from,
+            'subject' => $subject,
+            'emailBody' => $emailBody,
+        	'html' => true,
+        	'replyTo' => ''
+        );
+
+        // Setup cURL
+        $ch = curl_init('http://localhost:8080/emails-service/sendEmail');
+        curl_setopt_array($ch, array(
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            CURLOPT_POSTFIELDS => json_encode($postData)
+        ));
+
+        // Send the request
+        $response = curl_exec($ch);
+
+ }
  ?>
