9c39ed5e4fde1338102d1236e13c934798590064
Carlos Botelho <carlosbpf@gmail.com>
2016-05-23 15:03:21 -0300

    Merging with new dependencies

--- microservices/services/activities-service/src/main/java/org/xcolab/service/activities/persistence/PersistenceContext.java
+++ microservices/services/activities-service/src/main/java/org/xcolab/service/activities/persistence/PersistenceContext.java
@@ -16,7 +16,7 @@ import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.xcolab.service.activities.exceptions.JOOQToSpringExceptionTransformer;
-import org.xcolab.service.utils.DataSourceUtil;
+import org.xcolab.service.utils.db.DataSourceUtil;
 
 import javax.sql.DataSource;
 
