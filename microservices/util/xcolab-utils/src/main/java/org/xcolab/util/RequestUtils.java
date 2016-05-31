b6ec68eaed6983c2600b03425cc3f95c7ebdaead
Carlos Botelho <carlosbpf@gmail.com>
2016-05-31 10:29:40 -0300

    merge bugfix

--- microservices/util/xcolab-utils/src/main/java/org/xcolab/util/RequestUtils.java
+++ microservices/util/xcolab-utils/src/main/java/org/xcolab/util/RequestUtils.java
@@ -202,10 +202,7 @@ public final class RequestUtils {
                 .exchange(uriBuilder.build().toString(), HttpMethod.PUT, httpEntity, Boolean.class)
                 .getBody();
     }
-
-    public static <T> void delete(UriComponentsBuilder uriBuilder) {
-        restTemplate.delete(uriBuilder.build().toString());
-    }
+    
 
     public static <T> T post(UriComponentsBuilder uriBuilder, Object entity, Class<T> returnType) {
         return restTemplate.postForObject(uriBuilder.build().toString(), entity, returnType);
