026b938b47966ae79bd4e684a482a213e7bd9ec0
Carlos Botelho Paula Filho <carlosbpf@Carloss-MacBook-Pro.local>
2016-06-18 00:40:39 -0300

    eh cached

--- microservices/util/xcolab-utils/src/main/java/org/xcolab/util/caching/CacheProviderMemcachedImpl.java
+++ microservices/util/xcolab-utils/src/main/java/org/xcolab/util/caching/CacheProviderMemcachedImpl.java
@@ -10,17 +10,17 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-@Component
+
 public class CacheProviderMemcachedImpl implements CacheProvider, DisposableBean {
 
     private MemcachedClient memcached;
 
     public CacheProviderMemcachedImpl() {
-        try {
+       /* try {
             memcached = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11211));
         } catch (IOException ignored) {
             memcached = null;
-        }
+        }*/
     }
 
     @Override
