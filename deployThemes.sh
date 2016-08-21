ce0d42881115c9dfcc95f9fe78bacb552647c215
Johannes Bachhuber <jobachhu@mit.edu>
2016-08-22 01:11:01 +0200

    [1289] Added script to deploy all themes for binary uploads

--- deployThemes.sh
+++ deployThemes.sh
@@ -5,6 +5,11 @@ set -e
 
 cd themes/climatecolab-theme
   mvn clean compile package install
+  mvn clean compile liferay:build-css package liferay:deploy clean
+cd ../..
+
+cd themes/solve-theme
+  mvn clean compile liferay:build-css package liferay:deploy clean
 cd ../..
 
 cd themes/fow-theme
