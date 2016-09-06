b6784694b2b0ca96f05b281eb29fbd48e0ef8a9d
Carlos Botelho <carlosbpf@gmail.com>
2016-09-06 15:03:38 -0300

    added new client

---
+++ microservices/clients/members-client/src/main/java/org/xcolab/client/members/UsersGroupsClient.java
@@ -0,0 +1,28 @@
+package org.xcolab.client.members;
+
+import org.xcolab.client.members.pojo.UsersGroups;
+import org.xcolab.util.http.client.RestResource;
+import org.xcolab.util.http.client.RestService;
+
+import java.util.List;
+
+public class UsersGroupsClient {
+
+    private static final RestService usersGroupsService = new RestService("members-service");
+
+    private static final RestResource<UsersGroups> usersGroupsResource = new RestResource<>(usersGroupsService,
+            "usersGroups", UsersGroups.TYPES);
+
+    public static UsersGroups createUsersGroups(UsersGroups usersGroups) {
+        return usersGroupsResource.create(usersGroups).execute();
+    }
+
+
+    public static List<UsersGroups> getUserGroupsByUserIdGroupId(Long userId, Long groupId) {
+        return usersGroupsResource.list()
+                .optionalQueryParam("userId", userId)
+                .optionalQueryParam("groupId", groupId)
+                .execute();
+    }
+
+}
