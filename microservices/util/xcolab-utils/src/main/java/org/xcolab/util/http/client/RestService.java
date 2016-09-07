48bef7a4e3a7f4329e9a9340789d2b2214faee4f
Johannes Bachhuber <jobachhu@mit.edu>
2016-09-07 16:00:13 +0200

    [COLAB-1378] sharedcolab-service now throws exceptions when used wrong; Improved quality of changed RestService hosts

--- microservices/util/xcolab-utils/src/main/java/org/xcolab/util/http/client/RestService.java
+++ microservices/util/xcolab-utils/src/main/java/org/xcolab/util/http/client/RestService.java
@@ -7,26 +7,23 @@ import org.xcolab.util.http.client.interfaces.HttpEndpoint;
 public class RestService implements HttpEndpoint {
 
     private static final String SCHEMA = "HTTP://";
-    private static final String HOST = "localhost";
-    private static final String PORT = RequestUtils.getServicesPort();
+    private static final String DEFAULT_HOST_NAME = "localhost";
+    private static final String DEFAULT_PORT = RequestUtils.getServicesPort();
+
     private final String serviceName;
-    private String serviceHost = HOST;
-    private String servicePort = PORT;
+    private final UriProvider uriProvider;
 
     public RestService(String serviceName) {
         this.serviceName = serviceName;
+        uriProvider = getBaseUrl(DEFAULT_HOST_NAME, DEFAULT_PORT);
     }
 
     @Override
     public UriProvider getBaseUrl() {
-        return new UriProvider(SCHEMA + serviceHost + ":" + servicePort + "/" + serviceName);
+        return uriProvider;
     }
 
-    public void setServiceHost(String serviceHost) {
-        this.serviceHost = serviceHost;
-    }
-
-    public void setServicePort(String servicePort) {
-        this.servicePort = servicePort;
+    public UriProvider getBaseUrl(String hostName, String port) {
+        return new UriProvider(SCHEMA + hostName + ":" + port + "/" + serviceName);
     }
 }
