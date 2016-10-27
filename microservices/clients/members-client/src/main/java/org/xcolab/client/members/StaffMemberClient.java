30d3ad273e6fa692d185510a1fd7986ae7442d55
Lukas Mohs <ga47boq@mytum.de>
2016-10-27 15:41:39 -0400

    fixed staffmember bug, implemented messagelimit check

--- microservices/clients/members-client/src/main/java/org/xcolab/client/members/StaffMemberClient.java
+++ microservices/clients/members-client/src/main/java/org/xcolab/client/members/StaffMemberClient.java
@@ -16,6 +16,7 @@ public class StaffMemberClient {
     public static List<StaffMember> getStaffMembersByCategoryId(long categoryId) {
         return staffMemberResource.list()
                 .queryParam("categoryId", categoryId)
+                .queryParam("limitRecord", 50) // since not all members are retrieved by default (20)
                 .execute();
     }
 }
