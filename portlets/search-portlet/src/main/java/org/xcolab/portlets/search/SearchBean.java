c9ce18935de24328b73f59f32e2f497d9d95ec7c
Lukas Mohs <ga47boq@mytum.de>
2016-12-19 17:12:58 -0500

    [COLAB-1490] added link prefix

--- portlets/search-portlet/src/main/java/org/xcolab/portlets/search/SearchBean.java
+++ portlets/search-portlet/src/main/java/org/xcolab/portlets/search/SearchBean.java
@@ -93,6 +93,6 @@ public class SearchBean implements Serializable {
     }
 
     public String getSearchBlogURL() {
-        return ConfigurationAttributeKey.BLOG_URL.get() + "/?submit=Search&s=" + getSearchPhrase();
+        return "http://" + ConfigurationAttributeKey.BLOG_URL.get() + "/?submit=Search&s=" + getSearchPhrase();
     }
 }
