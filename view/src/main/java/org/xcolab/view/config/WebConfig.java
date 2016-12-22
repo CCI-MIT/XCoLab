a7ec5ebf869037ec9e45169ebd83568dcd8b2d20
Johannes Bachhuber <jobachhu@mit.edu>
2016-12-22 12:45:26 +0100

    [COLAB-1502] [COLAB-1503] Added VersionResourceResolver to replace liferay’s theme timestamp cache busting technique

--- view/src/main/java/org/xcolab/view/config/WebConfig.java
+++ view/src/main/java/org/xcolab/view/config/WebConfig.java
@@ -1,10 +1,15 @@
 package org.xcolab.view.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.Assert;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 import org.xcolab.view.theme.ThemeVariableInterceptor;
 
@@ -22,5 +27,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(themeVariableInterceptor);
+    }
+
+    @Bean
+    public FilterRegistrationBean resourceEncodingFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new ResourceUrlEncodingFilter());
+        return registrationBean;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/", "classpath:/dist/")
+                .resourceChain(true)
+                .addResolver(new VersionResourceResolver()
+                        .addContentVersionStrategy("/js/**", "/css/**"));
     }
 }
