791e565f1ac0a46baefcd2b08dacd6b5573446dc
Johannes Bachhuber <jobachhu@mit.edu>
2017-01-03 14:31:51 +0100

    [COLAB-1502] [COLAB-1507] Added rewrite rules for loginregister and from hooks

--- view/src/main/java/org/xcolab/view/config/rewrite/RewriteConfigProvider.java
+++ view/src/main/java/org/xcolab/view/config/rewrite/RewriteConfigProvider.java
@@ -1,24 +1,84 @@
 package org.xcolab.view.config.rewrite;
 
-import org.ocpsoft.logging.Logger.Level;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
-import org.ocpsoft.rewrite.config.Log;
+import org.ocpsoft.rewrite.config.Direction;
+import org.ocpsoft.rewrite.servlet.config.Forward;
 import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.ocpsoft.rewrite.servlet.config.Path;
+import org.ocpsoft.rewrite.servlet.config.Redirect;
 
 import javax.servlet.ServletContext;
 
 public class RewriteConfigProvider extends HttpConfigurationProvider {
 
     @Override
-    public Configuration getConfiguration(ServletContext servletContext) {
-        return ConfigurationBuilder.begin()
-                .addRule()
-                    .perform(Log.message(Level.INFO, "Filter is working"));
-    }
-
-    @Override
     public int priority() {
         return 10;
     }
+
+    @Override
+    public Configuration getConfiguration(ServletContext servletContext) {
+        final ConfigurationBuilder configurationBuilder = ConfigurationBuilder.begin();
+        redirectLegacyRegistration(configurationBuilder);
+        return configurationBuilder;
+
+    }
+
+    private void redirectLegacyRegistration(ConfigurationBuilder configurationBuilder) {
+        configurationBuilder
+                .addRule()
+                    .when(Direction.isInbound()
+                            .and(Path.matches("/web/guest/loginregister{path}")
+                                    .or(Path.matches("/loginregister{path}"))))
+                    .perform(Redirect.permanent("/register"))
+                    .where("path").matches(".*");
+    }
+
+    private void redirectLegacyWikiPages(ConfigurationBuilder configurationBuilder) {
+        //TODO: incomplete - these are just the redirects from the hooks! Rewrite for new wiki implementation
+        configurationBuilder
+                .addRule()
+                    .when(Path.matches("/web/guest/resources/-/wiki/Main/{page}")
+                        .or(Path.matches("/resources/-/wiki/Main/{page}")))
+                    .perform(Forward.to("/web/guest/wiki/-/wiki/page/{page}"))
+                    .where("page").matches(".*")
+                .addRule()
+                    .when(Path.matches("/handbook/-/wiki/Main/{page}")
+                        .or(Path.matches("/web/guest/handbook/-/wiki/Main/{page}")))
+                    .perform(Forward.to("/web/guest/handbook/-/wiki/page/{page}"))
+                    .where("page").matches(".*");
+    }
+
+    //TODO: when proposals portlet is ported, we need compatibility with these old redirects:
+//    <rule>
+//        <from>^/contests(.*)$</from>
+//        <to>/web/guest/plans/-/plans/contests$1</to>
+//    </rule>
+//
+//    <rule>
+//        <from>^/events(.*)$</from>
+//        <to>/web/guest/events/-/plans/contests$1</to>
+//    </rule>
+//
+//    <rule>
+//        <from>^/trends(.*)$</from>
+//        <to>/web/guest/trends/-/plans/contests$1</to>
+//    </rule>
+//
+//    <rule>
+//        <from>^/challenges(.*)$</from>
+//        <to>/web/guest/challenges/-/plans/contests$1</to>
+//    </rule>
+//
+//    <rule>
+//        <from>^/dialogues(.*)$</from>
+//        <to>/web/guest/dialogues/-/plans/contests$1</to>
+//    </rule>
+//
+//    <rule>
+//        <from>^(.*)plans/contestId/1302903/phaseId/1313504(.*)$</from>
+//        <to>$1plans/contestId/1302903$2</to>
+//    </rule>
+
 }
