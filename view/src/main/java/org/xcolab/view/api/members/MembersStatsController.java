5503a17dc4a38e5166fd2e0b31043b85feb6bbc2
Johannes Bachhuber <jobachhu@mit.edu>
2017-01-18 00:02:04 +0100

    [COLAB-1585] Added export functionality for country map

---
+++ view/src/main/java/org/xcolab/view/api/members/MembersStatsController.java
@@ -0,0 +1,31 @@
+package org.xcolab.view.api.members;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.xcolab.client.members.MembersClient;
+import org.xcolab.client.members.PermissionsClient;
+import org.xcolab.client.members.pojo.Member;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/members/stats")
+public class MembersStatsController {
+
+    @GetMapping
+    public MembersStats getStats(Member loggedInMember) {
+        if (PermissionsClient.canAdminAll(loggedInMember)) {
+            List<Member> members = MembersClient.listAllMembers();
+            final Map<String, Long> countryCounts = members.stream()
+                    .filter(member -> StringUtils.isNotBlank(member.getCountry()))
+                    .collect(Collectors.groupingBy(Member::getCountry, Collectors.counting()));
+            return new MembersStats(members.size(), countryCounts);
+        }
+        return null;
+    }
+}
