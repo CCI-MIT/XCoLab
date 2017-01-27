3c3a4ae1e4e81fbb0548d07e5c9abdbe129a50da
Carlos Botelho <carlosbpf@gmail.com>
2017-01-26 22:56:15 -0200

    final changes fro cross upload and batch contest creator

--- view/src/main/java/org/xcolab/view/config/spring/WebSecurityConfig.java
+++ view/src/main/java/org/xcolab/view/config/spring/WebSecurityConfig.java
@@ -2,10 +2,21 @@ package org.xcolab.view.config.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+import org.springframework.security.web.header.writers.DelegatingRequestMatcherHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter
+        .XFrameOptionsMode;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 
 import org.xcolab.view.auth.AuthenticationContext;
@@ -14,6 +25,9 @@ import org.xcolab.view.auth.handlers.AuthenticationSuccessHandler;
 import org.xcolab.view.auth.login.spring.MemberDetailsService;
 import org.xcolab.view.auth.login.spring.MemberPasswordEncoder;
 import org.xcolab.view.auth.handlers.LogoutSuccessHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -39,8 +53,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .csrf()
                     .disable()
-                .headers()
-                    .frameOptions().sameOrigin();
+                .headers().addHeaderWriter(new DelegatingRequestMatcherHeaderWriter(
+                        new NegatedRequestMatcher(
+                                new OrRequestMatcher(getWhiteList())),
+                                new XFrameOptionsHeaderWriter(XFrameOptionsMode.SAMEORIGIN)));
+
     }
 
     @Autowired
@@ -49,4 +66,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(new MemberPasswordEncoder());
     }
 
+    private List<RequestMatcher> getWhiteList(){
+        List<RequestMatcher> whitelist = new ArrayList<>();
+        whitelist.add(new RegexRequestMatcher(".*climatecolab.org.*", HttpMethod.POST.name()));
+        whitelist.add(new RegexRequestMatcher(".*solvecolab.mit.edu.*",HttpMethod.POST.name()));
+        whitelist.add(new RegexRequestMatcher(".*kcc.mit.edu.org.*",HttpMethod.POST.name()));
+        whitelist.add(new RegexRequestMatcher(".*colab2.mit.edu.org.*",HttpMethod.POST.name()));
+        return whitelist;
+    }
 }
