3ffde7fef3a71aa0a26160b8af850d3a2dfd1fb5
Johannes Bachhuber <jobachhu@mit.edu>
2017-03-08 15:39:27 +0100

    [COLAB-1782] Added unsubscribe link to message notification

--- microservices/services/members-service/src/main/java/org/xcolab/service/members/service/messaging/MessageConstants.java
+++ microservices/services/members-service/src/main/java/org/xcolab/service/members/service/messaging/MessageConstants.java
@@ -6,13 +6,17 @@ public class MessageConstants {
             "the following message:\n<br /><br />" +
             "<b>Subject: $SUBJECT</b>\n" +
             "<br /><br />$MESSAGE<br /><br /> \n" +
-            "--------------<br />\n"+
-            "<br /><br />Please do not reply to this email. You can view and respond to this message <a href='$URL'>here</a>.<br /><br />" +
+            "--------------\n"+
+            "<br /><br />Please do not reply to this email. "
+            + "If you no longer wish to receive email notifications when you receive messages, "
+            + "you can update your message settings <a href='$PROFILE_URL'>here</a> (login required).<br/><br />"
+            + "You can view and respond to this message <a href='$URL'>here</a>.<br /><br />" +
             "(If the above link does not work, please paste the following link directly into your browser: $URL )";
 
     public static final String EMAIL_MESSAGE_VAR_USER = "$USER";
     public static final String EMAIL_MESSAGE_VAR_MESSAGE = "$MESSAGE";
     public static final String EMAIL_MESSAGE_VAR_URL = "$URL";
+    public static final String EMAIL_MESSAGE_VAR_PROFILE_URL = "$PROFILE_URL";
 
     public static final String EMAIL_MESSAGE_URL_TEMPLATE = "/web/guest/messaging/-/messaging/message/";
 
