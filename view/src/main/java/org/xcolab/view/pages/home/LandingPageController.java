7c503a7316c3d841c3c958c15b45c97a7ae89b2e
Johannes Bachhuber <jobachhu@mit.edu>
2017-03-12 17:12:43 +0100

    Improved caching on landing page

--- view/src/main/java/org/xcolab/view/pages/home/LandingPageController.java
+++ view/src/main/java/org/xcolab/view/pages/home/LandingPageController.java
@@ -1,7 +1,7 @@
 package org.xcolab.view.pages.home;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.xcolab.client.admin.enums.ConfigurationAttributeKey;
@@ -12,10 +12,29 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class LandingPageController {
 
-    @RequestMapping(value = "/")
-    public String hello(HttpServletRequest request, HttpServletResponse response, Model model) {
-        model.addAttribute("homeSpotContentArticleId", ConfigurationAttributeKey.LANDING_PAGE_BANNER_CONTENT_ARTICLE_ID.get());
-        model.addAttribute("homeBottomContentArticleId", ConfigurationAttributeKey.LANDING_PAGE_BOTTOM_CONTENT_ARTICLE_ID.get());
+    private Long homeSpotContentArticleId;
+    private Long bottomContentArticleId;
+
+    @ModelAttribute("homeSpotContentArticleId")
+    public Long homeSpotContentArticleId() {
+        if (homeSpotContentArticleId == null) {
+            homeSpotContentArticleId =
+                    ConfigurationAttributeKey.LANDING_PAGE_BANNER_CONTENT_ARTICLE_ID.get();
+        }
+        return homeSpotContentArticleId;
+    }
+
+    @ModelAttribute("homeBottomContentArticleId")
+    public Long homeBottomContentArticleId() {
+        if (bottomContentArticleId == null) {
+            bottomContentArticleId =
+                    ConfigurationAttributeKey.LANDING_PAGE_BOTTOM_CONTENT_ARTICLE_ID.get();
+        }
+        return bottomContentArticleId;
+    }
+
+    @RequestMapping("/")
+    public String hello(HttpServletRequest request, HttpServletResponse response) {
         return "home/home";
     }
 }
