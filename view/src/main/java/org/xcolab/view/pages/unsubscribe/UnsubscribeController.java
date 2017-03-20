8865bd9eb3153a494429fe93189fe09c6919dad6
Carlos Botelho <carlosbpf@gmail.com>
2017-03-20 11:03:31 -0300

    COLAB-1338 work for

--- view/src/main/java/org/xcolab/view/pages/unsubscribe/UnsubscribeController.java
+++ view/src/main/java/org/xcolab/view/pages/unsubscribe/UnsubscribeController.java
@@ -22,72 +22,77 @@ import javax.servlet.http.HttpServletResponse;
 public class UnsubscribeController {
 
     private static final String UNSUBSCRIBE_TITLE = "You have been unsubscribed";
-    private static final String UNSUBSCRIBE_INDIVIDUAL_SUBSCRIPTION_RESPONSE_TEXT = "You may still receive email notifications if you are subscribed to other activity on the Climate CoLab.  " +
-            "To manage your subscriptions, please log in to your account, select “My profile”, and select the “Manage” " +
-            "button underneath “Subscribed Activity” on the right-hand side.";
+    private static final String UNSUBSCRIBE_INDIVIDUAL_SUBSCRIPTION_RESPONSE_TEXT =
+            "You may still receive email notifications if you are subscribed to other activity on"
+                    + " the Climate CoLab.  "
+                    +
+                    "To manage your subscriptions, please log in to your account, select “My "
+                    + "profile”, and select the “Manage” "
+                    +
+                    "button underneath “Subscribed Activity” on the right-hand side.";
 
 
     @GetMapping("/notifications/unsubscribe/member/{memberId"
             + "}/subscription/{subscriptionId}/token/{token}/type/{typeId}")
-	public String unsubscribe(HttpServletRequest request, HttpServletResponse response, Model model,
+    public String unsubscribe(HttpServletRequest request, HttpServletResponse response, Model model,
             @PathVariable long memberId, @PathVariable long subscriptionId,
             @PathVariable String token, @PathVariable long typeId) {
-	    
-	    Member member = null;
+
+        Member member = null;
         boolean error = false;
         if (memberId > 0) {
-	        try {
+            try {
                 member = MembersClient.getMember(memberId);
-	            error = ! NotificationUnregisterUtils.isTokenValid(token, member) ||
+                error = !NotificationUnregisterUtils.isTokenValid(token, member) ||
                         typeId != NotificationUnregisterUtils.ACTIVITY_TYPE;
-	        } catch (MemberNotFoundException e) {
+            } catch (MemberNotFoundException e) {
                 return ErrorMessage.error("Please make sure you copied the link correctly or "
                         + "contact an administrator.")
                         .withTitle("Invalid unsubscribe link")
                         .flashAndReturnView(request);
             }
-	    }
+        }
         ActivitySubscription subscription = null;
         if (subscriptionId > 0) {
-	        try {
-	            subscription = ActivitiesClientUtil.getActivitySubscription(subscriptionId);
+            try {
+                subscription = ActivitiesClientUtil.getActivitySubscription(subscriptionId);
                 //ActivitySubscriptionLocalServiceUtil.getActivitySubscription(subscriptionId);
-                error = ! NotificationUnregisterUtils.isTokenValid(token, subscription);
+                error = !NotificationUnregisterUtils.isTokenValid(token, subscription);
             } catch (ActivitySubscriptionNotFoundException e) {
                 //already unsubscribed
-	        }
-	    }
-	    
-	    if (error) {
-	        return ErrorMessage.error("Please make sure you copied the link correctly or "
+            }
+        }
+
+        if (error) {
+            return ErrorMessage.error("Please make sure you copied the link correctly or "
                     + "contact an administrator.")
                     .withTitle("Your unsubscribe token is invalid")
                     .flashAndReturnView(request);
-	    }
+        }
 
         String responseText = null;
-	    // unregister user
-	    if (subscription != null) {
+        // unregister user
+        if (subscription != null) {
             ActivitiesClientUtil.deleteSubscriptionById(subscription.getPk());
             responseText = UNSUBSCRIBE_INDIVIDUAL_SUBSCRIPTION_RESPONSE_TEXT;
-	    }
+        }
 
-	    if (member != null) {
+        if (member != null) {
             NotificationUnregisterHandler handler = getUnregisterUserHandler((int) typeId);
             if (handler != null) {
                 handler.unregister(member);
                 responseText = handler.getSuccessResponse();
             }
-	    }
+        }
 
-        if(!error) {
+        if (!error) {
             AlertMessage.success("You have successfully unsubscribed!").flash(request);
         }
         model.addAttribute("responseTitle", UNSUBSCRIBE_TITLE);
         model.addAttribute("responseText", responseText);
-	    
-	    return "unsubscribe/view";
-	}
+
+        return "unsubscribe/view";
+    }
 
     private NotificationUnregisterHandler getUnregisterUserHandler(int type) {
         if (type == NotificationUnregisterUtils.ACTIVITY_TYPE) {
