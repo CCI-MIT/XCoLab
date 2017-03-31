b450ae6f3d791fbd347dbf2963a37b5f01e4040b
Carlos Botelho <carlosbpf@gmail.com>
2017-03-31 11:18:38 -0300

    more testing for MainViewController

--- view/src/test/java/org/xcolab/view/pages/loginregister/MainViewTest.java
+++ view/src/test/java/org/xcolab/view/pages/loginregister/MainViewTest.java
@@ -1,9 +1,11 @@
 package org.xcolab.view.pages.loginregister;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -17,23 +19,41 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.xcolab.client.admin.AdminClient;
+import org.xcolab.client.admin.EmailTemplateClientUtil;
 import org.xcolab.client.admin.pojo.ConfigurationAttribute;
+import org.xcolab.client.admin.pojo.ContestEmailTemplate;
 import org.xcolab.client.contest.ContestClient;
 import org.xcolab.client.contest.ContestClientUtil;
 import org.xcolab.client.contest.pojo.ContestType;
+import org.xcolab.client.emails.EmailClient;
+import org.xcolab.client.members.MembersClient;
+import org.xcolab.client.members.pojo.Member;
+import org.xcolab.client.sharedcolab.SharedColabClient;
 import org.xcolab.util.http.ServiceRequestUtils;
 import org.xcolab.util.http.exceptions.EurekaNotInitializedException;
+import org.xcolab.view.util.TestUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.any;
+
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Matchers.anyLong;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.anyString;
 
 
 @RunWith(PowerMockRunner.class)
@@ -51,38 +71,99 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         }
 )
 
-@PrepareForTest({org.xcolab.client.admin.AdminClient.class,org.xcolab.client.contest.ContestClientUtil.class})
+@PrepareForTest({
+        org.xcolab.client.admin.AdminClient.class,
+        org.xcolab.client.contest.ContestClientUtil.class,
+        org.xcolab.client.sharedcolab.SharedColabClient.class,
+        org.xcolab.client.members.MembersClient.class,
+        org.xcolab.client.admin.EmailTemplateClientUtil.class,
+        org.xcolab.client.emails.EmailClient.class
+})
 
 public class MainViewTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void shouldReturnRegisterForm() throws Exception {
-
-
+    @Before
+    public void setup() throws Exception {
         ServiceRequestUtils.setInitialized(true);
 
 
         PowerMockito.mockStatic(AdminClient.class);
         PowerMockito.mockStatic(ContestClientUtil.class);
+        PowerMockito.mockStatic(SharedColabClient.class);
+        PowerMockito.mockStatic(MembersClient.class);
+        PowerMockito.mockStatic(EmailTemplateClientUtil.class);
+        PowerMockito.mockStatic(EmailClient.class);
 
 
-        Mockito.doAnswer(new Answer<String>() {
-            @Override
-            public String answer(InvocationOnMock invocation) {
-                return "Always the same";
-            }
-        }).when(listMock).get(anyInt());
+        Mockito.when(EmailTemplateClientUtil.getContestEmailTemplateByType(anyString()))
+                .thenAnswer(new Answer<ContestEmailTemplate>() {
+                    @Override
+                    public ContestEmailTemplate answer(InvocationOnMock invocation)
+                            throws Throwable {
 
-        Mockito.when(AdminClient.getConfigurationAttribute(anyString())).thenAnswer(new Answer<String>() {
-            @Override
-            public String answer(InvocationOnMock invocation) throws Throwable {
-                Object[] args = invocation.getArguments();
-                return (String) args[0];
-            }
-        });
+                        ContestEmailTemplate contestEmailTemplate = new ContestEmailTemplate();
+                        contestEmailTemplate.setFooter(TestUtil.createStringWithLength(10));
+                        contestEmailTemplate.setHeader(TestUtil.createStringWithLength(10));
+                        contestEmailTemplate.setSubject(TestUtil.createStringWithLength(10));
+                        contestEmailTemplate.setType_(TestUtil.createStringWithLength(10));
+
+                        return contestEmailTemplate;
+                    }
+                });
+
+        Mockito.when(MembersClient.getMemberUnchecked(anyLong()))
+                .thenAnswer(new Answer<Member>() {
+                                @Override
+                                public Member answer(InvocationOnMock invocation)
+                                        throws Throwable {
+
+                                    Member member = new Member();
+                                    member.setScreenName(TestUtil.createStringWithLength(10));
+                                    member.setFirstName(TestUtil.createStringWithLength(10));
+                                    member.setLastName(TestUtil.createStringWithLength(10));
+                                    member.setHashedPassword(TestUtil.createStringWithLength(10));
+                                    return member;
+                                }
+                            });
+
+        //generic mock for ConfigAttribute
+        Mockito.when(AdminClient.getConfigurationAttribute(anyString()))
+                .thenAnswer(new Answer<ConfigurationAttribute>() {
+                    @Override
+                    public ConfigurationAttribute answer(InvocationOnMock invocation)
+                            throws Throwable {
+                        Object[] arguments = invocation.getArguments();
+                        if (arguments != null && arguments.length > 0 && arguments[0] != null) {
+                            String key = (String) arguments[0];
+                            if (key.equals("ACTIVE_THEME")) {
+                                return new ConfigurationAttribute("ACTIVE_THEME", 0l, 0l,
+                                        "CLIMATE_COLAB", 0d);
+                            } else {
+                                return new ConfigurationAttribute(key, 0l, 0l, "12312312332", 0d);
+                            }
+
+                        }
+
+                        return new ConfigurationAttribute("GOOGLE_RECAPTCHA_SITE_KEY", 0l, 0l,
+                                "12312312332", 0d);
+                    }
+                });
+
+        Mockito.when(ContestClientUtil.getAllContestTypes()).thenReturn(
+                new ArrayList()
+        );
+    }
+
+    @Test
+    public void shouldReturnRegisterForm() throws Exception {
+
+
+
+
+        /*
         Mockito.when(AdminClient.getConfigurationAttribute("GOOGLE_RECAPTCHA_SITE_KEY")).thenReturn(
                 new ConfigurationAttribute("GOOGLE_RECAPTCHA_SITE_KEY",0l,0l,"12312312332", 0d)
         );
@@ -122,34 +203,70 @@ public class MainViewTest {
                 new ConfigurationAttribute("OPEN_GRAPH_SHARE_TITLE",0l,0l,"", 0d)
         );
 
-        Mockito.when(AdminClient.getConfigurationAttribute("OPEN_GRAPH_SHARE_DESCRIPTION")).thenReturn(
+        Mockito.when(AdminClient.getConfigurationAttribute("OPEN_GRAPH_SHARE_DESCRIPTION"))
+        .thenReturn(
                 new ConfigurationAttribute("OPEN_GRAPH_SHARE_DESCRIPTION",0l,0l,"", 0d)
         );
 
         Mockito.when(AdminClient.getConfigurationAttribute("META_PAGE_DESCRIPTION")).thenReturn(
                 new ConfigurationAttribute("META_PAGE_DESCRIPTION",0l,0l,"", 0d)
         );
+        */
+
+        //.andDo(print())
+        this.mockMvc.perform(get("/register")).andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/loginregister/register.jspx"));
+    }
+
+    @Test
+    public void registrationFailsWhenInvalidDataPostedAndSendsUserBackToForm() throws Exception {
 
 
+        this.mockMvc.perform(post("/register")
+                .param("screenName", "")
+                .param("email", "")
+                .param("imageId", "")
+                .param("file", "")
+                .param("retypeEmail", "")
+                .param("firstName", "")
+                .param("lastName", "")
+                .param("password", "")
+                .param("retypePassword", "")
+                .param("country", "")
+                .param("shortBio", ""))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/loginregister/register.jspx"))
+                .andExpect(model().attributeHasFieldErrors(
+                        "screenName", "email",
+                        "retypeEmail", "firstName",
+                        "lastName", "password",
+                        "retypePassword"));
+
+    }
 
 
+    @Test
+    public void registrationWorksAndUserToHome() throws Exception {
 
 
+        this.mockMvc.perform(post("/register")
+                .param("screenName", "username")
+                .param("email", "username@gmail.com")
+                .param("imageId", "")
+                .param("file", "")
+                .param("retypeEmail", "username@gmail.com")
+                .param("firstName", "User")
+                .param("lastName", "Name")
+                .param("password", "username")
+                .param("retypePassword", "username")
+                .param("country", "BR")
+                .param("shortBio", "shortbio"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/loginregister/register.jspx"))
+                .andExpect(model().attributeHasFieldErrors(
+                        "screenName", "email",
+                        "retypeEmail", "firstName",
+                        "lastName", "password",
+                        "retypePassword"));
 
-        Mockito.when(ContestClientUtil.getAllContestTypes()).thenReturn(
-                new ArrayList()
-        );
-
-
-
-
-
-
-
-
-
-        this.mockMvc.perform(get("/register")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("registrationForm")));
     }
 
 }
