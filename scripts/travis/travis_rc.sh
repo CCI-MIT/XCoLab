875349fb1c4c4037ea87742d583d9861f740c8e0
Johannes Bachhuber <jobachhu@mit.edu>
2017-04-16 10:30:52 +0200

    Added option to deploy on travis via special branches (with prefix deploy-)

--- scripts/travis/travis_rc.sh
+++ scripts/travis/travis_rc.sh
@@ -2,7 +2,7 @@
 
 # Set additional environment variables for travis
 
-if [ $TRAVIS_BRANCH == 'homolog' ] || [ $TRAVIS_BRANCH == 'master' ]; then
+if [ $TRAVIS_BRANCH == 'homolog' ] || [ $TRAVIS_BRANCH == 'master' ] || [[ $TRAVIS_BRANCH =~ ^deploy-.*$ ]]; then
     export BUILD_TYPE=deploy
 else
     export BUILD_TYPE=test
