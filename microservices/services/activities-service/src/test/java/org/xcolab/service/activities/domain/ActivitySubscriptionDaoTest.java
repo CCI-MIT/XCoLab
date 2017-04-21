399ce24f5b880cc58f96197b4fc1a351f7f7e7ef
Carlos Botelho <carlosbpf@gmail.com>
2017-04-21 17:40:13 -0300

    activity subscription dao and service

--- microservices/services/activities-service/src/test/java/org/xcolab/service/activities/domain/ActivitySubscriptionDaoTest.java
+++ microservices/services/activities-service/src/test/java/org/xcolab/service/activities/domain/ActivitySubscriptionDaoTest.java
@@ -1,5 +1,6 @@
 package org.xcolab.service.activities.domain;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
@@ -9,7 +10,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import org.xcolab.model.tables.pojos.ActivityEntry;
+import org.xcolab.model.tables.pojos.ActivitySubscription;
 import org.xcolab.service.activities.domain.activitySubscription.ActivitySubscriptionDao;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @DataJpaTest
@@ -22,9 +34,66 @@ import org.xcolab.service.activities.domain.activitySubscription.ActivitySubscri
     }
 )
 @ComponentScan("org.xcolab.service.activities")
+
 public class ActivitySubscriptionDaoTest {
 
     @Autowired
     ActivitySubscriptionDao activitySubscriptionDao;
+
+    @Test
+    public void shouldCreateNewActivitySubscription() throws Exception {
+
+        ActivitySubscription as = new ActivitySubscription();
+        as = activitySubscriptionDao.create(as);
+        assertNotNull(activitySubscriptionDao.get(as.getPk()));
+    }
+
+    @Test
+    public void shouldGetEmptyForSubscriptionPKNotFound() throws Exception {
+
+        assertFalse(activitySubscriptionDao.get(-1l).isPresent());
+    }
+
+    @Test
+    public void shouldGetSubscription() throws Exception {
+
+        assertTrue(activitySubscriptionDao.get(1463904l).isPresent());
+    }
+
+
+    @Test
+    public void shouldGetActivitySubscribersByReceiver() throws Exception {
+        assertTrue(activitySubscriptionDao.getActivitySubscribers(null,null,2664871l).size()==1);
+
+    }
+
+    @Test
+    public void shouldGetActivitySubscribersByClassNameClassPK() throws Exception {
+        assertTrue(activitySubscriptionDao.getActivitySubscribers(39202l,1366054l,null).size()==1);
+    }
+
+
+    @Test
+    public void shouldCheckIfIsSubscriber() throws Exception {
+        assertTrue(activitySubscriptionDao.isSubscribed(2664865l,1368503l,1333850l,0,null));
+        assertFalse(activitySubscriptionDao.isSubscribed(2694865l,1368503l,1333850l,0,null));
+    }
+    @Test
+    public void shouldDeleteSubscription() throws Exception {
+        Long subscId = 1463902l;
+        assertTrue(activitySubscriptionDao.get(subscId).isPresent());
+        assertTrue(activitySubscriptionDao.delete(subscId));
+        assertFalse(activitySubscriptionDao.get(subscId).isPresent());
+    }
+
+    @Test
+    public void shouldUpdateSubscription() throws Exception {
+        ActivitySubscription as = activitySubscriptionDao.get(1463904l).get();
+        String extraData = "-";
+        as.setExtraData(extraData);
+        assertTrue(activitySubscriptionDao.update(as));
+        assertTrue(activitySubscriptionDao.get(1463904l).get().getExtraData().equals(extraData));
+    }
+
 
 }
