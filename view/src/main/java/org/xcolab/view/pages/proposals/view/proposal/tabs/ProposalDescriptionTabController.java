defd3209f79e737b925a1205f69739129616c7b6
Johannes Bachhuber <jobachhu@mit.edu>
2017-04-25 22:53:47 +0200

    [COLAB-1926] Extended showShareButtons configuration to work for proposal summary

--- view/src/main/java/org/xcolab/view/pages/proposals/view/proposal/tabs/ProposalDescriptionTabController.java
+++ view/src/main/java/org/xcolab/view/pages/proposals/view/proposal/tabs/ProposalDescriptionTabController.java
@@ -95,6 +95,7 @@ public class ProposalDescriptionTabController extends BaseProposalTabController 
         model.addAttribute("edit", editValidated);
         model.addAttribute("voted", voted);
         model.addAttribute("reportTargets", FlaggingClient.listReportTargets(TargetType.PROPOSAL));
+        model.addAttribute("showShareButtons", ConfigurationAttributeKey.SHOW_SHARE_BUTTONS.get());
 
         final Proposal proposal = proposalsContext.getProposal(request);
         final Proposal proposalWrapped = proposalsContext.getProposalWrapped(request);
