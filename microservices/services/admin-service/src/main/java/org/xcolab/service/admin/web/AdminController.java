e140eee261d3484e850e4db5266f1681d9504272
Johannes Bachhuber <jobachhu@mit.edu>
2017-04-28 15:06:25 +0200

    [COLAB-1701] Fixed recurring exception when no notifications have been set

--- microservices/services/admin-service/src/main/java/org/xcolab/service/admin/web/AdminController.java
+++ microservices/services/admin-service/src/main/java/org/xcolab/service/admin/web/AdminController.java
@@ -1,7 +1,5 @@
 package org.xcolab.service.admin.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,83 +15,37 @@ import org.xcolab.service.admin.exceptions.NotFoundException;
 import org.xcolab.service.admin.pojo.Notification;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 @RestController
 public class AdminController {
 
     private final ConfigurationAttributeDao configurationAttributeDao;
-    private static final Logger log = LoggerFactory.getLogger(AdminController.class);
 
-    public static ArrayList<Notification> notificationsList;
-
-    @DeleteMapping("/notifications/{notificationId}")
-    public boolean deleteNotifications(@PathVariable long notificationId) throws NotFoundException {
-
-
-        try {
-            if (notificationsList == null || notificationsList.isEmpty()) {
-                return false;
-            }
-
-            //iterator searches for list items and delete
-            for (Iterator<Notification> iter = notificationsList.listIterator(); iter.hasNext(); ) {
-                Notification noti = iter.next();
-                if (noti.getNotificationId() == notificationId) {
-                    iter.remove();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return true;
-    }
-
-    @GetMapping("/notifications")
-    public ArrayList<Notification> getNotifications() {
-        if (notificationsList == null || notificationsList.isEmpty()) {
-            notificationsList = new ArrayList<>();
-        }
-
-        removeExpiredNotifications();
-        return notificationsList;
-    }
-
-    private void removeExpiredNotifications() {
-        try {
-            if (notificationsList == null || notificationsList.isEmpty()) {
-                return;
-            }
-
-            for (Iterator<Notification> iter = notificationsList.listIterator(); iter.hasNext(); ) {
-                Notification noti = iter.next();
-                if (noti.isExpired()) {
-                    iter.remove();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @PostMapping("/notifications")
-    public void setNotifications(@RequestBody Notification message) {
-        try {
-            log.debug("Inside Admin Service- AdminController.java setNotifications()");
-            if (message.getEndTime().before(message.getBeginTime())) {
-                log.warn("Begin time for notification cannot be after expire time");
-            } else {
-                notificationsList.add(message);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private final ArrayList<Notification> notificationsList = new ArrayList<>();
 
     @Autowired
     public AdminController(ConfigurationAttributeDao configurationAttributeDao) {
         this.configurationAttributeDao = configurationAttributeDao;
+    }
+
+    @DeleteMapping("/notifications/{notificationId}")
+    public boolean deleteNotifications(@PathVariable long notificationId) throws NotFoundException {
+        return notificationsList.removeIf(notification -> notification.getNotificationId() == notificationId);
+    }
+
+    @GetMapping("/notifications")
+    public ArrayList<Notification> getNotifications() {
+        notificationsList.removeIf(Notification::isExpired);
+        return notificationsList;
+    }
+
+    @PostMapping("/notifications")
+    public void createNotification(@RequestBody Notification message) {
+        if (message.getEndTime().before(message.getBeginTime())) {
+            throw new IllegalArgumentException("Begin time cannot be after end time.");
+        } else {
+            notificationsList.add(message);
+        }
     }
 
     @GetMapping("/attributes/{attributeName}")
