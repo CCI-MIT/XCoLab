d46c3057efc57cdfebebc0b0eb6c194989015436
Johannes Bachhuber <jobachhu@mit.edu>
2017-05-08 13:50:56 +0200

    Fixed ignored email exception

--- microservices/services/emails-service/src/main/java/org/xcolab/service/emails/web/EmailsController.java
+++ microservices/services/emails-service/src/main/java/org/xcolab/service/emails/web/EmailsController.java
@@ -2,15 +2,14 @@ package org.xcolab.service.emails.web;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.xcolab.model.tables.pojos.ColabEmail;
 import org.xcolab.service.emails.domain.ColabEmailDao;
 import org.xcolab.service.emails.pojo.Email;
-import org.xcolab.service.emails.util.EmailUtil;
+import org.xcolab.service.emails.util.EmailService;
 
 import java.sql.Timestamp;
 import java.util.Date;
@@ -19,13 +18,17 @@ import java.util.List;
 @RestController
 public class EmailsController {
 
-    @Autowired
-    private EmailUtil emailUtil;
+    private final EmailService emailService;
+
+    private final ColabEmailDao colabEmailDao;
 
     @Autowired
-    private ColabEmailDao colabEmailDao;
+    public EmailsController(ColabEmailDao colabEmailDao, EmailService emailService) {
+        this.colabEmailDao = colabEmailDao;
+        this.emailService = emailService;
+    }
 
-    @RequestMapping(value = "/emails/send", method = RequestMethod.POST)
+    @PostMapping("/emails/send")
     public String sendEmail(@RequestBody Email email) {
 
         boolean shouldSendEmail = true;
@@ -33,17 +36,12 @@ public class EmailsController {
             List<ColabEmail> colabEmails = colabEmailDao
                     .findByGiven(email.getSubject(), email.getTo().get(0), email.getReferenceId(),
                             hashEmail(email.getEmailBody()));
-            if(!colabEmails.isEmpty()){
-                shouldSendEmail = false;
-            }else{
-                shouldSendEmail = true;
-            }
-
+            shouldSendEmail = colabEmails.isEmpty();
         }
 
         createColabEmailFromEmail(email,shouldSendEmail);
-        if(shouldSendEmail) {
-            emailUtil.sendEmailToRecipient(email);
+        if (shouldSendEmail) {
+            emailService.sendEmailToRecipient(email);
         }
         return "Email sent successfully";
     }
