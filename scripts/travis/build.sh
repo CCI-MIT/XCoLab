c3031fda324ff1d39d784fbe8223ed44534f09b7
Johannes Bachhuber <jobachhu@mit.edu>
2017-05-16 00:46:45 +0200

    [COLAB-1981] Increased thread count for integration test builds

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -6,7 +6,7 @@ if [ ${BUILD_TYPE} == 'deploy' ]; then
    mvn clean package -B -T 4
 else
    if [ ${RUN_INTEGRATION_TESTS} == 'true' ]; then
-       mvn clean test -B -T 2 -Pintegration-tests
+       mvn clean test -B -T 4 -Pintegration-tests
    else
        mvn clean test -B -T 4
    fi
