e68432ef0ebf2dc3ba4cf5c4d23f22b219033ef3
Carlos Botelho <carlosbpf@gmail.com>
2017-05-16 15:05:12 -0300

    changed sql to the right deployment date script

--- sql/deployments/deployment-2017-05-15.sql
+++ sql/deployments/deployment-2017-05-15.sql
@@ -2,5 +2,4 @@ ALTER TABLE `xcolab_Contest`
 ADD COLUMN `defaultProposalLogoId` BIGINT(20) NULL DEFAULT NULL AFTER `sponsorLogoId`;
 
 
--- COLAB-1980
-INSERT INTO xcolab_ConfigurationAttribute` (`name`, `additionalId`, `numericValue`, `stringValue`, `realValue`) VALUES ('DISPLAY_FIRST_NAME_LAST_NAME', '0', '0', '0', '0');
+
---
+++ sql/deployments/deployment-2017-05-29.sql
@@ -0,0 +1,2 @@
+-- COLAB-1980
+INSERT INTO xcolab_ConfigurationAttribute` (`name`, `additionalId`, `numericValue`, `stringValue`, `realValue`) VALUES ('DISPLAY_FIRST_NAME_LAST_NAME', '0', '0', '0', '0');
