46f987a6a6f342c8fb03c763fe88bdcafcc6f9b6
Johannes Bachhuber <jobachhu@mit.edu>
2017-05-28 00:11:54 +0200

    [COLAB-1761] Added generic SendGrid webhook to receive and process events

---
+++ view/src/main/java/org/xcolab/view/webhooks/sendgrid/model/EventType.java
@@ -0,0 +1,23 @@
+package org.xcolab.view.webhooks.sendgrid.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum EventType {
+    PROCESSED,
+    DROPPED,
+    DELIVERED,
+    DEFERRED,
+    BOUNCE,
+    OPEN,
+    CLICK,
+    SPAMREPORT,
+    UNSUBSCRIBE,
+    GROUP_UNSUBSCRIBE,
+    GROUP_RESUBSCRIBE;
+
+    @JsonCreator
+    public static EventType fromString(String key) {
+        return key == null ? null
+                : EventType.valueOf(key.toUpperCase());
+    }
+}
