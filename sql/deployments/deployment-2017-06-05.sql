6c07bf2f07a83467ebbc91a945803bfb5e385b50
Johannes Bachhuber <jobachhu@mit.edu>
2017-05-29 10:57:57 +0200

    Removed already executed sql statement from deployment script

--- sql/deployments/deployment-2017-06-05.sql
+++ sql/deployments/deployment-2017-06-05.sql
@@ -1,4 +1,3 @@
 ALTER TABLE xcolab_ProposalUnversionedAttribute CHANGE addtionalId additionalId BIGINT(20);
-ALTER TABLE members_Member ADD isEmailBounced TINYINT(4) DEFAULT 0 AFTER isEmailConfirmed;
 
 update comment_CategoryGroup set url = '/discussion' where groupId = 701;
