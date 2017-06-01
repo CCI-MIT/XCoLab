4d1d6a6ea5a58ce62dd6143b96410a12247127e3
Johannes Bachhuber <jobachhu@mit.edu>
2017-06-01 16:57:44 +0000

    Added metrics reporting to hikari

--- microservices/util/service-utils/src/main/java/org/xcolab/service/utils/db/DataSourceUtil.java
+++ microservices/util/service-utils/src/main/java/org/xcolab/service/utils/db/DataSourceUtil.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.PropertyResolver;
 
+import org.xcolab.util.metrics.MetricsUtil;
+
 public final class DataSourceUtil {
 
     private static final Logger log = LoggerFactory.getLogger(DataSourceUtil.class);
@@ -66,11 +68,14 @@ public final class DataSourceUtil {
         dataSource.setRegisterMbeans(false);
 
         dataSource.setLeakDetectionThreshold(LEAK_DETECTION_THRESHOLD_MS);
+        dataSource.setMetricRegistry(MetricsUtil.REGISTRY);
+
         //mysql optimizations
         dataSource.addDataSourceProperty("cachePrepStmts", true);
         dataSource.addDataSourceProperty("prepStmtCacheSize", PREPARED_STATEMENT_CACHE_SIZE);
         dataSource.addDataSourceProperty("prepStmtCacheSqlLimit",
                 PREPARED_STATEMENT_CACHE_SQL_LIMIT);
+
 
         return dataSource;
     }
