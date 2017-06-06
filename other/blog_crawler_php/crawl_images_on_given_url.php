f94080dc1ab1d4234722955ae26f5b219d5d62c6
Mohammad Zeeshan <zeeshan.m.khan@live.com>
2017-06-05 22:57:04 -0400

    Update Crawler which fetches URLS from file
    Fetches URLs from text file from other PHP script that we've prerun to generate a text file already. And checks for images using the simple_html_dom.php library

--- other/blog_crawler_php/crawl_images_on_given_url.php
+++ other/blog_crawler_php/crawl_images_on_given_url.php
@@ -1,5 +1,41 @@
 <?php
 
-// TODO: Add code here
+include_once('simple_html_dom.php');
+
+$handle = fopen("URLS_crawled.txt", "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        //echo 'Bringing images from: '.$line."<br />";
+        
+        // Create DOM from URL
+        $html   = file_get_html($line); //chosen page
+        // Find all images
+        $images = array();
+        foreach ($html->find('img') as $element) {
+            $images[] = $element->src;
+        }
+        reset($images);
+        $result = count($images);
+        foreach ($images as $out) {
+            
+            
+            if (@getimagesize($out)) {
+                echo "$out<br />\n";
+                
+            } else {
+                $file = 'urls_crawled_not_existing.txt';
+                $url  = $line . ' | ' . $out . "\n";
+                file_put_contents($file, $url, FILE_APPEND | LOCK_EX);
+                echo '------------------------- Image not found --------------------' . "$out<br />\n";
+            }
+        }
+    }
+    
+    fclose($handle);
+} else {
+    echo 'File didn\'t open';
+}
+
+
 
 ?>
