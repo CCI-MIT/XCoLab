7ee1b0ea1e2ca81db353cf92f167b754d38ed3fa
Johannes Bachhuber <jobachhu@mit.edu>
2017-06-08 11:38:09 -0400

    [COLAB-2042] Fix for enum conversion when moving proposals
    Allow repeating arguments when converting string to enum

--- microservices/util/xcolab-utils/src/main/java/org/xcolab/util/enums/proposal/MoveType.java
+++ microservices/util/xcolab-utils/src/main/java/org/xcolab/util/enums/proposal/MoveType.java
@@ -1,5 +1,21 @@
 package org.xcolab.util.enums.proposal;
 
 public enum MoveType {
-    MOVE_PERMANENTLY, COPY, FORK
+    MOVE_PERMANENTLY, COPY, FORK;
+
+
+    public static MoveType valueOf(String stringValue, boolean allowArrays) {
+        try {
+            return valueOf(stringValue);
+        } catch (IllegalArgumentException e) {
+            if (allowArrays) {
+                //attempt to recover if an array of values was given
+                final String[] arguments = stringValue.split(",");
+                if (arguments.length > 0) {
+                    return valueOf(arguments[0]);
+                }
+            }
+            throw e;
+        }
+    }
 }
