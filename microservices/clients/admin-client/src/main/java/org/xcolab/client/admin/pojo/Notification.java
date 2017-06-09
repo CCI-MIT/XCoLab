7a4aad6e2e8b9a3bb5a5d51701959f1c63ceb123
Johannes Bachhuber <jobachhu@mit.edu>
2017-06-09 17:11:21 -0400

    [COLAB-2046] Cleaned up view code for notifications

--- microservices/clients/admin-client/src/main/java/org/xcolab/client/admin/pojo/Notification.java
+++ microservices/clients/admin-client/src/main/java/org/xcolab/client/admin/pojo/Notification.java
@@ -13,12 +13,19 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(Include.NON_NULL)
-public class Notification  implements Serializable {
+public class Notification implements Serializable {
 
     private static final long serialVersionUID = 17654;
 
+    public static final TypeProvider<Notification> TYPES =
+            new TypeProvider<>(Notification.class,
+                    new ParameterizedTypeReference<List<Notification>>() {
+                    });
+
     private long notificationId;
     private String notificationText;
+    private Date beginTime;
+    private Date endTime;
 
     public Date getBeginTime() {
         return beginTime;
@@ -35,14 +42,6 @@ public class Notification  implements Serializable {
     public void setEndTime(Date endTime) {
         this.endTime = endTime;
     }
-
-    private Date beginTime;
-    private Date endTime;
-
-    public static final TypeProvider<Notification> TYPES =
-            new TypeProvider<>(Notification.class,
-                    new ParameterizedTypeReference<List<Notification>>() {
-                    });
 
     public long getNotificationId() {
         return notificationId;
@@ -64,5 +63,46 @@ public class Notification  implements Serializable {
         return endTime.after(new Date());
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Notification)) {
+            return false;
+        }
 
+        Notification that = (Notification) o;
+
+        if (getNotificationId() != that.getNotificationId()) {
+            return false;
+        }
+        if (getNotificationText() != null ? !getNotificationText()
+                .equals(that.getNotificationText()) : that.getNotificationText() != null) {
+            return false;
+        }
+        if (getBeginTime() != null ? !getBeginTime().equals(that.getBeginTime())
+                : that.getBeginTime() != null) {
+            return false;
+        }
+        return getEndTime() != null ? getEndTime().equals(that.getEndTime())
+                : that.getEndTime() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (getNotificationId() ^ (getNotificationId() >>> 32));
+        result = 31 * result + (getNotificationText() != null ? getNotificationText().hashCode()
+                : 0);
+        result = 31 * result + (getBeginTime() != null ? getBeginTime().hashCode() : 0);
+        result = 31 * result + (getEndTime() != null ? getEndTime().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Notification{" + "notificationId=" + notificationId + ", notificationText='"
+                + notificationText + '\'' + ", beginTime=" + beginTime + ", endTime=" + endTime
+                + '}';
+    }
 }
--- view/src/main/java/org/xcolab/view/notification/NotificationController.java
+++ view/src/main/java/org/xcolab/view/notification/NotificationController.java
@@ -1,177 +1,55 @@
 package org.xcolab.view.notification;
 
-import org.json.JSONObject;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.xcolab.client.admin.AdminClient;
 import org.xcolab.client.admin.pojo.Notification;
-import org.xcolab.client.contents.ContentsClient;
-import org.xcolab.client.contents.exceptions.ContentNotFoundException;
-import org.xcolab.client.contents.pojo.ContentPage;
-import org.xcolab.client.contest.OntologyClientUtil;
-import org.xcolab.client.contest.pojo.Contest;
-import org.xcolab.client.contest.pojo.ontology.FocusArea;
-import org.xcolab.client.contest.pojo.ontology.OntologyTerm;
-import org.xcolab.client.members.PermissionsClient;
-import org.xcolab.client.members.pojo.Member;
-import org.xcolab.util.http.exceptions.EntityNotFoundException;
-import org.xcolab.view.auth.MemberAuthUtil;
-import org.xcolab.view.errors.ErrorText;
-import org.xcolab.view.pages.feedback.ContactBean;
-import org.xcolab.view.pages.proposals.impact.ProposalImpactSeries;
-import org.xcolab.view.pages.proposals.impact.ProposalImpactUtil;
-import org.xcolab.view.pages.proposals.utils.context.ProposalContextHelper;
-import org.xcolab.view.pages.proposals.utils.context.ProposalsContextImpl;
-import org.xcolab.view.util.entity.flash.AlertMessage;
 
-import java.io.Console;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Created by zeeshan on 30.03.17.
- */
-@Controller
+@RestController
 public class NotificationController {
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @GetMapping("/notificationMessage")
+    public NotificationResponse showNotification(HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException {
 
-    public static int notificationCounter = 0;
+        Notification firstNotification = AdminClient.getFirstNotification();
 
-    @RequestMapping(value = "/notificationMessageDelete", method = RequestMethod.POST)
-    public String saveNotification(HttpServletRequest request,
-            HttpServletResponse response, @RequestParam String notificationId)
-            throws IOException, ParseException {
-
-        long memberId = MemberAuthUtil.getMemberId(request);
-        if (!PermissionsClient.canAdminAll(memberId)) {
-            return ErrorText.ACCESS_DENIED.flashAndReturnView(request);
+        if (firstNotification != null) {
+            return new NotificationResponse(firstNotification.getNotificationId(),
+                    firstNotification.getNotificationText(), true);
         }
-
-
-        try {
-            AdminClient.deleteNotifications(notificationId);
-
-            AlertMessage.success("Notification Deleted Successfully!").flash(request);
-            response.sendRedirect("/admin/contest/manager/tab/ADMIN");
-
-            return null;
-
-        } catch (Exception e) {
-            AlertMessage.danger("Sorry. Notification cannot be Deleted Successfully!")
-                    .flash(request);
-            response.sendRedirect("/admin/contest/manager/tab/ADMIN");
-
-            e.printStackTrace();
-            return null;
-        }
+        return new NotificationResponse(0L, "", false);
     }
 
+    public static class NotificationResponse {
 
-    @RequestMapping(value = "/notificationMessage", method = RequestMethod.GET)
-    public String showNotification(HttpServletRequest request,
-            HttpServletResponse response) throws IOException {
+        private final long notificationId;
+        private final String notificationText;
+        private final boolean success;
 
-        try {
-            List<Notification> list = AdminClient.getNotifications();
+        public NotificationResponse(long notificationId, String notificationText, boolean success) {
+            this.notificationId = notificationId;
+            this.notificationText = notificationText;
+            this.success = success;
+        }
 
-            if (list != null && !list.isEmpty()) {
-                JSONObject responseJSON = new JSONObject();
+        public long getNotificationId() {
+            return notificationId;
+        }
 
+        public String getNotificationText() {
+            return notificationText;
+        }
 
-                Notification chosenNoti = list.get(0);
-
-                responseJSON.put("notificationId", chosenNoti.getNotificationId());
-                responseJSON.put("notificationText", chosenNoti.getNotificationText());
-                responseJSON.put("success", true);
-                response.getOutputStream().write(responseJSON.toString().getBytes());
-            }
-            return null;
-
-        } catch (EntityNotFoundException e) {
-            JSONObject responseJSON = new JSONObject();
-            responseJSON.put("success", false);
-            response.getOutputStream().write(responseJSON.toString().getBytes());
-
-            e.printStackTrace();
-            return null;
+        public boolean isSuccess() {
+            return success;
         }
     }
-
-    @RequestMapping(value = "/notificationMessageGET", method = RequestMethod.POST)
-    public String saveNotification(HttpServletRequest request,
-            HttpServletResponse response, @RequestParam String notificationText,
-            @RequestParam String expiretime)
-            throws IOException, ParseException {
-
-        long memberId = MemberAuthUtil.getMemberId(request);
-        if (!PermissionsClient.canAdminAll(memberId)) {
-            return ErrorText.ACCESS_DENIED.flashAndReturnView(request);
-        }
-
-
-        try {
-
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-
-            expiretime = expiretime.replace("T", " ");
-
-            if (expiretime.length() < "yyyy-MM-dd hh:mm:ss".length()) {
-                expiretime = expiretime + ":00";
-            }
-
-
-            Date beginDate = new Date();
-            Date endDate = formatter.parse(expiretime);
-
-            if (endDate.before(new Date())) {
-                AlertMessage
-                        .danger("Expiry date cannot be in the past. Notification will not be "
-                                + "saved!")
-                        .flash(request);
-                response.sendRedirect("/admin/contest/manager/tab/ADMIN");
-                return null;
-            }
-
-            List<Notification> list = AdminClient.getNotifications();
-            Date now = new Date();
-            Notification newNotification = new Notification();
-            newNotification.setNotificationId(++notificationCounter);
-            newNotification.setNotificationText(notificationText);
-            newNotification.setBeginTime(beginDate);
-            newNotification.setEndTime(endDate);
-
-            AdminClient.setNotifications(newNotification);
-            list = AdminClient.getNotifications();
-
-            AlertMessage.success("Notification Created Successfully!").flash(request);
-            response.sendRedirect("/admin/contest/manager/tab/ADMIN");
-
-            return null;
-
-        } catch (EntityNotFoundException e) {
-
-            AlertMessage.danger("Sorry. Notification cannot be created at the moment!")
-                    .flash(request);
-            response.sendRedirect("/admin/contest/manager/tab/ADMIN");
-
-            e.printStackTrace();
-            return null;
-        }
-    }
-
 }
