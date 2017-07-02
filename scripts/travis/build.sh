e7d62a73940c5d38d77a9c39182ea2a3da682d1c
Johannes Bachhuber <jobachhu@mit.edu>
2017-07-02 23:54:52 +0200

    Reduced parallelism of builds to prevent running out of memory on travis

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ ${BUILD_TYPE} == 'deploy' ]; then
-   mvn clean package -B -T 4
+   mvn clean package -B -T 3
 else
    if [ ${RUN_INTEGRATION_TESTS} == 'true' ]; then
        mvn clean test -B -T 3 -Pintegration-tests
    else
-       mvn clean test -B -T 4
+       mvn clean test -B -T 3
    fi
 fi
