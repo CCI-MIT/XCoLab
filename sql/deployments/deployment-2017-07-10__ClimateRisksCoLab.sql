640acf195aabe3f6d2d6d5b107248bf11fa810d7
Johannes Bachhuber <jobachhu@mit.edu>
2017-07-03 16:46:07 +0200

    [COLAB-2087] Added migration scripts for individual CoLabs

---
+++ sql/deployments/deployment-2017-07-10__ClimateRisksCoLab.sql
@@ -0,0 +1,12 @@
+-- COLAB-2087
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('CONTEST_NAME', 0, '', null, 'Category', null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('CONTEST_NAME_PLURAL', 0, '', null, 'Categories', null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('PROPOSAL_NAME', 0, '', null, 'Contribution', null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('CONTEST_BASE_URL', 0, '', null, '/contributions', null);
+
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('PROPOSALS_PITCH_NAME', 0, '', null, 'Summary', null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('PROPOSALS_CREATION_PROMPT', 0, '', null, 'Contribute', null);
+
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('DISCUSSION_IS_ACTIVE', 0, '', 0, null, null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('PROPOSALS_SHOW_TEAM_FIELD', 0, '', 0, null, null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('RESOURCES_SHOW_LINK', 0, '', 0, null, null);
