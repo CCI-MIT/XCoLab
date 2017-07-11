2378b96f0c84cd71ad6ea5a71e4f6ab3ce2c76ba
Johannes Bachhuber <jobachhu@mit.edu>
2017-07-11 19:08:33 +0200

    Fixed deployment script for solve

--- sql/deployments/deployment-2017-07-10__SolveCoLab.sql
+++ sql/deployments/deployment-2017-07-10__SolveCoLab.sql
@@ -1,5 +1,5 @@
 -- COLAB-2087
-INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('CONTEST_NAME', 0, '', null, 'Challenge', null);
-INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('PROPOSAL_NAME', 0, '', null, 'Solution', null);
-INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('SUGGESTIONS_CONTEST_ID', 0, '', 1303801, null, null);
-INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('SUGGESTIONS_IS_ACTIVE', 0, '', 1, null, null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('CONTEST_NAME', 3, '', null, 'Challenge', null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('PROPOSAL_NAME', 3, '', null, 'Solution', null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('SUGGESTIONS_CONTEST_ID', 3, '', 1303801, null, null);
+INSERT INTO admin_ContestTypeAttribute (name, additionalId, locale, numericValue, stringValue, realValue) VALUES ('SUGGESTIONS_IS_ACTIVE', 3, '', 1, null, null);
