2a14c9fd7225b32021a163266794a23ce394c192
Johannes Bachhuber <jobachhu@mit.edu>
2017-07-28 18:40:01 +0200

    Added second storage server for binaries

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -2,19 +2,23 @@
 
 if [ ${BUILD_TYPE} == 'deploy' ]; then
 
-   DEPLOY_FOLDER=xcolab/${TRAVIS_BRANCH}
-   DEPLOY_SERVER=dumbo@cognosis2.mit.edu
+    eval "$(ssh-agent -s)"
+    ssh-add deploy_rsa
 
-   echo "Copying binaries to ${DEPLOY_SERVER}:${DEPLOY_FOLDER} ..."
+    DEPLOY_FOLDER=xcolab/${TRAVIS_BRANCH}
+    DEPLOY_SERVER=dumbo@cognosis2.mit.edu
+    echo "Copying binaries to ${DEPLOY_SERVER}:${DEPLOY_FOLDER} ..."
+    rsync -r --delete-after --quiet binaries ${DEPLOY_SERVER}:${DEPLOY_FOLDER}
 
-   eval "$(ssh-agent -s)"
-   ssh-add deploy_rsa
+    # Temporarily copy files to second server
+    DEPLOY_FOLDER2=xcolab/${TRAVIS_BRANCH}
+    DEPLOY_SERVER2=travis@cognosis.mit.edu
+    echo "Copying binaries to ${DEPLOY_SERVER2}:${DEPLOY_FOLDER2} ..."
+    rsync -r --delete-after --quiet binaries ${DEPLOY_SERVER2}:${DEPLOY_FOLDER2}
 
-   rsync -r --delete-after --quiet binaries ${DEPLOY_SERVER}:${DEPLOY_FOLDER}
-
-   # Workaround for hanging builds
-   # taken from https://github.com/travis-ci/travis-ci/issues/8082#issuecomment-315147561
-   ssh-agent -k
+    # Workaround for hanging builds
+    # taken from https://github.com/travis-ci/travis-ci/issues/8082#issuecomment-315147561
+    ssh-agent -k
 else
     echo "Skipping deploy step on branch ${TRAVIS_BRANCH}"
 fi
