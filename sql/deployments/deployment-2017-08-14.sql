4060ee9a808eba4735a9fdbebb77b7d700b6fb12
Johannes Bachhuber <jobachhu@mit.edu>
2017-08-08 14:11:00 +0200

    Cleaned up sql scripts and tools

--- sql/deployments/deployment-2017-08-14.sql
+++
@@ -1,8 +0,0 @@
--- COLAB-2159
--- Climate CoLab only:
-insert into xcolab_ConfigurationAttribute (name, additionalId, stringValue) values ('FACEBOOK_URL', 0, 'https://www.facebook.com/ClimateCoLab');
-insert into xcolab_ConfigurationAttribute (name, additionalId, stringValue) values ('TWITTER_URL', 0, 'https://twitter.com/ClimateCoLab');
-insert into xcolab_ConfigurationAttribute (name, additionalId, stringValue) values ('YOUTUBE_URL', 0, 'https://www.youtube.com/c/MITClimateCoLab');
-insert into xcolab_ConfigurationAttribute (name, additionalId, stringValue) values ('LINKEDIN_URL', 0, 'https://www.linkedin.com/groups/5074465');
-insert into xcolab_ConfigurationAttribute (name, additionalId, stringValue) values ('GOOGLE_URL', 0, 'https://plus.google.com/u/0/+MITClimateCoLab');
-insert into xcolab_ConfigurationAttribute (name, additionalId, stringValue) values ('STORIFY_URL', 0, 'https://storify.com/ClimateCoLab');
--- sql/deployments/deployment-dateunknown-view-changes-resilienceDialogues.sql
+++
@@ -1,8 +0,0 @@
--- -------------------------
--- Resilience Dialogues SPECIFIC
---
--- -------------------------
-
-INSERT INTO xcolab_ConfigurationAttribute (`name`, `additionalId`, `numericValue`, `stringValue`, `realValue`) VALUES ('ACTIVE_THEME', '0', '0', 'RESILIENCE_DIALOGUES', '0.0');
-
-UPDATE xcolab_ConfigurationAttribute SET `stringValue`='{\n \"CALL_TO_ACTION\": \"\",\n \"CONTEST_TYPE_ID\": \"0\"}' WHERE `name`='PORTLET_PROPOSALS_PREFERENCES' and`additionalId`='0';
