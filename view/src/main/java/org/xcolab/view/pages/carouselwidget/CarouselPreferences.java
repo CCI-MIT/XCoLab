c22baa8d4118812044cf404772b367e9a8f12500
Aleksander Umov <aleksander.umov@quartett-mobile.de>
2017-08-24 16:56:57 -0400

    [COLAB-2013] Added example logos to carousel preferences

--- view/src/main/java/org/xcolab/view/pages/carouselwidget/CarouselPreferences.java
+++ view/src/main/java/org/xcolab/view/pages/carouselwidget/CarouselPreferences.java
@@ -5,11 +5,18 @@ import org.xcolab.entity.utils.WidgetPreference;
 import org.xcolab.util.attributes.AttributeGetter;
 import org.xcolab.util.i18n.I18nUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarouselPreferences extends WidgetPreference {
     private String title;
+    private List<LogoElement> logos;
 
     public String getTitle() {
         return title;
+    }
+    public List<LogoElement> getLogos() {
+        return logos;
     }
 
     @Override
@@ -22,5 +29,13 @@ public class CarouselPreferences extends WidgetPreference {
     }
     public CarouselPreferences(String preferenceId, String locale) {
         this.title = "LOLOLOL";
+        String testLogoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Thomson_Reuters_logo.svg/2000px-Thomson_Reuters_logo.svg.png";
+        String testLinkUrl = "https://www.google.com";
+        String testAltText = "Thomson Reuters";
+        LogoElement logo = new LogoElement(testLogoUrl, testLinkUrl, testAltText);
+        this.logos = new ArrayList<>();
+        this.logos.add(logo);
+        this.logos.add(logo);
+        this.logos.add(logo);
     }
 }
