3b96c65a54182c60ba8d3957e76131eb7aeca5af
Carlos Botelho <carlosbpf@gmail.com>
2017-08-28 23:42:38 -0300

    changed index to use a field that does not extrapolate limit COLAB-951

--- sql/deployments/deployment-2017-08-20.sql
+++ sql/deployments/deployment-2017-08-20.sql
@@ -1,3 +1,3 @@
 -- COLAB-951
-ALTER TABLE `xcolab_Contest` ADD UNIQUE `unique_index_contest_name_year`(`ContestShortName`, `ContestYear`);
+ALTER TABLE `xcolab_Contest` ADD UNIQUE `unique_index_contest_name_year`(`ContestUrlName`, `ContestYear`);
 
