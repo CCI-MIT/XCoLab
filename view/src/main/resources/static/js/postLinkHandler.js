c0971e78b6dfadd3c3f5e1e80c96338108eb3b43
Aleksander Umov <aleksander.umov@gmail.com>
2017-09-05 16:26:26 -0400

    [COLAB-2191] implement support for confirm text

--- view/src/main/resources/static/js/postLinkHandler.js
+++ view/src/main/resources/static/js/postLinkHandler.js
@@ -1,10 +1,14 @@
 $(function() {
     $(".js-PostLink").click(function (eventObject) {
+        var confirmText = $(this).data('confirm');
+        if (confirmText && !confirm(confirmText)) {
+            return false;
+        }
         eventObject.preventDefault();
         var form = document.createElement("form");
         form.method = "POST";
         form.action = this.href;
         document.body.appendChild(form);
         form.submit();
-   });
+    });
 });
