94380ca855f7ef821bd4a10cefdbab9c65ecbffa
Aleksander Umov <aleksander.umov@gmail.com>
2017-09-05 15:51:10 -0400

    Revert "[COLAB-2191] PostLinkHandler fix for dynamic links"
    This reverts commit 769ae062753f654fd7f3a197b28a640939d37a1b.

--- view/src/main/resources/static/js/postLinkHandler.js
+++ view/src/main/resources/static/js/postLinkHandler.js
@@ -1,10 +1,10 @@
 $(function() {
-    $(document).on("click", "a.js-PostLink", function (eventObject) {
+    $(".js-PostLink").click(function (eventObject) {
         eventObject.preventDefault();
         var form = document.createElement("form");
         form.method = "POST";
         form.action = this.href;
         document.body.appendChild(form);
         form.submit();
-    });
+   });
 });
