1c82232cf59610424a5474961a1444b8d2b74337
Aleksander Umov <aleksander.umov@gmail.com>
2017-09-08 15:37:28 -0400

    [COLAB-961] create abstract ContestMassAction

---
+++ view/src/main/java/org/xcolab/view/pages/contestmanagement/entities/massactions/ContestMassAction.java
@@ -0,0 +1,28 @@
+package org.xcolab.view.pages.contestmanagement.entities.massactions;
+
+import org.xcolab.view.pages.contestmanagement.wrappers.ContestOverviewWrapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class ContestMassAction {
+
+    private String displayName;
+    private String reverseDisplayName;
+    private Boolean hasReverseAction = false;
+
+    abstract void execute(HttpServletRequest request, HttpServletResponse response,
+            ContestOverviewWrapper contestOverviewWrapper);
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getReverseDisplayName() {
+        return reverseDisplayName;
+    }
+
+    public Boolean getHasReverseAction() {
+        return hasReverseAction;
+    }
+}
