eb7be8110b06cd504e4bd48c85b8347c50023173
Aleksander Umov <aleksander.umov@gmail.com>
2017-09-11 13:50:10 -0400

    [COLAB-961] implement flag mass action

--- view/src/main/java/org/xcolab/view/pages/contestmanagement/entities/massactions/FlagMassAction.java
+++ view/src/main/java/org/xcolab/view/pages/contestmanagement/entities/massactions/FlagMassAction.java
@@ -1,3 +1,34 @@
 package org.xcolab.view.pages.contestmanagement.entities.massactions;
 
-public class FlagMassAction {}
+import org.xcolab.client.contest.pojo.Contest;
+import org.xcolab.view.pages.contestmanagement.beans.ContestFlagTextToolTipBean;
+import org.xcolab.view.pages.contestmanagement.utils.ContestMassActionMethods;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlagMassAction extends ContestMassActionAdapter {
+
+    private ContestFlagTextToolTipBean contestFlagTextToolTipBean;
+
+    public FlagMassAction(String displayName) {
+        super(displayName);
+    }
+
+    @Override
+    public void setup(MassActionDataWrapper dataWrapper) {
+        contestFlagTextToolTipBean = dataWrapper.getContestFlagTextToolTipBean();
+    }
+
+    @Override
+    public void execute(List<Contest> contests, boolean actionConfirmed)
+            throws IllegalStateException {
+        if (contestFlagTextToolTipBean == null) {
+            throw new IllegalStateException("The mass action has not been setup yet.");
+        }
+        // TODO: Fix intermediate solution.
+        List<Long> contestIds =
+                contests.stream().map(Contest::getContestPK).collect(Collectors.toList());
+        ContestMassActionMethods.setFlag(contestIds, contestFlagTextToolTipBean, null);
+    }
+}
