a68eb130540054ae4b50886928a8c4dbf99d0400
Aleksander Umov <aleksander.umov@gmail.com>
2017-09-11 14:13:43 -0400

    [COLAB-961] implement model settings MA

--- view/src/main/java/org/xcolab/view/pages/contestmanagement/entities/massactions/ModelSettingsMassAction.java
+++ view/src/main/java/org/xcolab/view/pages/contestmanagement/entities/massactions/ModelSettingsMassAction.java
@@ -1,3 +1,35 @@
 package org.xcolab.view.pages.contestmanagement.entities.massactions;
 
-public class ModelSettingsMassAction {}
+import org.xcolab.client.contest.pojo.Contest;
+import org.xcolab.view.pages.contestmanagement.beans.ContestModelSettingsBean;
+import org.xcolab.view.pages.contestmanagement.entities.MassActionRequiresConfirmationException;
+import org.xcolab.view.pages.contestmanagement.utils.ContestMassActionMethods;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ModelSettingsMassAction extends ContestMassActionAdapter {
+
+    private ContestModelSettingsBean contestModelSettingsBean;
+
+    public ModelSettingsMassAction() {
+        super("Set model settings");
+    }
+
+    @Override
+    public void setup(MassActionDataWrapper dataWrapper) {
+        contestModelSettingsBean = dataWrapper.getContestModelSettingsBean();
+    }
+
+    @Override
+    public void execute(List<Contest> contests, boolean actionConfirmed)
+            throws IllegalStateException {
+        if (contestModelSettingsBean == null) {
+            throw new IllegalStateException("The mass action has not been setup yet");
+        }
+        // TODO: Fix intermediate solution.
+        List<Long> contestIds =
+                contests.stream().map(Contest::getContestPK).collect(Collectors.toList());
+        ContestMassActionMethods.setModelSettings(contestIds, contestModelSettingsBean, null);
+    }
+}
