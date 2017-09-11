a01914ccd54cac261ff7e2f8aee1a254f3b52ddf
Aleksander Umov <aleksander.umov@gmail.com>
2017-09-11 14:14:42 -0400

    [COLAB-961] implement subscribe mass action

--- view/src/main/java/org/xcolab/view/pages/contestmanagement/entities/massactions/SubscribeMassAction.java
+++ view/src/main/java/org/xcolab/view/pages/contestmanagement/entities/massactions/SubscribeMassAction.java
@@ -1,3 +1,41 @@
 package org.xcolab.view.pages.contestmanagement.entities.massactions;
 
-public class SubscribeMassAction {}
+import org.xcolab.client.contest.ContestClientUtil;
+import org.xcolab.client.contest.pojo.Contest;
+import org.xcolab.view.pages.contestmanagement.entities.MassActionRequiresConfirmationException;
+import org.xcolab.view.pages.contestmanagement.utils.ContestMassActionMethods;
+
+import java.util.List;
+
+public class SubscribeMassAction extends ContestMassActionAdapter {
+
+    private Long memberId;
+    private final boolean isSubscribe;
+
+    public SubscribeMassAction(boolean isSubscribe) {
+        super(isSubscribe ? "Subscribe to activity" : "Unsubscribe from activity");
+        this.isSubscribe = isSubscribe;
+    }
+
+    @Override
+    public void setup(MassActionDataWrapper dataWrapper) {
+        memberId = dataWrapper.getMemberId();
+    }
+
+    @Override
+    public void execute(List<Contest> contests, boolean actionConfirmed)
+            throws IllegalStateException {
+        if (memberId == null) {
+            throw new IllegalStateException("The mass action has not been setup yet.");
+        }
+        for (Contest contest : contests) {
+            if (!contest.getIsSharedContestInForeignColab()) {
+                if (isSubscribe) {
+                    ContestClientUtil.subscribeMemberToContest(contest.getContestPK(), memberId);
+                } else {
+                    ContestClientUtil.unsubscribeMemberFromContest(contest.getContestPK(), memberId);
+                }
+            }
+        }
+    }
+}
