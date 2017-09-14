88e97165ea75835919328fba89e1e3ae2d83951a
Johannes Bachhuber <jobachhu@mit.edu>
2017-09-14 13:49:21 +0200

    Update EntityIdListUtil to use lambdas

--- view/src/main/java/org/xcolab/view/util/entity/EntityIdListUtil.java
+++ view/src/main/java/org/xcolab/view/util/entity/EntityIdListUtil.java
@@ -3,15 +3,14 @@ package org.xcolab.view.util.entity;
 import org.xcolab.client.admin.ContestTypeClient;
 import org.xcolab.client.admin.pojo.ContestType;
 import org.xcolab.client.contest.ContestClientUtil;
-import org.xcolab.client.contest.exceptions.ContestNotFoundException;
 import org.xcolab.client.contest.pojo.Contest;
 import org.xcolab.client.proposals.ProposalClientUtil;
-import org.xcolab.client.proposals.exceptions.ProposalNotFoundException;
 import org.xcolab.client.proposals.pojo.Proposal;
 import org.xcolab.util.IdListUtil;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Utility class for common conversions of ids lists to strings or models.
@@ -23,72 +22,44 @@ public final class EntityIdListUtil {
     /**
      * Utility class to convert between lists of Contests, their ids, and comma separated id strings
      */
-    public final static IdListObjectConverter<Contest> CONTESTS = new IdListObjectConverter<Contest>() {
-        @Override
-        public Contest getObject(long id) {
-            try {
-                return ContestClientUtil.getContest(id);
-            } catch (ContestNotFoundException ignored) {
-                return null;
-            }
-        }
-
-        @Override
-        public long getId(Contest object) {
-            return object.getContestPK();
-        }
-    };
+    public final static IdListObjectConverter<Contest> CONTESTS =
+            new IdListObjectConverter<>(ContestClientUtil::getContest, Contest::getContestPK);
 
     /**
      * Utility class to convert between lists of Proposals, their ids, and comma separated id strings
      */
-    public final static IdListObjectConverter<Proposal> PROPOSALS = new IdListObjectConverter<Proposal>() {
-        @Override
-        public Proposal getObject(long id) {
-            try {
-                return ProposalClientUtil.getProposal(id);
-            } catch (ProposalNotFoundException ignored) {
-                return null;
-            }
-        }
-
-        @Override
-        public long getId(Proposal object) {
-            return object.getProposalId();
-        }
-    };
+    public final static IdListObjectConverter<Proposal> PROPOSALS =
+            new IdListObjectConverter<>(ProposalClientUtil::getProposal, Proposal::getProposalId);
 
     /**
      * Utility class to convert between lists of ContestTypes, their ids, and comma separated id strings
      */
-    public final static IdListObjectConverter<ContestType> CONTEST_TYPES = new IdListObjectConverter<ContestType>() {
-        @Override
-        public ContestType getObject(long id)  {
-            return ContestTypeClient.getContestType(id);
-        }
+    public final static IdListObjectConverter<ContestType> CONTEST_TYPES =
+            new IdListObjectConverter<>(ContestTypeClient::getContestType, ContestType::getId);
 
-        @Override
-        public long getId(ContestType object) {
-            return object.getId();
-        }
-    };
 
-    public static abstract class IdListObjectConverter<T> {
+    public static class IdListObjectConverter<T> {
+
+        private final Function<Long, T> toObjectFunction;
+        private final Function<T, Long> toIdFunction;
+
+        protected IdListObjectConverter(Function<Long, T> toObjectFunction,
+                Function<T, Long> toIdFunction) {
+            this.toObjectFunction = toObjectFunction;
+
+            this.toIdFunction = toIdFunction;
+        }
 
         public final List<T> fromIdList(List<Long> idList) {
-            List<T> objects = new ArrayList<>(idList.size());
-            for (long id : idList) {
-                    objects.add(getObject(id));
-            }
-            return objects;
+            return idList.stream()
+                    .map(toObjectFunction)
+                    .collect(Collectors.toList());
         }
 
         public final List<Long> toIdList(List<T> objectList) {
-            List<Long> ids = new ArrayList<>(objectList.size());
-            for (T object : objectList) {
-                ids.add(getId(object));
-            }
-            return ids;
+            return objectList.stream()
+                    .map(toIdFunction)
+                    .collect(Collectors.toList());
         }
 
         public final String toIdString(List<T> objects) {
@@ -98,19 +69,5 @@ public final class EntityIdListUtil {
         public final List<T> fromIdString(String idString) {
             return fromIdList(IdListUtil.getIdsFromString(idString));
         }
-
-        /**
-         * Retrieves a single object of type T from an id.
-         * @param id the id of the object to be fetched
-         * @return an object of type T
-         */
-        public abstract T getObject(long id) ;
-
-        /**
-         * Gets the id of a single object of type T
-         * @param object the object
-         * @return the object's id
-         */
-        public abstract long getId(T object);
     }
 }
