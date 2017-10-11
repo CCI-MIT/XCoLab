903ee666879d5aa2e668d678e89af2b921fb204d
Johannes Bachhuber <jobachhu@mit.edu>
2017-10-11 23:49:50 +0200

    [COLAB-2344] Improve code style

--- microservices/util/xcolab-utils/src/main/java/org/xcolab/util/enums/activity/ActivityEntryType.java
+++ microservices/util/xcolab-utils/src/main/java/org/xcolab/util/enums/activity/ActivityEntryType.java
@@ -3,12 +3,14 @@ package org.xcolab.util.enums.activity;
 import org.springframework.util.Assert;
 
 public enum ActivityEntryType {
+
     MEMBER(10038L),
     DISCUSSION(39202L),
     PROPOSAL(1368503L),
     CONTEST(39701L);
 
     private final Long primaryTypeId;
+
     ActivityEntryType(Long type) {
         this.primaryTypeId = type;
     }
@@ -20,12 +22,11 @@ public enum ActivityEntryType {
 
     public static ActivityEntryType getActivityEntryTypeByPrimaryType(Long tierType)  {
         Assert.notNull(tierType, "ActivityEntryType cannot be null");
-        for (ActivityEntryType contestTier : ActivityEntryType.values()) {
-            if (contestTier.getPrimaryTypeId().longValue() == tierType) {
-                return contestTier;
+        for (ActivityEntryType activityType : ActivityEntryType.values()) {
+            if (activityType.getPrimaryTypeId().longValue() == tierType) {
+                return activityType;
             }
         }
-        //throw new IllegalArgumentException("No ActivityEntryType " + tierType);
         return null;
     }
 }
