2e8d3269933a9b3993ce2eceb03f7412b72cb767
Johannes Bachhuber <jobachhu@mit.edu>
2017-10-12 19:27:18 +0200

    [COLAB-2334] Migrate files client to new client API
    WIP - [ci skip]

---
+++ microservices/clients/files-client/src/main/java/org/xcolab/client/files/FilesResource.java
@@ -0,0 +1,25 @@
+package org.xcolab.client.files;
+
+import org.xcolab.util.http.client.CoLabService;
+import org.xcolab.util.http.client.enums.ResourceEnum;
+
+public enum FilesResource implements ResourceEnum {
+    FILE_ENTRY("fileEntries")
+    ;
+
+    private final String resourceName;
+
+    FilesResource(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    @Override
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    @Override
+    public CoLabService getCoLabService() {
+        return null;
+    }
+}
