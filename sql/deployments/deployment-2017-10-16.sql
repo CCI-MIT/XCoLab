bac2f2aee63e4bad8926bdd5bb25d385d74a8e8c
Aleksander Umov <aleksander.umov@gmail.com>
2017-10-16 18:07:25 -0400

    [COLAB-2307] add team deployment script

---
+++ sql/deployments/deployment-2017-10-16.sql
@@ -0,0 +1,14 @@
+CREATE TABLE xcolab.xcolab_PlatformTeam
+(
+    id_ BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) DEFAULT NULL 
+);
+
+CREATE TABLE xcolab.xcolab_PlatformTeamMember
+(
+    userId BIGINT(20) NOT NULL,
+    teamId BIGINT(20) NOT NULL,
+    CONSTRAINT xcolab_PlatformTeamMember_userId_teamId_pk PRIMARY KEY (userId, teamId),
+    CONSTRAINT xcolab_PlatformTeamMember_members_Member_id__fk FOREIGN KEY (userId) REFERENCES members_Member (id_) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT xcolab_PlatformTeamMember_xcolab_PlatformTeam_id__fk FOREIGN KEY (teamId) REFERENCES xcolab_PlatformTeam (id_) ON DELETE CASCADE ON UPDATE CASCADE
+);
