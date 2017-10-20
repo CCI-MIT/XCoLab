e22949e0871522e69a8fca18a9b498eac8ea48ac
Aleksander Umov <aleksander.umov@gmail.com>
2017-10-20 16:01:00 -0400

    [COLAB-2307] update sql files

--- sql/deployments/deployment-2017-10-16.sql
+++ sql/deployments/deployment-2017-10-16.sql
@@ -1,14 +1,14 @@
-CREATE TABLE xcolab.xcolab_PlatformTeam
+CREATE TABLE xcolab_PlatformTeam
 (
     id_ BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) DEFAULT NULL 
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE xcolab.xcolab_PlatformTeamMember
+CREATE TABLE xcolab_PlatformTeamMember
 (
     userId BIGINT(20) NOT NULL,
     teamId BIGINT(20) NOT NULL,
     CONSTRAINT xcolab_PlatformTeamMember_userId_teamId_pk PRIMARY KEY (userId, teamId),
     CONSTRAINT xcolab_PlatformTeamMember_members_Member_id__fk FOREIGN KEY (userId) REFERENCES members_Member (id_) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT xcolab_PlatformTeamMember_xcolab_PlatformTeam_id__fk FOREIGN KEY (teamId) REFERENCES xcolab_PlatformTeam (id_) ON DELETE CASCADE ON UPDATE CASCADE
-);
+) ENGINE=InnoDB;
