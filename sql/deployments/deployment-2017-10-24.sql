98501f166ab65a7786589493345c42514127332c
Johannes Bachhuber <jobachhu@mit.edu>
2017-10-25 14:09:14 +0200

    [COLAB-2373] Fix drop index statements

--- sql/deployments/deployment-2017-10-24.sql
+++ sql/deployments/deployment-2017-10-24.sql
@@ -2,6 +2,6 @@
 ALTER TABLE xcolab_ProposalVote DROP PRIMARY KEY;
 ALTER TABLE xcolab_ProposalVote ADD PRIMARY KEY (contestPhaseId, userId, proposalId);
 ALTER TABLE xcolab_ProposalVote ADD value INT DEFAULT 1 AFTER userId;
-DROP INDEX `IX_EA28CF99`;
-DROP INDEX `IX_43559ACF`;
-DROP INDEX `IX_562EB409`;
+DROP INDEX `IX_EA28CF99` on xcolab_ProposalVote;
+DROP INDEX `IX_43559ACF` on xcolab_ProposalVote;
+DROP INDEX `IX_562EB409` on xcolab_ProposalVote;
