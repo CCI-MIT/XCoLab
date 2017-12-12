b32eee0c0aafc48211b4d9c8a4dfe4289559a603
Johannes Bachhuber <jobachhu@mit.edu>
2017-12-12 22:33:34 +0100

    [COLAB-2469] Add method to get activity title

--- view/src/main/java/org/xcolab/view/activityentry/ActivityEntryHelper.java
+++ view/src/main/java/org/xcolab/view/activityentry/ActivityEntryHelper.java
@@ -9,6 +9,7 @@ import org.xcolab.client.activities.pojo.ActivityEntry;
 import org.xcolab.view.activityentry.provider.ActivityEntryContentProvider;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ActivityEntryHelper {
@@ -23,18 +24,32 @@ public class ActivityEntryHelper {
     }
 
     public String getActivityBody(ActivityEntry entry) {
-        try {
-            for (ActivityEntryContentProvider provider : providerList) {
+        final ActivityEntryContentProvider provider = getProvider(entry);
+        return provider != null ? provider.getBody() : "";
+    }
 
-                if (provider.getPrimaryType() == entry.getPrimaryType().longValue()
-                        && provider.getSecondaryType() == entry.getSecondaryType().longValue()) {
-                    provider.setActivityEntry(entry);
-                    return provider.getBody();
-                }
+    public String getActivityTitle(ActivityEntry entry) {
+        final ActivityEntryContentProvider provider = getProvider(entry);
+        return provider != null ? provider.getTitle() : "";
+    }
+
+    private ActivityEntryContentProvider getProvider(ActivityEntry entry) {
+        try {
+            final Optional<ActivityEntryContentProvider> providerOpt = providerList.stream()
+                    .filter(provider -> provider.getPrimaryType() == entry.getPrimaryType()
+                            .longValue())
+                    .filter(provider -> provider.getSecondaryType() == entry.getSecondaryType()
+                            .longValue()).findAny();
+            if (!providerOpt.isPresent()) {
+                throw new IllegalStateException("Activity provider for entry "
+                        + entry.getActivityEntryId() + " does not exist");
             }
+            final ActivityEntryContentProvider provider = providerOpt.get();
+            provider.setActivityEntry(entry);
+            return provider;
         } catch (ActivityInitializationException e) {
             log.warn("Error when getting activity body: {}", e.getMessage());
+            return null;
         }
-        return "";
     }
 }
