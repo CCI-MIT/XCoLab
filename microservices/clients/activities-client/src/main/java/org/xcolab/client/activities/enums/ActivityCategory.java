16a0d796d063d34b3877c8f37687a3765d6acafc
Johannes Bachhuber <jobachhu@mit.edu>
2017-12-12 22:13:36 +0100

    [COLAB-2469] Add method to get type from category

--- microservices/clients/activities-client/src/main/java/org/xcolab/client/activities/enums/ActivityCategory.java
+++ microservices/clients/activities-client/src/main/java/org/xcolab/client/activities/enums/ActivityCategory.java
@@ -1,8 +1,25 @@
 package org.xcolab.client.activities.enums;
 
+import java.util.Arrays;
+
 public enum ActivityCategory {
-    MEMBER,
-    DISCUSSION,
-    PROPOSAL,
-    CONTEST;
+
+    MEMBER(ContestActivityType.values()),
+    DISCUSSION(DiscussionActivityType.values()),
+    PROPOSAL(ProposalActivityType.values()),
+    CONTEST(ContestActivityType.values());
+
+    private final ActivityType[] activityTypes;
+
+    ActivityCategory(ActivityType[] activityTypes) {
+        this.activityTypes = activityTypes;
+    }
+
+    public ActivityType getActivityType(String activityType) {
+        return Arrays.stream(activityTypes)
+                .filter(s -> s.name().equalsIgnoreCase(activityType))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "No enum constant " + name() + "." + activityType));
+    }
 }
