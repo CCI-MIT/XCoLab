79ccae991cdf5e7d30c6006fd492537ade35b098
Johannes Bachhuber <jobachhu@mit.edu>
2017-12-13 02:14:33 +0100

    [COLAB-2469] Add queries to populate new fields

--- sql/deployments/deployment-2017-12-11-COLAB-2469.sql
+++ sql/deployments/deployment-2017-12-11-COLAB-2469.sql
@@ -6,3 +6,38 @@ ALTER TABLE activities_ActivityEntry ADD activityType VARCHAR(75) NULL;
 ALTER TABLE activities_ActivityEntry
   MODIFY COLUMN activityCategory VARCHAR(30) AFTER createDate,
   MODIFY COLUMN activityType VARCHAR(75) AFTER activityCategory;
+
+-- Update activity type fields
+
+update activities_ActivityEntry set activityCategory = 'MEMBER' where primaryType = 10038;
+update activities_ActivityEntry set activityCategory = 'DISCUSSION' where primaryType = 39202;
+update activities_ActivityEntry set activityCategory = 'PROPOSAL' where primaryType = 1368503;
+update activities_ActivityEntry set activityCategory = 'CONTEST' where primaryType = 39701;
+
+-- Proposal activity types
+update activities_ActivityEntry set activityCategory = 'PROPOSAL', activityType = 'CREATED' where primaryType = 39701 and secondaryType = 0;
+update activities_ActivityEntry set activityType = 'UPDATED' where primaryType = 1368503 and secondaryType = 1;
+update activities_ActivityEntry set activityType = 'UPDATED' where primaryType = 1368503 and secondaryType = 2;
+update activities_ActivityEntry set activityType = 'VOTE_ADDED' where primaryType = 1368503 and secondaryType = 3;
+update activities_ActivityEntry set activityType = 'VOTE_RETRACTED' where primaryType = 1368503 and secondaryType = 4;
+update activities_ActivityEntry set activityType = 'VOTE_SWITCHED' where primaryType = 1368503 and secondaryType = 5;
+update activities_ActivityEntry set activityType = 'MEMBER_ADDED' where primaryType = 1368503 and secondaryType = 6;
+update activities_ActivityEntry set activityType = 'MEMBER_REMOVED' where primaryType = 1368503 and secondaryType = 7;
+update activities_ActivityEntry set activityType = 'SUPPORT_ADDED' where primaryType = 1368503 and secondaryType = 8;
+update activities_ActivityEntry set activityType = 'SUPPORT_REMOVED' where primaryType = 1368503 and secondaryType = 9;
+
+-- Member activity types
+update activities_ActivityEntry set activityType = 'REGISTERED' where primaryType = 10038 and secondaryType = 1;
+
+-- Comments
+update activities_ActivityEntry set activityCategory = 'PROPOSAL', activityType = 'COMMENT_ADDED' where primaryType = 39202 and secondaryType = 1;
+update activities_ActivityEntry set activityCategory = 'CONTEST', activityType = 'COMMENT_ADDED' where primaryType = 39202 and secondaryType = 6;
+update activities_ActivityEntry set activityCategory = 'DISCUSSION', activityType = 'THREAD_ADDED' where primaryType = 39202 and secondaryType = 3;
+update activities_ActivityEntry set activityCategory = 'DISCUSSION', activityType = 'COMMENT_ADDED' where primaryType = 39202 and secondaryType = 5;
+
+-- Delete add category activities
+delete from activities_ActivityEntry where primaryType = 39203 and secondaryType = 1;
+delete from activities_ActivityEntry where activityEntryId in (52007, 1002426, 1462191);
+
+-- Delete liferay's upload picture activities
+delete from activities_ActivityEntry where primaryType = 10054;
