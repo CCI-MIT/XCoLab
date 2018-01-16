16fbbf73171d995fb2e7d9d468b29af629a5638b
cwoebker <cwoebker@gmail.com>
2018-01-16 12:08:43 -0500

    Finish SQL statement in order to fix NULL values in members table

--- sql/fix_null_values_colab-2495.sql
+++ sql/fix_null_values_colab-2495.sql
@@ -1,8 +1,18 @@
-Select * from xcolab.members_Member where members_Member.createDate is  NULL ;
+# Display all entries that have the issue
+SELECT * FROM xcolab.members_Member WHERE members_Member.status IS  NULL ;
 
-UPDATE xcolab.members_Member set status=0 where status is NULL;
-UPDATE xcolab.members_Member set autoRegisteredMemberStatus=0 where autoRegisteredMemberStatus is NULL;
-UPDATE xcolab.members_Member set loginIP='' where loginIP is NULL;
+# Display entries without a createDate and the replacement createDate (DryRun)
+SELECT T1.id_, T1.screenName, T1.createDate, T2.createDate
+FROM xcolab.members_Member T1, xcolab.members_Member T2
+WHERE T1.id_ = T2.id_ + 1 AND T1.createDate IS NULL;
 
-UPDATE xcolab.members_Member set createDate=() where createDate is NULL;
-UPDATE xcolab.members_Member set loginDate=createDate where loginDate is NULL;
+# Update status fields
+UPDATE xcolab.members_Member SET status=0 WHERE status IS NULL;
+UPDATE xcolab.members_Member SET autoRegisteredMemberStatus=0 WHERE autoRegisteredMemberStatus IS NULL;
+
+# Update createDate field
+UPDATE xcolab.members_Member AS T1
+JOIN xcolab.members_Member AS T2
+    ON T1.id_-1 = T2.id_
+SET T1.createDate = T2.createDate
+WHERE T1.createDate IS NULL;
