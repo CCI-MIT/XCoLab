0320216eeb620512c22042edf9794427706f4f60
cwoebker <cwoebker@gmail.com>
2018-01-30 12:09:07 -0500

    Remove remaining "xcolab" table name references

--- sql/deployments/deployment-2018-01-16-COLAB-2495-fix-member-null-fields.sql
+++ sql/deployments/deployment-2018-01-16-COLAB-2495-fix-member-null-fields.sql
@@ -7,12 +7,12 @@
 # WHERE T1.id_ = T2.id_ + 1 AND T1.createDate IS NULL;
 
 # Update status fields
-UPDATE xcolab.members_Member SET status=0 WHERE status IS NULL;
-UPDATE xcolab.members_Member SET autoRegisteredMemberStatus=0 WHERE autoRegisteredMemberStatus IS NULL;
+UPDATE members_Member SET status=0 WHERE status IS NULL;
+UPDATE members_Member SET autoRegisteredMemberStatus=0 WHERE autoRegisteredMemberStatus IS NULL;
 
 # Update createDate field
-UPDATE xcolab.members_Member AS T1
-JOIN xcolab.members_Member AS T2
+UPDATE members_Member AS T1
+JOIN members_Member AS T2
     ON T1.id_-1 = T2.id_
 SET T1.createDate = T2.createDate
 WHERE T1.createDate IS NULL;
