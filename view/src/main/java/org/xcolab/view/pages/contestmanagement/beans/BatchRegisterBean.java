d9630f18f874550a431f88644ff4a46bfa4956f2
cwoebker <cwoebker@gmail.com>
2018-02-09 19:58:41 -0500

    Add new BatchRegisterLineBean to split up different batch entries

--- view/src/main/java/org/xcolab/view/pages/contestmanagement/beans/BatchRegisterBean.java
+++ view/src/main/java/org/xcolab/view/pages/contestmanagement/beans/BatchRegisterBean.java
@@ -1,24 +1,10 @@
 package org.xcolab.view.pages.contestmanagement.beans;
 
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
 import java.io.Serializable;
 
 public class BatchRegisterBean implements Serializable {
-
-    private static final String EMAIL_REGEX =
-            "(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zA-Z0-9-]*[a-zA-Z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-
-    @NotBlank
-    private String firstName;
-
-    @NotBlank
-    private String lastName;
-
-    @Email(regexp = EMAIL_REGEX)
-    @NotBlank
-    private String email;
 
     @NotBlank
     private Boolean asGuests;
@@ -42,34 +28,9 @@ public class BatchRegisterBean implements Serializable {
         this.asGuests = asGuests;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     @Override
     public String toString() {
-        return "BatchRegisterBean [firstName='" + firstName + "', lastName='" + lastName
-                + "', email='" + email + "']";
+        return "BatchRegisterBean [batchText='" + batchText + "', asGuests='" + asGuests.toString() + "']";
     }
 
 }
