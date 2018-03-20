a178ab7969f69329aca4fea8b5b7458121002f04
Johannes Bachhuber <jobachhu@mit.edu>
2018-03-20 02:35:58 +0100

    [COLAB-2733] Fix mixed content in comments

--- sql/deployments/2018-03-20-COLAB-2733-mixed-content.sql
+++ sql/deployments/2018-03-20-COLAB-2733-mixed-content.sql
@@ -1,7 +1,7 @@
+-- Content articles
 update xcolab_ContentArticleVersion set content = replace(content,'http://climatecolab.org','https://www.climatecolab.org');
 update xcolab_ContentArticleVersion set content = replace(content,'http://www.climatecolab.org','https://www.climatecolab.org');
 update xcolab_ContentArticleVersion set content = replace(content,'https://climatecolab.org','https://www.climatecolab.org');
-
 update xcolab_ContentArticleVersion set content = replace(content,'http://i.imgur.com','https://i.imgur.com');
 update xcolab_ContentArticleVersion set content = replace(content,'http://imgur.com','https://i.imgur.com');
 update xcolab_ContentArticleVersion set content = replace(content,'http://youtube.com','https://www.youtube.com');
@@ -9,3 +9,15 @@ update xcolab_ContentArticleVersion set content = replace(content,'http://energy
 update xcolab_ContentArticleVersion set content = replace(content,'http://lae.mit.edu','https://lae.mit.edu');
 update xcolab_ContentArticleVersion set content = replace(content,'http://environment.yale.edu','https://environment.yale.edu');
 update xcolab_ContentArticleVersion set content = replace(content,'http://static3.businessinsider.com','https://static3.businessinsider.com');
+
+-- Comments
+update comment_Comment set content = replace(content,'http://climatecolab.org','https://www.climatecolab.org');
+update comment_Comment set content = replace(content,'http://www.climatecolab.org','https://www.climatecolab.org');
+update comment_Comment set content = replace(content,'https://climatecolab.org','https://www.climatecolab.org');
+update comment_Comment set content = replace(content,'http://i.imgur.com','https://i.imgur.com');
+update comment_Comment set content = replace(content,'http://imgur.com','https://i.imgur.com');
+update comment_Comment set content = replace(content,'http://youtube.com','https://www.youtube.com');
+update comment_Comment set content = replace(content,'http://energy.mit.edu','https://energy.mit.edu');
+update comment_Comment set content = replace(content,'http://lae.mit.edu','https://lae.mit.edu');
+update comment_Comment set content = replace(content,'http://environment.yale.edu','https://environment.yale.edu');
+update comment_Comment set content = replace(content,'http://static3.businessinsider.com','https://static3.businessinsider.com');
