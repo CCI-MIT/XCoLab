97c8459134f623cb8235fd21788a14e2f8ee79fa
Johannes Bachhuber <jobachhu@mit.edu>
2018-03-20 02:16:14 +0100

    [COLAB-2733] Add mixed content migration script

---
+++ sql/deployments/2018-03-20-COLAB-2733-mixed-content.sql
@@ -0,0 +1,11 @@
+update xcolab_ContentArticleVersion set content = replace(content,'http://climatecolab.org','https://www.climatecolab.org');
+update xcolab_ContentArticleVersion set content = replace(content,'http://www.climatecolab.org','https://www.climatecolab.org');
+update xcolab_ContentArticleVersion set content = replace(content,'https://climatecolab.org','https://www.climatecolab.org');
+
+update xcolab_ContentArticleVersion set content = replace(content,'http://i.imgur.com','https://i.imgur.com');
+update xcolab_ContentArticleVersion set content = replace(content,'http://imgur.com','https://i.imgur.com');
+update xcolab_ContentArticleVersion set content = replace(content,'http://youtube.com','https://www.youtube.com');
+update xcolab_ContentArticleVersion set content = replace(content,'http://energy.mit.edu','https://energy.mit.edu');
+update xcolab_ContentArticleVersion set content = replace(content,'http://lae.mit.edu','https://lae.mit.edu');
+update xcolab_ContentArticleVersion set content = replace(content,'http://environment.yale.edu','https://environment.yale.edu');
+update xcolab_ContentArticleVersion set content = replace(content,'http://static3.businessinsider.com','https://static3.businessinsider.com');
