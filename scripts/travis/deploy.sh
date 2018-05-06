e304958615173c9cf290b44e863c09b456a355d1
Johannes Bachhuber <jobachhu@mit.edu>
2018-05-06 16:34:43 +0200

    Remove old binary storage server

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -10,12 +10,6 @@ if [ ${BUILD_TYPE} == 'deploy' ]; then
 
     WORKER_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-    # Temporarily copy files to second server
-    DEPLOY_FOLDER2=${TRAVIS_BRANCH}
-    DEPLOY_SERVER2=travis@cognosis.mit.edu
-    echo "Copying binaries from ${WORKER_IP} to ${DEPLOY_SERVER2}:${DEPLOY_FOLDER2} ..."
-    rsync -r --delete-after --quiet binaries ${DEPLOY_SERVER2}:${DEPLOY_FOLDER2}
-
     DEPLOY_FOLDER=xcolab/${TRAVIS_BRANCH}
     DEPLOY_SERVER=binaries@cognosis2.mit.edu
     echo "Copying binaries from ${WORKER_IP} to ${DEPLOY_SERVER}:${DEPLOY_FOLDER} ..."
