a0ac825a4613032474a08c3c6ed905c21dc72023
Johannes Bachhuber <jobachhu@mit.edu>
2018-05-09 01:57:20 +0200

    [COLAB-2777] Add OpenID Connect id_token support

--- view/src/main/java/org/xcolab/view/config/spring/beans/SsoServerConfig.java
+++ view/src/main/java/org/xcolab/view/config/spring/beans/SsoServerConfig.java
@@ -1,6 +1,5 @@
 package org.xcolab.view.config.spring.beans;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
@@ -8,18 +7,22 @@ import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2A
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.xcolab.client.admin.attributes.platform.PlatformAttributeKey;
 import org.xcolab.client.members.pojo.Member;
+import org.xcolab.view.config.spring.sso.openid.OpenIdHelper;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -30,48 +33,48 @@ import java.util.Map;
 @Configuration
 public class SsoServerConfig extends OAuth2AuthorizationServerConfiguration {
 
+    private final TokenEnhancer tokenEnhancer;
+
     @Autowired
-    public SsoServerConfig(BaseClientDetails details,
-            AuthenticationManager authenticationManager, ObjectProvider<TokenStore> tokenStore,
+    public SsoServerConfig(BaseClientDetails details, AuthenticationManager authenticationManager,
+            ObjectProvider<TokenStore> tokenStore,
             ObjectProvider<AccessTokenConverter> tokenConverter,
-            AuthorizationServerProperties properties) {
+            AuthorizationServerProperties properties, TokenEnhancer tokenEnhancer) {
         super(details, authenticationManager, tokenStore, tokenConverter, properties);
+        this.tokenEnhancer = tokenEnhancer;
     }
 
     @Override
-    public void configure(AuthorizationServerSecurityConfigurer security)
-            throws Exception {
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenEnhancer(tokenEnhancer);
+        super.configure(endpoints);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.allowFormAuthenticationForClients();
         super.configure(security);
     }
 
-    @GetMapping("/api/user")
-    public Map<String, String> user(Member member) {
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("sub", Long.toString(member.getId_()));
-        map.put("name", member.getFullName());
-        map.put("given_name", member.getFirstName());
-        map.put("family_name", member.getLastName());
-        map.put("email", member.getEmailAddress());
-        map.put("preferred_username", member.getScreenName());
-        if (StringUtils.isNotEmpty(member.getAbsoluteImageUrl())) {
-            map.put("picture", member.getAbsoluteImageUrl());
+    @GetMapping(OpenIdHelper.OAUTH_USERINFO_ENDPOINT)
+    public Map<String, Object> user(Member member) {
+        OAuth2Authentication authentication =
+                (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
+        final OpenIdHelper openIdHelper = new OpenIdHelper(authentication);
+        Map<String, Object> map = new LinkedHashMap<>();
+        openIdHelper.addSubjectField(map, member);
+        if (openIdHelper.hasEmailScope()) {
+            openIdHelper.addEmailScopedFields(map, member);
+        }
+        if (openIdHelper.hasProfileScope()) {
+            openIdHelper.addProfileScopedFields(map, member);
         }
         return map;
     }
 
-    @GetMapping("/oauth-configuration")
+    @GetMapping("/.well-known/openid-configuration")
     public Map<String, Object> openIdConfiguration() {
-        Map<String, Object> map = new LinkedHashMap<>();
-        final String url = PlatformAttributeKey.COLAB_URL.get();
-        map.put("issuer", url);
-        map.put("authorization_endpoint", url + "/oauth/authorize");
-        map.put("token_endpoint", url + "/oauth/token");
-        map.put("userinfo_endpoint", url + "/api/user");
-        map.put("token_endpoint_auth_methods_supported", new String[]{"client_secret_basic", "client_secret_post"});
-        map.put("scopes_supported", new String[] {"email", "profile"});
-        map.put("claims_supported", new String[] {"sub", "email", "name", "given_name", "family_name", "picture"});
-        return map;
+        return OpenIdHelper.getOpenIdConfiguration();
     }
 
     @Configuration
@@ -80,8 +83,8 @@ public class SsoServerConfig extends OAuth2AuthorizationServerConfiguration {
 
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            http.antMatcher("/api/user")
-                    .authorizeRequests().anyRequest().authenticated();
+            http.antMatcher(OpenIdHelper.OAUTH_USERINFO_ENDPOINT).authorizeRequests().anyRequest()
+                    .authenticated();
         }
     }
 }
