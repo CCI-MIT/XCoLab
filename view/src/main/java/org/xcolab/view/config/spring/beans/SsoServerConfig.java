fba048b8afd074b04f8610ee6d316004a21a8a7f
Johannes Bachhuber <jobachhu@mit.edu>
2018-05-21 01:02:11 +0200

    [COLAB-2777] Make SsoServerConfig conditional

--- view/src/main/java/org/xcolab/view/config/spring/beans/SsoServerConfig.java
+++ view/src/main/java/org/xcolab/view/config/spring/beans/SsoServerConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +38,7 @@ import java.util.Map;
 @SuppressWarnings("ProhibitedExceptionDeclared")
 @EnableAuthorizationServer
 @RestController
+@ConditionalOnBean(BaseClientDetails.class)
 @Configuration
 public class SsoServerConfig extends OAuth2AuthorizationServerConfiguration {
 
