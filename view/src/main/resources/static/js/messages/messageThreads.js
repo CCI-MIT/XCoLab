f8d4975ed7651ed8cbbdb651b593694e31491c77
Miguel <gamallomiguel@gmail.com>
2018-06-29 11:10:49 -0400

    [COLAB-1087] Identify previous messages in thread

---
+++ view/src/main/resources/static/js/messages/messageThreads.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    identifyPreviousMessages();
+    removeSpaces();
+});
+function identifyPreviousMessages(){
+    var beginPattern="-- original message begin --";
+    var endPattern="-- original message end --";
+    var message=$("#messageContents")[0].innerHTML;
+
+    while (message.search(beginPattern)!=-1 && message.search(endPattern)!=-1) {
+        try {
+            message=message.replace(beginPattern,"<div class=\"c-Previous__message\">");
+            var pos=message.lastIndexOf(endPattern);
+            message=message.substring(0,pos)+"</div>"+message.substring(pos+endPattern.length);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    $("#messageContents")[0].innerHTML=message;
+}
+function removeSpaces(){
+    $("#messageContents")[0].innerHTML=$("#messageContents")[0].innerHTML.replace(/<p>(&nbsp;|<br> *)<\/p>/g,'');
+}
