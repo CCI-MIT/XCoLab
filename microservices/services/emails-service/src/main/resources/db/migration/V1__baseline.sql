41b9c309a3b8c766ad224d2f37b912d56d064a2f
Johannes Bachhuber <jobachhu@mit.edu>
2018-07-19 01:05:45 +0200

    [COLAB-2313] Fix index length for utf8mb4 fields

--- microservices/services/emails-service/src/main/resources/db/migration/V1__baseline.sql
+++ microservices/services/emails-service/src/main/resources/db/migration/V1__baseline.sql
@@ -1,12 +1,14 @@
 CREATE TABLE IF NOT EXISTS `xcolab_ColabEmail` (
   `colabEmailId` BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `emailSubject` VARCHAR(255) NOT NULL COMMENT '	',
-  `emailBody` TEXT NOT NULL COMMENT '			',
-  `emailTo` VARCHAR(255) NOT NULL,
-  `emailFrom` VARCHAR(255) NOT NULL,
+  `emailSubject` VARCHAR(191) NOT NULL,
+  `emailBody` TEXT NOT NULL,
+  `emailTo` VARCHAR(191) NOT NULL,
+  `emailFrom` VARCHAR(191) NOT NULL,
   `dateSent` DATETIME NOT NULL,
   `referenceId` BIGINT(20) NULL,
-  `emailBodyHash` VARCHAR(255) NOT NULL,
+  `emailBodyHash` VARCHAR(64) NOT NULL,
   `sent` TINYINT(1) NULL DEFAULT 0,
-  INDEX `index2` (`emailSubject` ASC, `emailTo` ASC, `dateSent` ASC, `emailBodyHash` ASC)
+  INDEX `index_xcolab_ColabEmail_emailSubject` (`emailSubject`),
+  INDEX `index_xcolab_ColabEmail_emailTo` (`emailTo`),
+  INDEX `index_xcolab_ColabEmail_dateSent` (`dateSent`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
--- sql/deployments/2018-07-17-pre-automatic-database-migrations.sql
+++ sql/deployments/2018-07-17-pre-automatic-database-migrations.sql
@@ -8,3 +8,12 @@ ALTER TABLE xcolab_PlatformTeamMember DROP FOREIGN KEY FK_big_ontology_term;
 ALTER TABLE xcolab_PlatformTeamMember DROP FOREIGN KEY FK_ontology_term_to_load;
 ALTER TABLE xcolab_PlatformTeamMember DROP FOREIGN KEY FK_parent;
 ALTER TABLE xcolab_PlatformTeamMember DROP FOREIGN KEY FK_small_ontology_term;
+
+--
+-- Fix weird indexes
+--
+
+DROP INDEX index2 on xcolab_ColabEmail;
+CREATE INDEX `index_xcolab_ColabEmail_emailSubject` on xcolab_ColabEmail (`emailSubject`);
+CREATE INDEX `index_xcolab_ColabEmail_emailTo` on xcolab_ColabEmail (`emailTo`);
+CREATE INDEX `index_xcolab_ColabEmail_dateSent` on xcolab_ColabEmail (`dateSent`);
