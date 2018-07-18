39c8fb726c13f34cee9757c698e2319ef3356b19
Johannes Bachhuber <jobachhu@mit.edu>
2018-07-19 01:05:22 +0200

    [COLAB-2313] Remove unused import

--- microservices/services/contest-service/src/test/java/org/xcolab/service/contest/web/ContestControllerTest.java
+++ microservices/services/contest-service/src/test/java/org/xcolab/service/contest/web/ContestControllerTest.java
@@ -17,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
