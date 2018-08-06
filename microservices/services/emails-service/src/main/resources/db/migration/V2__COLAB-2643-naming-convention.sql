d46ff113fdfa28a6580fb1ff3fba4054ca50dac0
Johannes Bachhuber <jobachhu@mit.edu>
2018-08-07 01:46:11 +0200

    [COLAB-2643] Fix utf8mb4 key lengths

--- microservices/services/emails-service/src/main/resources/db/migration/V2__COLAB-2643-naming-convention.sql
+++ microservices/services/emails-service/src/main/resources/db/migration/V2__COLAB-2643-naming-convention.sql
@@ -1,11 +1,11 @@
 rename table xcolab_ColabEmail to email__outgoing_email;
 
 ALTER TABLE email__outgoing_email CHANGE colabEmailId id bigint(20) NOT NULL auto_increment;
-ALTER TABLE email__outgoing_email CHANGE emailSubject email_subject varchar(255) NOT NULL COMMENT '  ';
+ALTER TABLE email__outgoing_email CHANGE emailSubject email_subject varchar(191) NOT NULL COMMENT '  ';
 ALTER TABLE email__outgoing_email CHANGE emailBody email_body text NOT NULL COMMENT '      ';
-ALTER TABLE email__outgoing_email CHANGE emailTo email_to varchar(255) NOT NULL;
-ALTER TABLE email__outgoing_email CHANGE emailFrom email_from varchar(255) NOT NULL;
+ALTER TABLE email__outgoing_email CHANGE emailTo email_to varchar(191) NOT NULL;
+ALTER TABLE email__outgoing_email CHANGE emailFrom email_from varchar(191) NOT NULL;
 ALTER TABLE email__outgoing_email CHANGE dateSent sent_at datetime NOT NULL;
 ALTER TABLE email__outgoing_email CHANGE referenceId reference_id bigint(20);
-ALTER TABLE email__outgoing_email CHANGE emailBodyHash email_body_hash varchar(255) NOT NULL;
+ALTER TABLE email__outgoing_email CHANGE emailBodyHash email_body_hash varchar(64) NOT NULL;
 ALTER TABLE email__outgoing_email CHANGE sent was_sent tinyint(1) DEFAULT '0';
