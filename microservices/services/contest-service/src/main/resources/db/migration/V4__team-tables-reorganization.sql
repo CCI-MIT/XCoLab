b33bf1eee8f22fdd5f0cbb3b6d05c34b1f45ff56
Johannes Bachhuber <jobachhu@mit.edu>
2018-08-07 00:40:05 +0200

    [COLAB-2643] Add workaround to make cross-service migration script repeatable

--- microservices/services/contest-service/src/main/resources/db/migration/V4__team-tables-reorganization.sql
+++ microservices/services/contest-service/src/main/resources/db/migration/V4__team-tables-reorganization.sql
@@ -7,6 +7,10 @@ CREATE TABLE IF NOT EXISTS contest__proposal_team_member
 
 CREATE INDEX contest__proposal_team_member_userId_index ON contest__proposal_team_member (user_id);
 
+
+-- Workaround to make this work in the build system, where Users_Groups doesn't exist:
+create table if not exists Users_Groups(groupId bigint not null, userId bigint not null);
+
 insert into contest__proposal_team_member (proposal_id, user_id)
   select proposal.proposalId, ug.userId from xcolab_Proposal proposal
     join Users_Groups ug on ug.groupId = proposal.groupId;
