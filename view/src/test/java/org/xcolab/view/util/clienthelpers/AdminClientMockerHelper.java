4db3aba7c4393c41a384d8003cc447dd282f525b
Johannes Bachhuber <jobachhu@mit.edu>
2018-08-28 01:57:25 +0200

    [COLAB-2889] Default config attributes in tests

--- view/src/test/java/org/xcolab/view/util/clienthelpers/AdminClientMockerHelper.java
+++ view/src/test/java/org/xcolab/view/util/clienthelpers/AdminClientMockerHelper.java
@@ -4,31 +4,23 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 
 import org.xcolab.client.admin.AdminClient;
-import org.xcolab.client.admin.pojo.ConfigurationAttribute;
+import org.xcolab.client.admin.exceptions.ConfigurationAttributeNotFoundException;
 
 import static org.mockito.Matchers.anyString;
 
 public class AdminClientMockerHelper {
 
-    public static void mockAdminClient(){
+    public static void mockAdminClient() {
 
         PowerMockito.mockStatic(AdminClient.class);
         //generic mock for ConfigAttribute
         Mockito.when(AdminClient.getConfigurationAttribute(anyString(), anyString()))
                 .thenAnswer(invocation -> {
                     Object[] arguments = invocation.getArguments();
-                    if (arguments != null && arguments.length > 0 && arguments[0] != null) {
-                        String key = (String) arguments[0];
-                        if (key.equals("ACTIVE_THEME")) {
-                            return new ConfigurationAttribute("ACTIVE_THEME", 0L, 0L,
-                                    "CLIMATE_COLAB", 0d);
-                        } else {
-                            return new ConfigurationAttribute(key, 0L, 0L, "12312312332", 0d);
-                        }
-                    }
+                    String key = (String) arguments[0];
 
-                    return new ConfigurationAttribute("GOOGLE_RECAPTCHA_SITE_KEY", 0L, 0L,
-                            "12312312332", 0d);
+                    // throw not found exception so the default value is used
+                    throw new ConfigurationAttributeNotFoundException(key);
                 });
     }
 }
