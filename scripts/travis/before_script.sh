2b8ad50528b9618b10a54f0a4c85608f2d3dc31b
Johannes Bachhuber <jobachhu@mit.edu>
2018-09-04 18:02:26 +0200

    Remove schema creation from travis script

--- scripts/travis/before_script.sh
+++ scripts/travis/before_script.sh
@@ -5,7 +5,6 @@ echo "MAVEN_OPTS='-Xmx1536m -XX:+TieredCompilation -XX:TieredStopAtLevel=1'" > ~
 
 echo "Running create database script"
 mysql -e 'CREATE DATABASE IF NOT EXISTS xcolab;'
-mysql -u root xcolab < sql/starter/xcolab-schema.sql
 
 echo "Copying application properties file"
 cp microservices/services/src/main/resources/application.properties $HOME/.xcolab.application.properties
