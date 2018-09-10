c89883cdd9b3e0f51ccf89fdf4bfab374910ae1a
Nikolai Kraler <nikolaikraler@gmail.com>
2018-09-10 16:01:00 -0400

    [COLAB-2815]: delete remaining file

--- microservices/services/contest-service/src/main/resources/db/migration/V8__COLAB-2815-show-TOS-only-once.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS contest__tos_agreement
-(
-  contest_id BIGINT(20) NOT NULL,
-  user_id BIGINT(20) NOT NULL,
-  agreed TINYINT(4) DEFAULT NULL,
-  PRIMARY KEY (contest_id, user_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
