6e72816f28b1c02b3ee8bad9b03ef2b0c5cfa5aa
Johannes Bachhuber <jobachhu@mit.edu>
2018-10-03 22:39:13 +0200

    [COLAB-2921] Add sentry error monitoring

--- view/src/main/java/org/xcolab/view/theme/CredentialVariables.java
+++ view/src/main/java/org/xcolab/view/theme/CredentialVariables.java
@@ -1,18 +1,21 @@
 package org.xcolab.view.theme;
 
 import org.xcolab.client.admin.attributes.configuration.ConfigurationAttributeKey;
+import org.xcolab.client.admin.attributes.platform.PlatformAttributeKey;
 
 public class CredentialVariables {
     private final String googleAnalyticsKey;
     private final String pingdomRumId;
     private final String typekitId;
     private final String typekitIdLocal;
+    private final String sentryDsn;
 
     public CredentialVariables() {
         this.googleAnalyticsKey = ConfigurationAttributeKey.GOOGLE_ANALYTICS_KEY.get();
         this.pingdomRumId = ConfigurationAttributeKey.PINGDOM_RUM_ID.get();
         this.typekitId = ConfigurationAttributeKey.TYPEKIT_KIT_ID.get();
         this.typekitIdLocal = ConfigurationAttributeKey.TYPEKIT_KIT_ID_LOCALHOST.get();
+        this.sentryDsn = PlatformAttributeKey.SENTRY_FRONTEND_DSN_PUBLIC.get();
     }
 
     public String getGoogleAnalyticsKey() {
@@ -29,5 +32,9 @@ public class CredentialVariables {
 
     public String getTypekitIdLocal() {
         return typekitIdLocal;
+    }
+
+    public String getSentryDsn() {
+        return sentryDsn;
     }
 }
