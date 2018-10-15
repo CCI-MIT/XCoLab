85dab64a0404cea11a0fa605dd865d2be0eb932d
Johannes Bachhuber <jobachhu@mit.edu>
2018-10-15 19:24:58 +0200

    [COLAB-2918] Only add ThemeContext if view exists

--- view/src/main/java/org/xcolab/view/theme/PopulateLayoutVariablesFilter.java
+++ view/src/main/java/org/xcolab/view/theme/PopulateLayoutVariablesFilter.java
@@ -3,6 +3,7 @@ package org.xcolab.view.theme;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,12 +15,21 @@ public class PopulateLayoutVariablesFilter extends HandlerInterceptorAdapter {
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
             Object handler, ModelAndView modelAndView) {
 
-        final Object attribute =
-                request.getAttribute(ThemeVariableFilter.REQUEST_ATTRIBUTE_NAME);
+        if (modelAndView != null && !isRedirectView(modelAndView)) {
+            final Object attribute =
+                    request.getAttribute(ThemeVariableFilter.REQUEST_ATTRIBUTE_NAME);
 
-        if (attribute != null) {
+            if (attribute == null) {
+                throw new IllegalStateException("ThemeContext variable was not populated in request: "
+                                + request.getRequestURI());
+            }
             ThemeContext themeContext = (ThemeContext) attribute;
             modelAndView.addObject("_themeContext", themeContext);
         }
+    }
+
+    private boolean isRedirectView(ModelAndView modelAndView) {
+        return (modelAndView.getView() != null && modelAndView.getView() instanceof RedirectView)
+                || modelAndView.getViewName().startsWith("redirect:");
     }
 }
