c0c983c46c3313073970240cd85d8b4fc4e332df
Johannes Bachhuber <jobachhu@mit.edu>
2018-11-02 09:40:43 +0100

    Remove local typekit distinction

--- view/src/main/java/org/xcolab/view/theme/CredentialVariables.java
+++ view/src/main/java/org/xcolab/view/theme/CredentialVariables.java
@@ -7,14 +7,12 @@ public class CredentialVariables {
     private final String googleAnalyticsKey;
     private final String pingdomRumId;
     private final String typekitId;
-    private final String typekitIdLocal;
     private final String sentryDsn;
 
     public CredentialVariables() {
         this.googleAnalyticsKey = ConfigurationAttributeKey.GOOGLE_ANALYTICS_KEY.get();
         this.pingdomRumId = ConfigurationAttributeKey.PINGDOM_RUM_ID.get();
         this.typekitId = ConfigurationAttributeKey.TYPEKIT_KIT_ID.get();
-        this.typekitIdLocal = ConfigurationAttributeKey.TYPEKIT_KIT_ID_LOCALHOST.get();
         this.sentryDsn = PlatformAttributeKey.SENTRY_FRONTEND_DSN_PUBLIC.get();
     }
 
@@ -28,10 +26,6 @@ public class CredentialVariables {
 
     public String getTypekitId() {
         return typekitId;
-    }
-
-    public String getTypekitIdLocal() {
-        return typekitIdLocal;
     }
 
     public String getSentryDsn() {
