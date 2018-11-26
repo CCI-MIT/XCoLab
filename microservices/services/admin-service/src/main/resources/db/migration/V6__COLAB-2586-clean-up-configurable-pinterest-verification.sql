31b56f586b5083a7407c31244f21a1ecc82552d0
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2018-11-26 12:00:03 -0500

    [COLAB-2586] repair admin-service pinterest ID db migration

---
+++ microservices/services/admin-service/src/main/resources/db/migration/V6__COLAB-2586-clean-up-configurable-pinterest-verification.sql
@@ -0,0 +1,9 @@
+SET @isClimateColab = (SELECT EXISTS(SELECT * FROM admin__configuration_attribute WHERE name='COLAB_NAME' AND string_value='Climate CoLab'));
+
+-- Delete database entry from previous migration
+DELETE admin__configuration_attribute WHERE name='PINTEREST_ID'
+
+-- BEGIN: Climate CoLab specific
+INSERT INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value)
+SELECT ('PINTEREST_ID', 0, null, '70e4477f0ab70225da1533151386c16f', null)
+WHERE @isClimateColab;
