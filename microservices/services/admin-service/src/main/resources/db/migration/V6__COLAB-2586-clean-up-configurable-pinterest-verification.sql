76722a4ea807ebeb8d112f43f322b12203a39e47
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2018-11-26 15:30:29 -0500

    [COLAB-2586] add dual in sql statement

--- microservices/services/admin-service/src/main/resources/db/migration/V6__COLAB-2586-clean-up-configurable-pinterest-verification.sql
+++ microservices/services/admin-service/src/main/resources/db/migration/V6__COLAB-2586-clean-up-configurable-pinterest-verification.sql
@@ -7,4 +7,5 @@ WHERE name='PINTEREST_ID'
 -- BEGIN: Climate CoLab specific
 INSERT INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value)
 SELECT ('PINTEREST_ID', 0, null, '70e4477f0ab70225da1533151386c16f', null)
+FROM DUAL
 WHERE @isClimateColab;
