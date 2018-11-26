8deec0e06afa97a11345a33ef9d695285fba85fb
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2018-11-26 14:34:51 -0500

    [COLAB-2586] adjust SQL statement

--- microservices/services/admin-service/src/main/resources/db/migration/V6__COLAB-2586-clean-up-configurable-pinterest-verification.sql
+++ microservices/services/admin-service/src/main/resources/db/migration/V6__COLAB-2586-clean-up-configurable-pinterest-verification.sql
@@ -1,7 +1,8 @@
 SET @isClimateColab = (SELECT EXISTS(SELECT * FROM admin__configuration_attribute WHERE name='COLAB_NAME' AND string_value='Climate CoLab'));
 
 -- Delete database entry from previous migration
-DELETE admin__configuration_attribute WHERE name='PINTEREST_ID'
+DELETE FROM admin__configuration_attribute
+WHERE name='PINTEREST_ID'
 
 -- BEGIN: Climate CoLab specific
 INSERT INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value)
