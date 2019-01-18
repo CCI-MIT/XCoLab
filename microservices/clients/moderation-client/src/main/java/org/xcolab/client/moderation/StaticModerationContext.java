447a06f9b762d4cc13d67961d834e3bb3af4c1f6
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2019-01-18 12:08:47 -0500

    [COLAB-2961] assert not null in static moderation context

--- microservices/clients/moderation-client/src/main/java/org/xcolab/client/moderation/StaticModerationContext.java
+++ microservices/clients/moderation-client/src/main/java/org/xcolab/client/moderation/StaticModerationContext.java
@@ -1,14 +1,19 @@
 package org.xcolab.client.moderation;
 
+import org.springframework.util.Assert;
+
 public class StaticModerationContext {
 
     private static IModerationClient moderationClient;
+
+    private StaticModerationContext() { }
 
     public static IModerationClient getModerationClient() {
         return moderationClient;
     }
 
     public static void setModerationClient(IModerationClient moderationClient) {
+        Assert.notNull(moderationClient, "moderationClient must not be null!");
         StaticModerationContext.moderationClient = moderationClient;
     }
 }
