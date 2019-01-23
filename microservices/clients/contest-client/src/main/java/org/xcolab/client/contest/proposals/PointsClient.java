82c96de96dee846e5c46e084e997a422cf887bf9
Nikolai Kraler <nikolaikraler@gmail.com>
2019-01-23 12:44:58 -0500

    [COLAB-2918]-[contest]: add missing file

--- microservices/clients/contest-client/src/main/java/org/xcolab/client/contest/proposals/PointsClient.java
+++ microservices/clients/contest-client/src/main/java/org/xcolab/client/contest/proposals/PointsClient.java
@@ -1,93 +1,59 @@
 package org.xcolab.client.contest.proposals;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import org.xcolab.client.contest.pojo.IPointsDistributionConfiguration;
 import org.xcolab.client.contest.pojo.wrapper.PointTypeWrapper;
-import org.xcolab.util.http.caching.CacheKeys;
-import org.xcolab.util.http.caching.CacheName;
-import org.xcolab.util.http.client.RestResource1;
-import org.xcolab.util.http.exceptions.EntityNotFoundException;
 
 import java.util.List;
 
-public final class PointsClient {
+@FeignClient("xcolab-contest-service")
+public interface PointsClient {
 
-    private final RestResource1<IPointsDistributionConfiguration, Long>
-            pointsDistributionConfigurationResource = null; // pointsDistributionConfigurations
-    private final RestResource1<PointTypeWrapper, Long> pointTypeResource = null; // pointTypes
+    @PostMapping("/pointsDistributionConfigurations")
+    IPointsDistributionConfiguration createPointsDistributionConfiguration(@RequestBody
+            IPointsDistributionConfiguration pointsDistributionConfiguration);
 
-    public IPointsDistributionConfiguration createPointsDistributionConfiguration(
-            IPointsDistributionConfiguration pointsDistributionConfiguration) {
-        return pointsDistributionConfigurationResource
-                .create(pointsDistributionConfiguration)
-                .execute();
-    }
+    @GetMapping("/pointsDistributionConfigurations/targetProposalTemplateSectionDefinitionId")
+    IPointsDistributionConfiguration getPointsDistributionConfigurationByTargetProposalTemplateSectionDefinitionId(
+            @PathVariable("targetProposalTemplateSectionDefinitionId")
+                    Long targetProposalTemplateSectionDefinitionId);
 
-    public IPointsDistributionConfiguration
-    getPointsDistributionConfigurationByTargetProposalTemplateSectionDefinitionId(
-            long targetSectionDefinitionId) {
-        try {
-            return pointsDistributionConfigurationResource
-                    .collectionService("getByTargetProposalTemplateSectionDefinitionId",
-                            IPointsDistributionConfiguration.class)
-                    .queryParam("targetProposalTemplateSectionDefinitionId",
-                            targetSectionDefinitionId)
-                    .getChecked();
-        } catch (EntityNotFoundException ignored){
-            return null;
-        }
+    @PutMapping("/pointsDistributionConfigurations")
+    boolean updatePointsDistributionConfiguration(
+            @RequestBody IPointsDistributionConfiguration pointsDistributionConfiguration);
 
-    }
+    @GetMapping("/pointsDistributionConfigurations")
+    List<IPointsDistributionConfiguration> getPointsDistributionByProposalIdPointTypeId(
+            @RequestParam(value = "proposalId", required = false) Long proposalId,
+            @RequestParam(value = "pointTypeId", required = false) Long pointTypeId);
 
-    public boolean updatePointsDistributionConfiguration(
-            IPointsDistributionConfiguration pointsDistributionConfiguration) {
-        return pointsDistributionConfigurationResource
-                .update(pointsDistributionConfiguration,
-                        pointsDistributionConfiguration.getId())
-                .execute();
-    }
+    @DeleteMapping("/pointsDistributionConfigurations/{id}")
+    boolean deletePointsDistributionConfiguration(@PathVariable("id") Long id);
 
-    public List<IPointsDistributionConfiguration> getPointsDistributionByProposalIdPointTypeId(
-            Long proposalId, Long pointTypeId) {
-        return pointsDistributionConfigurationResource.list()
-                .optionalQueryParam("proposalId", proposalId)
-                .optionalQueryParam("pointTypeId", pointTypeId)
-                .execute();
-    }
+    @GetMapping("/pointsDistributionConfigurations/verifyDistributionConfigurationsForProposalId")
+    boolean verifyDistributionConfigurationsForProposalId(
+            @RequestParam("proposalId") Long proposalId);
 
-    public Boolean deletePointsDistributionConfiguration(Long pointsDistributionConfigurationId) {
-        return pointsDistributionConfigurationResource.delete(pointsDistributionConfigurationId)
-                .execute();
-    }
+    @DeleteMapping("/pointsDistributionConfigurations/removeByProposalId")
+    boolean deletePointsDistributionConfigurationByProposalId(
+            @RequestParam("proposalId") Long proposalId);
 
-    public void verifyDistributionConfigurationsForProposalId(Long proposalId) {
-         pointsDistributionConfigurationResource.collectionService("verifyDistributionConfigurationsForProposalId", String.class)
-                .queryParam("proposalId", proposalId).execute();
-    }
+    @GetMapping("/pointTypes/{pointTypeId}")
+    PointTypeWrapper getPointType(@PathVariable("pointTypeId") Long pointTypeId);
 
-    public Boolean deletePointsDistributionConfigurationByProposalId(Long proposalId) {
-        return pointsDistributionConfigurationResource.collectionService("removeByProposalId", Boolean.class)
-                .queryParam("proposalId", proposalId).delete();
-    }
+    @GetMapping("/pointTypes")
+    List<PointTypeWrapper> getPointTypes(
+            @RequestParam(value = "parentPointTypeId", required = false) Long parentPointTypeId);
 
-    public PointTypeWrapper getPointType(long Id) {
-        return pointTypeResource.get(Id)
-                .withCache(CacheKeys.of(PointTypeWrapper.class, Id), CacheName.MISC_REQUEST)
-                .execute();
-
-    }
-
-    public List<PointTypeWrapper> getAllPointTypes() {
-        return pointTypeResource.list()
-                .execute();
-    }
-
-    public List<PointTypeWrapper> getChildrenOfPointType(Long parentPointTypeId) {
-        return pointTypeResource.list()
-                .withCache(CacheKeys.withClass(PointTypeWrapper.class)
-                                .withParameter("parentPointTypeId", parentPointTypeId)
-                                .asList(),
-                        CacheName.MISC_MEDIUM)
-                .optionalQueryParam("parentPointTypeId", parentPointTypeId)
-                .execute();
+    default List<PointTypeWrapper> getAllPointTypes() {
+        return getPointTypes(null);
     }
 }
