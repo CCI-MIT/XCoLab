e4e8d95d8bade309ad0265bf6c15e82525a30ff1
Nikolai Kraler <nikolaikraler@gmail.com>
2019-01-28 18:01:42 -0500

    [COLAB-2918]-[contest]: fix compile error

--- microservices/services/contest-service/src/main/java/org/xcolab/service/contest/StaticInjector.java
+++ microservices/services/contest-service/src/main/java/org/xcolab/service/contest/StaticInjector.java
@@ -7,20 +7,20 @@ import org.xcolab.client.comment.ICategoryClient;
 import org.xcolab.client.comment.ICommentClient;
 import org.xcolab.client.comment.IThreadClient;
 import org.xcolab.client.comment.StaticCommentContext;
-import org.xcolab.client.contest.ContestClient;
-import org.xcolab.client.contest.ContestTeamMemberClient;
-import org.xcolab.client.contest.ImpactClient;
-import org.xcolab.client.contest.OntologyClient;
-import org.xcolab.client.contest.ProposalTemplateClient;
+import org.xcolab.client.contest.IContestClient;
+import org.xcolab.client.contest.IContestTeamMemberClient;
+import org.xcolab.client.contest.IImpactClient;
+import org.xcolab.client.contest.IOntologyClient;
+import org.xcolab.client.contest.IProposalTemplateClient;
 import org.xcolab.client.contest.StaticContestContext;
 import org.xcolab.client.contest.proposals.IMembershipClient;
+import org.xcolab.client.contest.proposals.IPointsClient;
+import org.xcolab.client.contest.proposals.IProposalAttributeClient;
 import org.xcolab.client.contest.proposals.IProposalClient;
 import org.xcolab.client.contest.proposals.IProposalJudgeRatingClient;
 import org.xcolab.client.contest.proposals.IProposalMemberRatingClient;
-import org.xcolab.client.contest.proposals.PointsClient;
-import org.xcolab.client.contest.proposals.ProposalAttributeClient;
-import org.xcolab.client.contest.proposals.ProposalMoveClient;
-import org.xcolab.client.contest.proposals.ProposalPhaseClient;
+import org.xcolab.client.contest.proposals.IProposalMoveClient;
+import org.xcolab.client.contest.proposals.IProposalPhaseClient;
 import org.xcolab.client.contest.proposals.StaticProposalContext;
 
 @Component
@@ -28,11 +28,11 @@ public class StaticInjector {
 
     @Autowired
     public StaticInjector(ICommentClient commentClient, ICategoryClient categoryClient,
-            IThreadClient threadClient, ContestClient contestClient,
-            ContestTeamMemberClient contestTeamMemberClient, ImpactClient impactClient,
-            OntologyClient ontologyClient, ProposalTemplateClient proposalTemplateClient,
-            PointsClient pointsClient, ProposalAttributeClient proposalAttributeClient,
-            ProposalMoveClient proposalMoveClient, ProposalPhaseClient proposalPhaseClient,
+            IThreadClient threadClient, IContestClient contestClient,
+            IContestTeamMemberClient contestTeamMemberClient, IImpactClient impactClient,
+            IOntologyClient ontologyClient, IProposalTemplateClient proposalTemplateClient,
+            IPointsClient pointsClient, IProposalAttributeClient proposalAttributeClient,
+            IProposalMoveClient proposalMoveClient, IProposalPhaseClient proposalPhaseClient,
             IProposalClient proposalClient, IMembershipClient membershipClient,
             IProposalMemberRatingClient proposalMemberRatingClient,
             IProposalJudgeRatingClient proposalJudgeRatingClient) {
