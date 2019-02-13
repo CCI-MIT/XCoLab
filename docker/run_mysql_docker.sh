9da1261408c920013e152497519f0513be367645
miguel <gamallomiguel@gmail.com>
2019-02-13 19:32:58 +0100

    Create Docker database configuration files with mysql-server:5.7

---
+++ docker/run_mysql_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "Enter the host path for the database files (must be empty): " dataPath
+read -s -p "Enter root password for MySQL: " mysqlRootPassword
+echo
+read -s -p "Enter password for database user xcolab: " mysqlPassword
+echo
+
+currentPath=$(pwd)
+
+docker run -d -p 3306:3306 --name xcolab_mysql --env-file ${currentPath}/mysql_env.list \
+-e MYSQL_PASSWORD=${mysqlPassword} -e MYSQL_ROOT_PASSWORD=${mysqlRootPassword} \
+--mount type=bind,src=${currentPath}/mysql_config,dst=/etc/mysql \
+--mount type=bind,src=${dataPath},dst=/var/lib/mysql \
+mysql/mysql-server:5.7
