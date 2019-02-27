98bb32656ecfeabc25f44fc964638a8f3b0dd653
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2019-02-27 16:16:50 -0500

    [COLAB-2964] add chile colab starter SQL statements

---
+++ sql/starter/chile-colab.sql
@@ -0,0 +1,7 @@
+-- Starter SQL statements for the Chile Colab
+
+REPLACE INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value) VALUES ('COLAB_NAME', 0, 1, 'Le de Cancer CoLab', 0);
+REPLACE INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value) VALUES ('ACTIVE_THEME', 0, 1, 'CHILE_COLAB', 0);
+REPLACE INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value) VALUES ('PROPOSALS_COMMENTS_IN_SEPARATE_TAB', 0, 1, '', 1);
+REPLACE INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value) VALUES ('PROPOSALS_COMMENTS_IN_SEPARATE_TAB', 0, 0, '', 0);
+REPLACE INTO admin__configuration_attribute (name, additional_id, numeric_value, string_value, real_value) VALUES ('IS_I18N_ACTIVE', 0, 1, '', 0);
