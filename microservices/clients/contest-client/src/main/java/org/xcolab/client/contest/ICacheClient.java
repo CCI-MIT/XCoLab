b066c0b9e8c0f638190525d831a95b5d8713e4e9
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2019-05-29 05:47:40 +0200

    [COLAB-2972] change feign-controller annotation for admin-service

--- microservices/clients/contest-client/src/main/java/org/xcolab/client/contest/ICacheClient.java
+++ microservices/clients/contest-client/src/main/java/org/xcolab/client/contest/ICacheClient.java
@@ -3,7 +3,7 @@ package org.xcolab.client.admin;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("xcolab-admin-service")
+@FeignClient("xcolab-user-service")
 public interface ICacheClient {
 
     @GetMapping("/clearCache")
