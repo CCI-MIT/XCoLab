164ff6e1eb0894b17baeaa7264555b92416ac623
Carlos Botelho <carlosbpf@gmail.com>
2019-09-27 21:38:25 -0300

    fixes for file upload issues, jar not found and content and page editor issues

--- view/src/main/resources/static/js/configureCkEditor.js
+++ view/src/main/resources/static/js/configureCkEditor.js
@@ -2,6 +2,7 @@
 $.fn.modal.Constructor.prototype.enforceFocus = function() {
     modal_this = this;
     $(document).on('focusin.modal', function (e) {
+        console.log("Modal window opened.")
         if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
                 && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
                 && $(e.target.parentNode).hasClass('cke_contents cke_reset')
